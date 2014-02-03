dad9e8afc47cc9ecab5914fd6d8097ed34cdd2e7
jamescw <jcranwellward@gmail.com>
2014-02-03 14:32:39 +0200

    Set default ordering of funds to amount desc

--- EquiTrack/partners/models.py
+++ EquiTrack/partners/models.py
@@ -207,6 +207,9 @@ class PCAGrant(models.Model):
     grant = models.ForeignKey(Grant)
     funds = models.IntegerField(null=True, blank=True)
 
+    class Meta:
+        ordering = ['-funds']
+
     def __unicode__(self):
         return self.grant
 
