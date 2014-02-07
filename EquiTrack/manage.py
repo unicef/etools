8847f86ed5f806842dafd98449754d58d4feea93
jamescw <jcranwellward@gmail.com>
2014-02-07 15:22:34 +0200

    move assets to S3

--- EquiTrack/manage.py
+++ EquiTrack/manage.py
@@ -3,6 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
+
+    # Load the Heroku environment.
+    from herokuapp.env import load_env
+    load_env(__file__, "equitrack")
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EquiTrack.settings.local")
 
     from django.core.management import execute_from_command_line
