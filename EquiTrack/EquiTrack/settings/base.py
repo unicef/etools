1ea5b6f07a95c0e8ebaa5ca58707f6e6439ea5dd
jamescw <jcranwellward@gmail.com>
2014-02-08 17:16:17 +0200

    changed storages

--- EquiTrack/EquiTrack/settings/base.py
+++ EquiTrack/EquiTrack/settings/base.py
@@ -142,7 +142,7 @@ MEDIA_ROOT = normpath(join(SITE_ROOT, 'media'))
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 # Use Amazon S3 for static files storage.
-STATICFILES_STORAGE = "require_s3.storage.OptimizedCachedStaticFilesStorage"
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
