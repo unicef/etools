2755abf2945c0de039d9a44745469d163658e3af
jamescw <jcranwellward@gmail.com>
2014-02-25 18:26:56 +0200

    add sentry config for production

--- EquiTrack/EquiTrack/settings/production.py
+++ EquiTrack/EquiTrack/settings/production.py
@@ -21,6 +21,14 @@ DATABASES = {
 }
 ########## END DATABASE CONFIGURATION
 
+RAVEN_CONFIG = {
+    'dsn': 'https://02b60c5ae099494d8ffe93d1701f9448:d55afaa1a9cb498180f112e41deb34e9@app.getsentry.com/20023',
+}
+
+INSTALLED_APPS = INSTALLED_APPS + (
+    'raven.contrib.django.raven_compat',
+)
+
 ########## EMAIL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
