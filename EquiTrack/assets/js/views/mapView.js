313296d109271e423e538e5563aed0beedce45ad
jamescw <jcranwellward@gmail.com>
2014-05-07 19:31:10 +0300

    Replaced the tile provider with MapQuest. Maps now work,Yay!

--- EquiTrack/assets/js/views/mapView.js
+++ EquiTrack/assets/js/views/mapView.js
@@ -18,9 +18,9 @@ define([
     render: function() {
       var self = this;
       L.Icon.Default.imagePath = '/static/img';
-      var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png',
-        cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade, Points &copy 2012 LINZ',
-        cloudmade = L.tileLayer(cloudmadeUrl, {maxZoom: 17, attribution: cloudmadeAttribution}),
+      var tileUrl = 'http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg',
+        tileAttribution = 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">',
+        tiles = L.tileLayer(tileUrl, {maxZoom: 17, attribution: tileAttribution}),
         latlng = L.latLng(33.89, 35.51);
 
       // remove existing markers
@@ -61,7 +61,7 @@ define([
       if (typeof this.map != 'undefined') {
         this.map.addLayer(this.markers);
       } else {
-        this.map = L.map('map', {center: latlng, zoom: 9, layers: [cloudmade, this.markers]});
+        this.map = L.map('map', {center: latlng, zoom: 9, layers: [tiles, this.markers]});
       }
     }
   });
