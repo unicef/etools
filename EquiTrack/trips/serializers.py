fc60ed355f8257edb9a59bb69fe9c646930299b9
jamescw <jcranwellward@gmail.com>
2014-07-25 13:42:41 +0300

    added admin url for trips

--- EquiTrack/trips/serializers.py
+++ EquiTrack/trips/serializers.py
@@ -1,5 +1,6 @@
 __author__ = 'jcranwellward'
 
+from django.contrib.sites.models import Site
 
 from rest_framework import serializers
 
@@ -11,6 +12,7 @@ class TripSerializer(serializers.ModelSerializer):
     traveller = serializers.CharField(source='owner')
     section = serializers.CharField(source='section.name')
     related_to_pca = serializers.CharField(source='no_pca')
+    url = serializers.CharField(source='get_admin_url')
 
     def transform_traveller(self, obj, value):
         return obj.owner.get_full_name()
@@ -18,10 +20,17 @@ class TripSerializer(serializers.ModelSerializer):
     def transform_partners(self, obj, value):
         return ', '.join(obj.partners.all())
 
+    def transform_url(self, obj, value):
+        return '{}{}'.format(
+            Site.objects.get_current(),
+            obj.get_admin_url()
+        )
+
     class Meta:
         model = Trip
         fields = (
             'id',
+            'url',
             'traveller',
             'section',
             'purpose_of_travel',
