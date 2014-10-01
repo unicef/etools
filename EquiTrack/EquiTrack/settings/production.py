f84a352f59babeb5248b5ad415358ce9191ad344
jamescw <jcranwellward@gmail.com>
2014-10-01 12:50:35 +0300

    disable 404 sentry logging for the moment

--- EquiTrack/EquiTrack/settings/production.py
+++ EquiTrack/EquiTrack/settings/production.py
@@ -19,9 +19,9 @@ INSTALLED_APPS = INSTALLED_APPS + (
     'raven.contrib.django.raven_compat',
 )
 
-MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
-    'raven.contrib.django.raven_compat.middleware.Sentry404CatchMiddleware',
-)
+# MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
+#     'raven.contrib.django.raven_compat.middleware.Sentry404CatchMiddleware',
+# )
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', None)
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', None)
