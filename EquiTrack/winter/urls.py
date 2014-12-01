0488097fa21fae70ec3679bb0753b4c3f824e674
jamescw <jcranwellward@gmail.com>
2014-12-01 02:14:52 +0200

    Added winter app

--- EquiTrack/winter/urls.py
+++ EquiTrack/winter/urls.py
@@ -8,4 +8,5 @@ from .views import WinterDashboardView
 urlpatterns = patterns(
     '',
     url(r'^$', WinterDashboardView.as_view(), name='winter_dashboard'),
+    url(r'^app$', 'winter.views.send_apk', name='winter_app'),
 )
--- EquiTrack/winter/views.py
+++ EquiTrack/winter/views.py
@@ -1,8 +1,27 @@
 __author__ = 'jcranwellward'
 
+import os
+
+from django.conf import settings
+from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.core.servers.basehttp import FileWrapper
 
 
 class WinterDashboardView(TemplateView):
 
     template_name = 'winter/dashboard.html'
+
+
+@login_required
+def send_apk(request):
+
+    filename = os.path.join(settings.STATIC_ROOT, 'app/UniSupply.apk')
+
+    wrapper = FileWrapper(file(filename))
+    response = HttpResponse(wrapper)
+    response['Content-Length'] = os.path.getsize(filename)
+    response['Content-Type'] = 'application/vnd.android.package-archive'
+    response['Content-Disposition'] = 'inline; filename={}'.format(os.path.basename(filename))
+    return response
