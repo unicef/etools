c0487c65bec88b79eca9c2a609cef2173940e804
jamescw <jcranwellward@gmail.com>
2015-07-17 16:19:07 -0400

    removed duplicate

--- EquiTrack/EquiTrack/settings/base.py
+++ EquiTrack/EquiTrack/settings/base.py
@@ -302,7 +302,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 )
 ########## END MIDDLEWARE CONFIGURATION
 
