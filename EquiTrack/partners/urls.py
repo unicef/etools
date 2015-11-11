36c8b11b760277934d09a68d8e1282cad6b16f0c
jamescw <jcranwellward@gmail.com>
2015-11-11 20:38:56 +0200

    disable partner login

--- EquiTrack/partners/urls.py
+++ EquiTrack/partners/urls.py
@@ -14,5 +14,5 @@ urlpatterns = patterns(
     url(r'locations/$', LocationView.as_view(), name='locations'),
 
     # auth and registration for partners
-    url(r'', include('registration.auth_urls')),
+    #url(r'', include('registration.auth_urls')),
 )
