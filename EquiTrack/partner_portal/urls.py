1f28f9ee685bc8d9da49ef7488868490d7e3e482
Robert Avram <robert@ravdev.com>
2015-12-07 15:28:29 -0500

    updates

--- EquiTrack/partner_portal/urls.py
+++ EquiTrack/partner_portal/urls.py
@@ -7,7 +7,7 @@ from EquiTrack.utils import partner_required
 
 from .views import (
     DashView,
-    LoginFailedView
+    LoginFailedView,
 )
 
 
@@ -15,7 +15,8 @@ from .views import (
 
 urlpatterns = patterns(
     '',
-    url(r'^$', partner_required(DashView.as_view())),
+    #url(r'^$', partner_required(DashView.as_view())),
+    url(r'^$', DashView.as_view()),
     url(r'^accounts/loginfailed/(?P<email>.+)/$', LoginFailedView.as_view(), name='sociallogin_notamember'),
 
 
--- EquiTrack/partner_portal/views.py
+++ EquiTrack/partner_portal/views.py
@@ -2,10 +2,20 @@ __author__ = 'Robi'
 
 from django.views.generic import FormView, TemplateView, View
 from django.utils.http import urlsafe_base64_decode
+from django.http import HttpResponse
+from django.conf import settings
+# class DashView(TemplateView):
+#     template_name = "partner_portal/dash.html"
 
-class DashView(TemplateView):
-    template_name = "partner_portal/dash.html"
 
+class DashView(View):
+
+    def get(self, request):
+        # not serving this as a static file in case in the future we want to be able to change versions
+        with open(settings.SITE_ROOT + '/templates/partner_portal/index.html', 'r') as my_f:
+            result = my_f.read()
+
+        return HttpResponse(result)
 
 class LoginFailedView(TemplateView):
 
