ff17227539cc0724fda080ba2bd68d44d231d70d
Robert Avram <robert@ravdev.com>
2016-01-14 11:22:52 -0500

    update path for interventions api

--- EquiTrack/partners/urls.py
+++ EquiTrack/partners/urls.py
@@ -19,7 +19,7 @@ urlpatterns = patterns(
     url(r'^accounts/loginfailed/(?P<email>.+)/$', PortalLoginFailedView.as_view(), name='sociallogin_notamember'),
     url(r'^locations/$', LocationView.as_view(), name='locations'),
     url(r'^staffmember/(?P<pk>\d+)/$', PartnerStaffMemberPropertiesView.as_view()),
-    url(r'^interventions/$', PartnerInterventionsView.as_view()),
+    url(r'^api/interventions/$', PartnerInterventionsView.as_view()),
 
     # auth and registration for partners
     #url(r'', include('registration.auth_urls')),
