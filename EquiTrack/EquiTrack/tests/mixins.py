5f5baf3b68629fe4c7110914bc7bd7ab3910a733
jamescw <jcranwellward@gmail.com>
2016-01-14 15:19:44 +0200

    rerun tests

--- EquiTrack/EquiTrack/tests/mixins.py
+++ EquiTrack/EquiTrack/tests/mixins.py
@@ -5,11 +5,10 @@ from django.test import TestCase
 
 from rest_framework.test import APIRequestFactory, APIClient, force_authenticate
 
-from tenant_schemas.utils import get_tenant_model
-from tenant_schemas.utils import get_public_schema_name
+from tenant_schemas.test.cases import TenantTestCase
 
 
-class APITenantTestCase(TestCase):
+class APITenantTestCase(TenantTestCase):
     client_class = APIClient
 
     def forced_auth_req(self, method, url, handler, user=None, data={}):
@@ -21,27 +20,3 @@ class APITenantTestCase(TestCase):
         force_authenticate(request, user=user)
         return view(request)
 
-    @classmethod
-    def setUpClass(cls):
-        cls.factory = APIRequestFactory
-        cls.sync_shared()
-        tenant_domain = 'tenant.test.com'
-        cls.tenant = get_tenant_model()(domain_url=tenant_domain, schema_name='test', name="Test Country")
-        cls.tenant.save(verbosity=0)  # todo: is there any way to get the verbosity from the test command here?
-        connection.set_tenant(cls.tenant)
-
-
-    @classmethod
-    def tearDownClass(cls):
-        connection.set_schema_to_public()
-        cls.tenant.delete()
-
-        cursor = connection.cursor()
-        cursor.execute('DROP SCHEMA test CASCADE')
-
-    @classmethod
-    def sync_shared(cls):
-        call_command('migrate_schemas',
-                     schema_name=get_public_schema_name(),
-                     interactive=False,
-                     verbosity=0)
