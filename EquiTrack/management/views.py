da4ad89ca04c31213194e949573ba9ae8c53a94b
Robert Avram <robert@ravdev.com>
2016-01-19 10:52:13 -0500

    agreements stats api

--- EquiTrack/management/views.py
+++ EquiTrack/management/views.py
@@ -1,6 +1,7 @@
 __author__ = 'RobertAvram'
 
 
+from datetime import date, timedelta
 from django.db import connection
 
 from rest_framework.response import Response
@@ -129,36 +130,46 @@ class AgreementsStatisticsView(APIView):
     model = Agreement
 
     def get(self, request, **kwargs):
+        today = date.today()
         # get all the countries:
         country_list = Country.objects.all()
-        agreements_by_country = {}
+        results = []
         for country in country_list:
             # set tenant for country
             connection.set_tenant(country)
             # get count for agreements
-            country_agreements_count = Agreement.objects.count()
+            country_agreements_count = Agreement.objects.filter(
+                start__lt=today,
+                end__gt=today
+            ).count()
 
-            agreements_by_country[country.name] = {
-                "total_agreements": country_agreements_count
-            }
-        return Response(agreements_by_country)
+            results.append({
+                "countryName": country.name,
+                "totalAgreements": country_agreements_count
+            })
+        return Response(results)
 
 
 class InterventionsStatisticsView(APIView):
     model = PCA
 
     def get(self, request, **kwargs):
+        today = date.today()
         # get all the countries:
         country_list = Country.objects.all()
-        interventions_by_country = {}
+        results = []
         for country in country_list:
             # set tenant for country
             connection.set_tenant(country)
-            # get count for interventions
-            country_interventions_count = Agreement.objects.count()
+            # get count for agreements
+            country_interventions_count = PCA.objects.filter(
+                start__lt=today,
+                end__gt=today
+            ).count()
 
-            interventions_by_country[country.name] = {
-                "total_interventions": country_interventions_count
-            }
-        return Response(interventions_by_country)
+            results.append({
+                "countryName": country.name,
+                "totalInterventions": country_interventions_count
+            })
+        return Response(results)
 
