979557d8d5b4b5ecf7a25dcaa7cc811730aba9c2
jamescw <jcranwellward@gmail.com>
2016-02-12 20:41:14 +0200

    typo

--- EquiTrack/EquiTrack/settings/test.py
+++ EquiTrack/EquiTrack/settings/test.py
@@ -2,6 +2,6 @@ from local import *
 
 
 # This has to be set to this particular backend in order for django to grab email and expose in tests
-POST_OFFICE['BACKENDs']['default'] = 'django.core.mail.backends.locmem.EmailBackend'
+POST_OFFICE['BACKENDS']['default'] = 'django.core.mail.backends.locmem.EmailBackend'
 
 
