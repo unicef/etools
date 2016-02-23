86c1d417f27a77c3ad8633f8518596fc759ef5b0
jamescw <jcranwellward@gmail.com>
2016-02-23 11:13:25 +0200

    remove imports and exports

--- EquiTrack/partners/admin.py
+++ EquiTrack/partners/admin.py
@@ -7,7 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 
 import autocomplete_light
 from reversion import VersionAdmin
-from import_export.admin import ImportExportMixin, ExportMixin, base_formats
+from import_export.admin import ExportMixin, base_formats
 from generic_links.admin import GenericLinkStackedInline
 
 from EquiTrack.mixins import CountryUsersAdminMixin
@@ -454,7 +454,7 @@ class DocumentInlineAdmin(admin.TabularInline):
     changeform_link.short_description = 'View Intervention Details'
 
 
-class PartnerAdmin(ImportExportMixin, admin.ModelAdmin):
+class PartnerAdmin(ExportMixin, admin.ModelAdmin):
     form = PartnersAdminForm
     resource_class = PartnerResource
     list_display = (
