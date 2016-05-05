f759199e7ab144dcc666c51ac42b2cbc2147c315
Robert Avram <robertavram@users.noreply.github.com>
2016-05-05 13:21:22 -0400

    Migrations fix (#225)
    * consistency updates
    
    * update migrations
    
    * cleanup
    
    * update migration
    
    a bit chopped but works
    
    * migration updates

--- EquiTrack/partners/migrations/0044_pca_planned_visits.py
+++ EquiTrack/partners/migrations/0044_pca_planned_visits.py
@@ -12,11 +12,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
-            model_name='indicatorreport',
-            name='report_status',
-            field=models.CharField(default=b'ontrack', max_length=15, choices=[(b'ontrack', b'On Track'), (b'constrained', b'Constrained'), (b'noprogress', b'No Progress'), (b'targetmet', b'Target Met')]),
-        ),
-        migrations.AddField(
             model_name='pca',
             name='planned_visits',
             field=models.IntegerField(default=0),
