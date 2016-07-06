2bc17850b161e27899ce9923ab41be45f4ef77e3
Robert Avram <robert@ravdev.com>
2016-07-06 15:42:43 -0400

    build updates

---
+++ docker-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /application;
+pip wheel -r requirements.txt;
+rm -rf frontend;
+
+if [ "$CIRCLE_BRANCH" != "develop" ] && [ "$CIRCLE_BRANCH" != "staging" ] && [ "$CIRCLE_BRANCH" != "master" ]; then
+        CIRCLE_BRANCH="develop";
+fi
+
+
+git clone -b $CIRCLE_BRANCH https://github.com/unicef/etools-partner-portal.git frontend;
+cp -ar /node_modules frontend/node_modules;
+
+cd frontend; ./build.sh;
+
+cp -ar node_modules /node_modules;
+
+cd ../frontend_build; ./build.sh;
