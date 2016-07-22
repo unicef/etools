cbdf18dcc52a74a20a6c0f5cebcd3bd2c753114c
Robert Avram <robertavram@users.noreply.github.com>
2016-07-22 14:28:17 -0400

    Develop (#58)
    * changed deprecated name for the number (#37)
    
    #118468621
    
    * update build process (#38)
    
    * update build process
    
    [DO NOT MERGE]
    
    * build updates
    
    * small tweaks
    
    * small tweaks
    
    * get around permissions
    
    * update gitignore
    
    * display spinner when waiting for map location search results (#27)
    
    * display spinner when waiting for map location search results
    
    * simplify loading spinner
    
    * remove unneeded Icon file
    
    * fix government ntervention implementation status
    
    [Finishes #105550212]
    
    * update Android / iOS builds (#41)
    
    * set agreement start end dates based on signed dates and result structure (#34)
    
    * set agreement start end dates based on signed dates and result structure
    
    * move start end date auto population from model to form
    
    * updated error messages
    
    * added agreement form tests
    
    * added start date validation and test
    
    * fix-government-in-simple WIP
    
    [WIP] [DO NOT MERGE]
    
    * migration fixes
    
    * fix-circle-ci-build (#50)
    
    * fix-circle-ci-build
    
    updated library fail
    
    * correct spelling error
    
    * small updates
    
    * Added etools/github contribution best practices guide (#40)
    
    * Added etools/github contribution best practices guide
    
    A document introducing new developers to the eTools project workflow,
    and best practices when contributing to the GitHub.
    
    * docs(CONTRIBUTING): change file name
    
    * docs(guide): add commit guidelines section 
    
    Add section on committing message best practices
    Add section headers dividing concept from practices
    
    PR (#40)
    
    * small tweaks
    
    * HACT dashboard tweaks to show trips for government interventions
    
    * small tweak
    
    * Listing Trip's Action Points in email notification (#30)
    
    * Fix small issues - BY Ali (#44)
    
    * fix distribution items checkbox readonly
    
    * Fix some issues
    
    Result display name
    Approve trip by supervisor
    Do not send trip notification to PA if no budget owner
    
    * Fix test sending  email to PA with budget owner
    
    * tweaks
    
    * migration add (#56)
    
    * remove existing migration
    
    * fix migration dependency
    
    * gitingore update (#57)
    
    * small tweaks
    
    * add migration
    
    IN DEV ROLL BACK TRIPS MIGRATIONS TO 16 AND RUN MIGRATIONS AGAIN
    
    * ENJOY PERFORMANCE
    
    Fix performance issue on compound queries
    
    * fix locations latency
    
    * planned cash transfers for non-gov
    
    * planned visits for NGOs
    
    * only show government partners if they have interventions
    
    * fix unicode return
    
    * formatting fix
    
    * small tweaks
    
    * refractor(templates/admin): change test environments to have more obvious headers (#53)
    
    * refractor(templates/admin): change test environments to have more obvious headers
    
    Remove "Testing Environment" top bar.
    Change banner in localhost, etools-dev, and etools-staging to be red
    and display "Local Testing Environment", "Develop Testing Environment",
    and "Staging Testing Environment" on banner respectively.
    
    * refractor(#header): obvious testing header for dashboard
    
    Added change from 097f25cbdfa58b4866e385b19be9aa22284c198e to etools dashboard
    
    * docs(guide): new dev setup guide for linux/ubuntu (#54)

--- EquiTrack/partners/forms.py
+++ EquiTrack/partners/forms.py
@@ -291,6 +291,11 @@ class DistributionPlanFormSet(ParentInlineAdminFormSet):
 
 class AgreementForm(UserGroupForm):
 
+    ERROR_MESSAGES = {
+         'end_date': 'End date must be greater than start date',
+         'start_date_val': 'Start date must be greater than laatest of signed by partner/unicef date',
+     }
+
     user_field = u'signed_by'
     group_name = u'Senior Management Team'
 
@@ -310,14 +315,19 @@ class AgreementForm(UserGroupForm):
         agreement_number = cleaned_data.get(u'agreement_number')
         start = cleaned_data.get(u'start')
         end = cleaned_data.get(u'end')
+        signed_by_partner_date = cleaned_data.get(u'signed_by_partner_date')
+        signed_by_unicef_date = cleaned_data.get(u'signed_by_unicef_date')
 
         if partner and agreement_type == Agreement.PCA:
             # Partner can only have one active PCA
             # pca_ids = partner.agreement_set.filter(agreement_type=Agreement.PCA).values_list('id', flat=True)
             # if (not self.instance.id and pca_ids) or \
             #         (self.instance.id and pca_ids and self.instance.id not in pca_ids):
-            if partner.get_last_agreement and partner.get_last_agreement != self.instance:
-                if not start > partner.get_last_agreement.start and not end > partner.get_last_agreement.end:
+            if start and end and \
+                    partner.get_last_pca and \
+                    partner.get_last_pca != self.instance:
+
+                if start < partner.get_last_pca.end:
                     err = u'This partner can only have one active {} agreement'.format(agreement_type)
                     raise ValidationError({'agreement_type': err})
 
@@ -340,6 +350,37 @@ class AgreementForm(UserGroupForm):
                 raise ValidationError(
                     _(u'SSFA can not be more than a year')
                 )
+
+        if start and end and start > end:
+            raise ValidationError({'end': self.ERROR_MESSAGES['end_date']})
+
+        # check if start date is greater than or equal than greatest signed date
+        if signed_by_partner_date and signed_by_unicef_date and start:
+            if signed_by_partner_date > signed_by_unicef_date:
+                if start < signed_by_partner_date:
+                    raise ValidationError({'start': self.ERROR_MESSAGES['start_date_val']})
+            else:
+                if start < signed_by_unicef_date:
+                    raise ValidationError({'start': self.ERROR_MESSAGES['start_date_val']})
+
+        if self.instance.id and self.instance.agreement_type != agreement_type \
+                and signed_by_partner_date and signed_by_unicef_date:
+            raise ValidationError(
+                    _(u'Agreement type can not be changed once signed by unicef and partner ')
+            )
+
+        #  set start date to one of the signed dates
+        if start is None and agreement_type == Agreement.PCA:
+            # if both signed dates exist
+            if signed_by_partner_date and signed_by_unicef_date:
+                if signed_by_partner_date > signed_by_unicef_date:
+                    self.cleaned_data[u'start'] = signed_by_partner_date
+                else:
+                    self.cleaned_data[u'start'] = signed_by_unicef_date
+
+        # set end date to result structure end date
+        if end is None:
+            self.cleaned_data[u'end'] = ResultStructure.current().to_date
 
         # TODO: prevent more than one agreement being created for the current period
         # agreements = Agreement.objects.filter(
@@ -573,7 +614,7 @@ class PartnershipForm(UserGroupForm):
         signed_by_partner_date = cleaned_data[u'signed_by_partner_date']
         start_date = cleaned_data[u'start_date']
         end_date = cleaned_data[u'end_date']
-        initiation_date = cleaned_data[u'initiation_date']
+        initiation_date = cleaned_data.get(u'initiation_date')
         submission_date = cleaned_data[u'submission_date']
         review_date = cleaned_data[u'review_date']
 
