c7721b00b81aa7368db621208c842871c91c8dba
ilcsik <ilcsik@gmail.com>
2016-08-17 02:57:05 +0200

    Fix development setup (#90)
    * Fix development environment setup
    
    * Move local dependencies to requirements/local.txt
    
    * Add local_base settings so local deps can be removed when CI runs

--- EquiTrack/EquiTrack/settings/local.py
+++ EquiTrack/EquiTrack/settings/local.py
@@ -1,21 +1,4 @@
-"""Development settings and globals."""
-from os.path import join, normpath
-
-from base import *
-
-
-########## DEBUG CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = True
-FILER_DEBUG = DEBUG
-
-ALLOWED_HOSTS = ['127.0.0.1']
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
-TEMPLATE_DEBUG = DEBUG
-########## END DEBUG CONFIGURATION
-
-CELERY_ALWAYS_EAGER = True
+from local_base import *
 
 ########## TOOLBAR CONFIGURATION
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
@@ -37,41 +20,3 @@ DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TEMPLATE_CONTEXT': True,
 }
 ########## END TOOLBAR CONFIGURATION
-
-
-JWT_AUTH = {
-   'JWT_ENCODE_HANDLER':
-   'rest_framework_jwt.utils.jwt_encode_handler',
-
-   'JWT_DECODE_HANDLER':
-   'rest_framework_jwt.utils.jwt_decode_handler',
-
-   'JWT_PAYLOAD_HANDLER':
-   'rest_framework_jwt.utils.jwt_payload_handler',
-
-   'JWT_PAYLOAD_GET_USER_ID_HANDLER':
-   'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
-
-   'JWT_PAYLOAD_GET_USERNAME_HANDLER':
-   'rest_framework_jwt.utils.jwt_get_username_from_payload_handler',
-
-   'JWT_RESPONSE_PAYLOAD_HANDLER':
-   'rest_framework_jwt.utils.jwt_response_payload_handler',
-
-   #'JWT_SECRET_KEY': JWT_SECRET_KEY,
-   'JWT_SECRET_KEY': 'ssdfsdfsdfsd',
-   #'JWT_ALGORITHM': 'RS256',
-   'JWT_ALGORITHM': 'HS256',
-   'JWT_VERIFY': True,
-   'JWT_VERIFY_EXPIRATION': True,
-   'JWT_LEEWAY': 30,
-   'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=30000),
-   #'JWT_AUDIENCE': 'https://etools-staging.unicef.org/API',
-   'JWT_AUDIENCE': None,
-   'JWT_ISSUER': None,
-
-   'JWT_ALLOW_REFRESH': False,
-   'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
-
-   'JWT_AUTH_HEADER_PREFIX': 'JWT',
-}
