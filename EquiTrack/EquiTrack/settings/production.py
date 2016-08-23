bd3102ef3885898b91eca8f6399df751931ba5f6
ntrncic <ntrncic@unicef.org>
2016-08-23 12:36:39 -0400

    allow 30 seconds for time difference between adfs server and etools server

--- EquiTrack/EquiTrack/settings/production.py
+++ EquiTrack/EquiTrack/settings/production.py
@@ -154,8 +154,8 @@ SAML_CONFIG = {
     # set to 1 to output debugging information
     'debug': 1,
 
-    # allow 3 seconds for time difference between adfs server and etools server
-    'accepted_time_diff': 3,  # in seconds
+    # allow 30 seconds for time difference between adfs server and etools server
+    'accepted_time_diff': 30,  # in seconds
 
     # certificate
     'key_file': join(DJANGO_ROOT, 'saml/certs/saml.key'),  # private part
