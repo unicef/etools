18b8bebfde3d8b00839df9f05d36dbffaf362fbf
Zoltan Ilcsik <ilcsik@gmail.com>
2016-08-25 10:59:41 +0200

    Resolve database migration conflicts

---
+++ EquiTrack/reports/migrations/0024_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reports', '0023_auto_20160812_2322'),
+        ('reports', '0018_auto_20160811_1654'),
+    ]
+
+    operations = [
+    ]
