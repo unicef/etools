23bb9440374aa6cf513952f0ce8aaa3abf0cac52
Robert Avram <robert@ravdev.com>
2016-09-13 10:55:03 -0400

    add clean result type script

--- EquiTrack/EquiTrack/util_scripts.py
+++ EquiTrack/EquiTrack/util_scripts.py
@@ -96,7 +96,7 @@ def fix_duplicate_results(country_name):
             "resultchain_set",
             "tripfunds_set"]
     fattrs_mapping = {
-        "governmentinterventionresult_set" : "result",
+        "governmentinterventionresult_set": "result",
         "indicator_set" : "result",
         "ramindicator_set": "result",
         "resultchain_set": "result",
@@ -171,10 +171,6 @@ def fix_duplicate_results(country_name):
     dupes = Result.objects.values('wbs').annotate(Count('wbs')).order_by().filter(wbs__count__gt=1, wbs__isnull=False).exclude(wbs__exact='').all()
     dupes = sorted(dupes, key=lambda x: x['wbs'].count('/'), reverse=True)
     _run_clean('wbs', dupes)
-    # get all duplicates that have a null wbs but same names
-#    printtf("GETTING ALL DUPLICATES FOR RESULTS WITHOUT WBSS")
-#    dupes = Result.objects.values('wbs', 'name').annotate(Count('name')).order_by().filter(name__count__gt=1, wbs__isnull=True).filter(Q(wbs__isnull=True) | Q(wbs__exact='')).all()
-#    _run_clean('name', dupes)
 
 
 #execute first
@@ -225,6 +221,65 @@ def cp_fix(country_name):
         res.country_programme = cp
         res.save()
 
+def clean_result_types(country_name):
+    if not country_name:
+        printtf("country name required /n")
+        set_country(country_name)
+    if not country_name:
+        printtf("country name required /n")
+
+    set_country(country_name)
+
+    fattrs = ["result_set"]
+
+    def relates_to_anything(cobj):
+        for a in fattrs:
+            if getattr(cobj, a).count():
+                printtf(cobj.id, cobj, "relates to ", a)
+                return True
+        return False
+
+    def update_relationships(dpres, keep):
+        for a in fattrs:
+            objs = getattr(dpres, a).all()
+            if len(objs):
+                for obj in objs:
+                    obj.result_type = keep
+                    obj.save()
+                    printtf("saved obj.id={} obj {} with keepid{} keep {}".format(obj.id, obj, keep.id, keep))
+
+    def _run_clean(dupes):
+        printtf(len(dupes), dupes)
+        for dup in dupes:
+            dupresults = ResultType.objects.filter(name=dup['name']).all()
+            delq = []
+            keep = None
+            for dpres in dupresults:
+                if not keep:
+                    keep = dpres
+                    continue
+                else:
+                    error = False
+                    if relates_to_anything(dpres):
+                        try:
+                            update_relationships(dpres, keep)
+                        except Exception as exp:
+                            printtf('Cannot remove Object {}, id={}'.format(dpres, dpres.id))
+                            error = True
+                    if error:
+                        printtf("ERROR OCCURED ON RECORD", dpres.id, dpres)
+                        continue
+                    delq.append(dpres)
+            if not len(delq):
+                printtf("Nothing is getting removed for {}".format(dupes))
+            else:
+                # delete everyting in the queue
+                [i.delete() for i in delq]
+                printtf("deleting: ", delq)
+
+    # get all duplicates that have the same wbs
+    dupes = ResultType.objects.values('name').annotate(Count('name')).order_by().filter(name__count__gt=1).all()
+    _run_clean(dupes)
 
 
 def up_cp_first(n):
