841927730d5940a85267f9ebfc893101cf85725d
Simon Sagi <github@simonsagi.com>
2016-09-13 09:14:56 -0400

    - Change comment model

--- EquiTrack/workplan/migrations/0001_initial.py
+++ EquiTrack/workplan/migrations/0001_initial.py
@@ -8,7 +8,6 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contenttypes', '0002_remove_content_type_name'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
@@ -19,9 +18,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
                 ('text', models.TextField()),
-                ('object_id', models.PositiveIntegerField(null=True, blank=True)),
                 ('author', models.ForeignKey(related_name='+', to=settings.AUTH_USER_MODEL)),
-                ('content_type', models.ForeignKey(blank=True, to='contenttypes.ContentType', null=True)),
                 ('tagged_users', models.ManyToManyField(related_name='_comment_tagged_users_+', to=settings.AUTH_USER_MODEL, blank=True)),
             ],
         ),
--- EquiTrack/workplan/models.py
+++ EquiTrack/workplan/models.py
@@ -10,8 +10,3 @@ class Comment(models.Model):
     tagged_users = models.ManyToManyField(User, blank=True, related_name='+')
     timestamp = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
-
-    # This model will have relations to multiple other models. One FK would not be enough
-    content_type = models.ForeignKey(ContentType, null=True, blank=True, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField(null=True, blank=True)
-    content_object = GenericForeignKey('content_type', 'object_id')
