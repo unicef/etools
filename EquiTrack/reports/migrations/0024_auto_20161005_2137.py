6054215aef7ec938b08e01706f1a9fa710bc16b4
Robert Avram <robert@ravdev.com>
2016-10-13 13:39:03 -0400

    Migration Tweaks

---
+++ EquiTrack/reports/migrations/0024_auto_20161005_2137.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reports', '0023_auto_20160909_2203'),
+    ]
+
+    operations = [
+
+    ]
