b3c380e8b3f34507a6363116a0c3b2b8e1105860
ntrncic <ntrncic@unicef.org>
2016-10-17 17:56:13 -0400

    add agreements api

--- EquiTrack/partners/filters.py
+++ EquiTrack/partners/filters.py
@@ -194,5 +194,6 @@ class PCAOutputFilter(admin.SimpleListFilter):
 
 class PartnerScopeFilter(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
-        partner_pk = request.parser_context['kwargs']['partner_pk']
-        return queryset.filter(partner__pk=partner_pk)
+        if request.parser_context['kwargs']:
+            return queryset.filter(partner__pk=request.parser_context['kwargs']['partner_pk'])
+        return queryset
