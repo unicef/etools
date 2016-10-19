efbf5cac95d7ef2e7e94199ce4b030adccbf39b7
Simon Sagi <github@simonsagi.com>
2016-10-19 10:51:24 -0400

    - fix export fields

--- EquiTrack/partners/exports.py
+++ EquiTrack/partners/exports.py
@@ -220,6 +220,7 @@ class PCAResource(BaseExportResource):
 # ---- NEW EXPORTS STARTS HERE ----
 
 class PartnerExport(resources.ModelResource):
+    risk_rating = resources.Field()
     agreement_count = resources.Field()
     intervention_count = resources.Field()
     active_staff_members = resources.Field()
@@ -235,6 +236,9 @@ class PartnerExport(resources.ModelResource):
                   'agreement_count', 'intervention_count', 'active_staff_members')
         export_order = fields
 
+    def dehydrate_risk_rating(self, partner_organization):
+        return partner_organization.rating
+
     def dehydrate_agreement_count(self, partner_organization):
         return partner_organization.agreement_set.count()
 
@@ -248,19 +252,25 @@ class PartnerExport(resources.ModelResource):
 
 
 class AgreementExport(resources.ModelResource):
+    reference_number = resources.Field()
     signed_by_partner = resources.Field()
     signed_by_unicef = resources.Field()
     authorized_officers = resources.Field()
+    start_date = resources.Field()
+    end_date = resources.Field()
 
     class Meta:
         model = Agreement
         # TODO add missing fields:
         #   Attached Signed Agreement Link
         #   Amendments (comma separated list of amended fields)
-        fields = ('reference_number', 'partner__vendor_number', 'partner__name', 'partner__short_name', 'agreement',
+        fields = ('reference_number', 'partner__vendor_number', 'partner__name', 'partner__short_name',
                   'start_date', 'end_date', 'signed_by_partner', 'signed_by_partner_date',
                   'signed_by_unicef', 'signed_by_unicef_date', 'authorized_officers')
         export_order = fields
+
+    def dehydrate_reference_number(self, agreement):
+        return agreement.reference_number
 
     def dehydrate_signed_by_partner(self, agreement):
         if agreement.partner_manager:
@@ -276,13 +286,24 @@ class AgreementExport(resources.ModelResource):
         names = [ao.officer.get_full_name() for ao in agreement.authorized_officers.all()]
         return ', '.join(names)
 
+    def dehydrate_start_date(self, agreement):
+        return agreement.start
+
+    def dehydrate_end_date(self, agreement):
+        return agreement.end
+
 
 class InterventionExport(resources.ModelResource):
+    reference_number = resources.Field()
     locations = resources.Field()
     sectors = resources.Field()
     partner_manager_name = resources.Field()
     unicef_manager_name = resources.Field()
     supplies = resources.Field()
+    days_from_submission_to_signed = resources.Field()
+    days_from_review_to_signed = resources.Field()
+    total_unicef_cash = resources.Field()
+    total_budget = resources.Field()
 
     class Meta:
         model = PCA
@@ -297,6 +318,9 @@ class InterventionExport(resources.ModelResource):
                   'signed_by_partner_date', 'partner_manager_name', 'signed_by_unicef_date', 'unicef_manager_name',
                   'total_unicef_cash', 'supplies', 'total_budget', 'planned_visits')
         export_order = fields
+
+    def dehydrate_reference_number(self, intervention):
+        return intervention.reference_number
 
     def dehydrate_locations(self, intervention):
         location_names = [l.location.name for l in intervention.locations.all()]
@@ -319,8 +343,21 @@ class InterventionExport(resources.ModelResource):
         supply_names = [sp.item.name for sp in intervention.supply_plans.all()]
         return ', '.join(supply_names)
 
+    def dehydrate_days_from_submission_to_signed(self, intervention):
+        return intervention.days_from_submission_to_signed
+
+    def dehydrate_days_from_review_to_signed(self, intervention):
+        return intervention.days_from_review_to_signed
+
+    def dehydrate_total_unicef_cash(self, intervention):
+        return intervention.total_unicef_cash
+
+    def dehydrate_total_budget(self, intervention):
+        return intervention.total_budget
+
 
 class GovernmentExport(resources.ModelResource):
+    sectors = resources.Field()
     cash_transfer = resources.Field()
     year = resources.Field()
 
@@ -329,6 +366,9 @@ class GovernmentExport(resources.ModelResource):
         fields = ('number', 'partner__name', 'result_structure__name', 'sectors', 'cash_transfer',
                   'year')
         export_order = fields
+
+    def dehydrate_sectors(self, government):
+        return ''
 
     def dehydrate_cash_transfer(self, government):
         return sum([r.planned_amount for r in government.results.all()])
