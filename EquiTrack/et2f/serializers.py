6086ed9f1ed38abd7ff138b4f4db81ea4858435a
Simon Sagi <github@simonsagi.com>
2016-11-08 09:32:37 -0500

    - support update

--- EquiTrack/et2f/serializers.py
+++ EquiTrack/et2f/serializers.py
@@ -13,42 +13,55 @@ from .models import Travel, IteneraryItem, Expense, Deduction, CostAssignment, C
 
 
 class IteneraryItemSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = IteneraryItem
-        fields = ('origin', 'destination', 'departure_date', 'arrival_date', 'dsa_region', 'overnight_travel',
+        fields = ('id', 'origin', 'destination', 'departure_date', 'arrival_date', 'dsa_region', 'overnight_travel',
                   'mode_of_travel', 'airline')
 
 
 class ExpenseSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = Expense
-        fields = ('type', 'document_currency', 'account_currency', 'amount')
+        fields = ('id', 'type', 'document_currency', 'account_currency', 'amount')
 
 
 class DeductionSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
     day_of_the_week = serializers.CharField(read_only=True)
 
     class Meta:
         model = Deduction
-        fields = ('date', 'breakfast', 'lunch', 'dinner', 'accomodation', 'no_dsa', 'day_of_the_week')
+        fields = ('id', 'date', 'breakfast', 'lunch', 'dinner', 'accomodation', 'no_dsa', 'day_of_the_week')
 
 
 class CostAssignmentSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = CostAssignment
-        fields = ('wbs', 'share', 'grant')
+        fields = ('id', 'wbs', 'share', 'grant')
 
 
 class ClearancesSerializer(serializers.ModelSerializer):
+    medical_clearance = serializers.NullBooleanField()
+    security_clearance = serializers.NullBooleanField()
+    security_course = serializers.NullBooleanField()
+
     class Meta:
         model = Clearances
-        fields = ('medical_clearance', 'security_clearance', 'security_course')
+        fields = ('id', 'medical_clearance', 'security_clearance', 'security_course')
 
 
 class TravelActivitySerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = TravelActivity
-        fields = ('travel_type', 'partner', 'partnership', 'result', 'location')
+        fields = ('id', 'travel_type', 'partner', 'partnership', 'result', 'location')
 
 
 class TravelDetailsSerializer(serializers.ModelSerializer):
@@ -64,6 +77,8 @@ class TravelDetailsSerializer(serializers.ModelSerializer):
         fields = ('reference_number', 'supervisor', 'office', 'end_date', 'section', 'international_travel',
                   'traveller', 'start_date', 'ta_required', 'purpose', 'id', 'itinerary', 'expenses', 'deductions',
                   'cost_assignments', 'clearances', 'status', 'activities')
+        # Review this, as a developer could be confusing why the status field is not saved during an update
+        read_only_fields = ('status',)
 
     def create(self, validated_data):
         itinerary = validated_data.pop('itinerary', [])
@@ -81,7 +96,7 @@ class TravelDetailsSerializer(serializers.ModelSerializer):
                 d['travel'] = instance
                 instances.append(model(**d))
 
-            model.objects.bulk_create(itinerary)
+            model.objects.bulk_create(instances)
 
         # Reverse FK and M2M relations
         create_related_models(IteneraryItem, itinerary)
@@ -98,17 +113,60 @@ class TravelDetailsSerializer(serializers.ModelSerializer):
         return instance
 
     def update(self, instance, validated_data):
-        itinerary = validated_data.pop('itinerary', [])
-        expenses = validated_data.pop('expenses', [])
-        deductions = validated_data.pop('deductions', [])
-        cost_assignments = validated_data.pop('cost_assignments', [])
-        clearances = validated_data.pop('clearances', None)
+        related_attributes = {}
+        for attr_name in ('itinerary', 'expenses', 'deductions', 'cost_assignments', 'activities', 'clearances'):
+            if isinstance(self._fields[attr_name], serializers.ListSerializer):
+                default = []
+            elif self._fields[attr_name].allow_null:
+                default = None
+            else:
+                default = {}
+            related_attributes[attr_name] = validated_data.pop(attr_name, default)
 
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        instance.save()
+        self.update_object(instance, validated_data)
+
+        for attr_name, related_data in related_attributes.items():
+            self.update_related_objects(attr_name, related_data)
 
         return instance
+
+    def update_object(self, obj, data):
+        for attr, value in data.items():
+            setattr(obj, attr, value)
+        obj.save()
+
+    def update_related_objects(self, attr_name, related_data):
+        many = isinstance(self._fields[attr_name], serializers.ListSerializer)
+
+        related = getattr(self.instance, attr_name)
+        if many:
+            # Load the 1ueryset
+            related = related.all()
+
+            model = self._fields[attr_name].child.Meta.model
+            related_to_delete = {o.pk for o in related}
+
+            # Iterate over incoming data and create/update the models
+            for attrs in related_data:
+                pk = attrs.pop('id', None)
+                if pk:
+                    obj = related.get(pk=pk)
+                    self.update_object(obj, attrs)
+                    related_to_delete.remove(pk)
+                else:
+                    model.objects.create(travel=self.instance, **attrs)
+
+            # Delete the leftover
+            model.objects.filter(pk__in=related_to_delete).delete()
+
+        else:
+            if related_data and related:
+                self.update_object(related, related_data)
+            elif related_data and related is None:
+                model = self._fields[attr_name].Meta.model
+                model.objects.creat(travel=self.instance, **related_data)
+            elif related_data is None and related:
+                related.delete()
 
 
 class TravelListSerializer(TravelDetailsSerializer):
