8111256271b676b3452ea49aa5aac6017a9c426c
Simon Sagi <simon@pulilab.com>
2016-11-14 13:31:29 +0100

    - fix exporter

--- EquiTrack/et2f/exports.py
+++ EquiTrack/et2f/exports.py
@@ -6,9 +6,26 @@ from et2f.models import Travel
 
 
 class TravelListExporter(resources.ModelResource):
+    is_driver = resources.Field(attribute='get_is_driver')
+    approval_date = resources.Field(attribute='get_approval_date')
+    ta_reference_number = resources.Field(attribute='get_ta_reference_number')
+    attachment_count = resources.Field(attribute='get_attachment_count')
+
     class Meta:
         model = Travel
         fields = ('id', 'reference_number', 'traveller', 'purpose', 'start_date', 'end_date', 'status', 'created',
                   'section', 'office', 'supervisor', 'ta_required', 'ta_reference_number', 'approval_date', 'is_driver',
                   'attachment_count')
         export_order = fields
+
+    def get_is_driver(self, obj):
+        return obj.is_driver
+
+    def get_ta_reference_number(self, obj):
+        return obj.ta_reference_number
+
+    def get_approval_date(self, obj):
+        return obj.approval_date
+
+    def get_attachment_count(self, obj):
+        return obj.attachment_count
