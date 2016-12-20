32c4d43bbdc3e2ecec144ed1f62f0cb3b72e345a
Simon Sagi <simon@pulilab.com>
2016-12-20 17:26:44 +0100

    - more dsa fix

--- EquiTrack/t2f/helpers.py
+++ EquiTrack/t2f/helpers.py
@@ -134,9 +134,11 @@ class CostSummaryCalculator(object):
 
             last_day_multiplier = self.get_deduction_multiplier(end_date, end_date)
             last_day_rate = daily_rate * Decimal('0.4')
+            last_day_deduction_amount = daily_rate * last_day_multiplier
+            deduction_amount += last_day_deduction_amount
             # The inverted math is here because we are not deducting the deductions, rather multiplying the value with
             # the deductions multiplier
-            amount += last_day_rate * (Decimal(1) - last_day_multiplier)
+            amount += last_day_rate - last_day_deduction_amount
 
         dsa = {'start_date': start_date,
                'end_date': end_date,
