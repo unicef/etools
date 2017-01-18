7f5b43029c64760849177054df93b1947a2588d8
Silin Na <n1207n@gmail.com>
2017-01-18 14:37:43 -0800

    feat: Added EmailTemplates for partners

--- EquiTrack/notification/management/commands/load_email_templates.py
+++ EquiTrack/notification/management/commands/load_email_templates.py
@@ -301,3 +301,30 @@ class Command(NoneCommand):
                 """
             }
         )
+
+        # PartnershipCreatedEmail
+        template, created = EmailTemplate.objects.update_or_create(
+            template_name = 'partners/partnership/created/updated',
+            defaults={
+                'description': 'The email that is sent when a PD/SSFA is added or is updated',
+                'subject': 'PD/SSFA {{number}} has been {{state}}',
+                'content': """
+                Dear Colleague,
+
+                PD/SSFA {{number}} has been {{state}} here:
+
+                {{url}}
+
+                Thank you.
+                """,
+                'html_content': """
+                Dear Colleague,
+                <br/>
+                PD/SSFA {{number}} has been {{state}} here:
+                <br/>
+                {{url}}
+                <br/>
+                Thank you.
+                """
+            }
+        )
