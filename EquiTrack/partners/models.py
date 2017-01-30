e83b07e73535b5c065be550ac1142cfc323bc3d6
ntrncic <ntrncic@unicef.org>
2017-01-30 11:12:17 -0500

    new migrations

--- EquiTrack/partners/models.py
+++ EquiTrack/partners/models.py
@@ -849,7 +849,7 @@ class BankDetails(models.Model):
     """
 
     #TODO: remove agreement field after running util_scripts.bank_details_to_partner()
-    agreement = models.ForeignKey('partners.Agreement', related_name='bank_details')
+    # agreement = models.ForeignKey('partners.Agreement', related_name='bank_details')
 
     # TODO: remove the ability to add blank for the partner_organization field
     partner_organization = models.ForeignKey(PartnerOrganization, related_name='bank_details', null=True, blank=True)
