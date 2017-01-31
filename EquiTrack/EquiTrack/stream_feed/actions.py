9fced9be916d81396daddc5e0fafb504d5e0e160
Silin Na <n1207n@gmail.com>
2017-01-31 15:41:05 -0800

    refactor: Fixed create_snapshot_activity_stream method to handle create action properly
    + New instance is None value

--- EquiTrack/EquiTrack/stream_feed/actions.py
+++ EquiTrack/EquiTrack/stream_feed/actions.py
@@ -12,37 +12,36 @@ def create_snapshot_activity_stream(actor, target, created=False):
     created: A boolean flag that indicates target has been newly created.
     """
 
-    if hasattr(target, 'tracker'):
+    if created:
+        action.send(actor, verb="created",
+                    target=target, previous={},
+                    changes={})
+
+    elif hasattr(target, 'tracker'):
         with transaction.atomic():
-            if created:
-                action.send(actor, verb="created",
-                            target=target, previous={},
-                            changes={})
+            # Get current mutated state of object as dictionary
+            current_obj_dict = model_to_dict(target)
 
-            else:
-                # Get current mutated state of object as dictionary
-                current_obj_dict = model_to_dict(target)
+            # Get all previous values of mutated fields for current object
+            changed_prev_values = target.tracker.changed()
 
-                # Get all previous values of mutated fields for current object
-                changed_prev_values = target.tracker.changed()
+            # Restore the previous state of current object by merging above
+            previous = dict(current_obj_dict.items() + changed_prev_values.items())
 
-                # Restore the previous state of current object by merging above
-                previous = dict(current_obj_dict.items() + changed_prev_values.items())
+            # Extract current field changes from key lookups with current object
+            changes = {k:v for k,v in current_obj_dict.items() if k in changed_prev_values}
 
-                # Extract current field changes from key lookups with current object
-                changes = {k:v for k,v in current_obj_dict.items() if k in changed_prev_values}
+            # Stringify any non-JSON Serializeable data types
+            for key, value in previous.items():
+                if type(value) not in [int, float, bool, str]:
+                    previous[key] = str(previous[key])
 
-                # Stringify any non-JSON Serializeable data types
-                for key, value in previous.items():
-                    if type(value) not in [int, float, bool, str]:
-                        previous[key] = str(previous[key])
+            # Stringify any non-JSON Serializeable data types
+            for key, value in changes.items():
+                if type(value) not in [int, float, bool, str]:
+                    changes[key] = str(changes[key])
 
-                # Stringify any non-JSON Serializeable data types
-                for key, value in changes.items():
-                    if type(value) not in [int, float, bool, str]:
-                        changes[key] = str(changes[key])
-
-                # TODO: Use a different action verb for each status choice in Agreement
-                # Draft, Active, Expired, Suspended, Terminated
-                action.send(actor, verb="changed",
-                            target=target, previous=previous, changes=changes)
+            # TODO: Use a different action verb for each status choice in Agreement
+            # Draft, Active, Expired, Suspended, Terminated
+            action.send(actor, verb="changed",
+                        target=target, previous=previous, changes=changes)
