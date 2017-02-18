4c39362da7f9cba79bce8cc2d430155ca67b6cfd
robertavram <robert.avram@gmail.com>
2017-02-18 15:49:31 -0500

    Gov int validation fix

--- EquiTrack/partners/validation/government_intervention_results.py
+++ EquiTrack/partners/validation/government_intervention_results.py
@@ -1,34 +1,35 @@
 from EquiTrack.validation_mixins import CompleteValidation, check_rigid_fields
+from reports.models import ResultType
 
 
-def planned_amount_valid(r):
-    if r.old_instance:
-        valid, _ = check_rigid_fields(r, ["planned_amount"])
-        return valid
-    return True
-
-def planned_visits_valid(r):
-    if r.old_instance:
-        valid, _ = check_rigid_fields(r, ["planned_visits"])
-        return valid
-    return True
-
-def activity_valid(r):
-    for k, v in r.activity.items():
-        if not v:
-            return False
-    return True
-
-def sectors_valid(r):
-    if not r.sectors.exists():
+def check_if_year_correct(year):
+    try:
+        y = int(year)
+        return 1980 < y < 2030
+    except Exception as e:
         return False
     return True
 
-def sections_valid(r):
-    # TODO Figure out how to create Section on test schema
-    # if not r.sections.exists():
-    #     return False
+
+def cp_output_valid(r):
+    if not r.result:
+        return False
+    elif r.result.result_type.name != ResultType.OUTPUT:
+        return False
     return True
+
+def year_valid(r):
+    if not r.year:
+        return False
+    elif not check_if_year_correct(r.year):
+        return False
+    return True
+
+def planned_amount_valid(r):
+    if not r.planned_amount:
+        return False
+    return True
+
 
 
 class GovernmentInterventionResultValid(CompleteValidation):
@@ -36,17 +37,13 @@ class GovernmentInterventionResultValid(CompleteValidation):
     VALIDATION_CLASS = 'partners.GovernmentInterventionResult'
     # validations that will be checked on every object... these functions only take the new instance
     BASIC_VALIDATIONS = [
-        planned_amount_valid,
-        planned_visits_valid,
-        activity_valid,
-        sectors_valid,
-        sections_valid,
+        cp_output_valid,
+        year_valid,
+        planned_amount_valid
     ]
 
     VALID_ERRORS = {
-        'planned_amount_valid': 'Planned amount cannot be changed.',
-        'planned_visits_valid': 'Planned visits cannot be changed.',
-        'activity_valid': 'Both ID and Description is needed for activity.',
-        'sectors_valid': 'Sector is required.',
-        'sections_valid': 'Section is required.',
+        'cp_output_valid': 'Invalid CP Output, please select an Output as your CP Output',
+        'year_valid': 'Invalid Year, please select a valid year',
+        'planned_amount_valid': 'Please fill in Planned Cash Transfers'
     }
