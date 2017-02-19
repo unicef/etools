565ff3a44638b9fab91c83f4a4bddfc7dc7f6e1e
ntrncic <ntrncic@unicef.org>
2017-02-19 14:41:58 -0500

    new fc and fr models

--- EquiTrack/funds/models.py
+++ EquiTrack/funds/models.py
@@ -44,3 +44,51 @@ class Grant(models.Model):
             self.donor.name,
             self.name
         )
+
+class FundsReservationHeader(models.Model):
+    vendor_code = models.CharField(max_length=20)
+    fr_number = models.CharField(max_length=20)
+    fr_doc_date = models.DateField(null=True, blank=True)
+    fr_type = models.CharField(max_length=50, null=True, blank=True)
+    currency = models.CharField(max_length=5, null=True, blank=True)
+    fr_document_text = models.CharField(max_length=255, null=True, blank=True)
+    fr_start_date = models.DateField(null=True, blank=True)
+    fr_end_date = models.DateField(null=True, blank=True)
+
+
+class FundsReservationItem(models.Model):
+    line_item = models.IntegerField(default=0)
+    wbs = models.CharField(max_length=30, null=True, blank=True)
+    grant_number = models.CharField(max_length=20, null=True, blank=True)
+    fund = models.CharField(max_length=10, null=True, blank=True)
+    overall_amount = models.DecimalField(default=0, max_digits=12, decimal_places=2)
+    due_date = models.DateField(null=True, blank=True)
+
+
+class FundsCommitmentHeader(models.Model):
+    vendor_code = models.CharField(max_length=20)
+    fc_number = models.CharField(max_length=20)
+    fc_doc_date = models.DateField(null=True, blank=True)
+    fr_type = models.CharField(max_length=50, null=True, blank=True)
+    currency = models.CharField(max_length=5, null=True, blank=True)
+    fc_document_text = models.CharField(max_length=255, null=True, blank=True)
+    exchange_rate = models.DecimalField(blank=True, null=True, max_digits=10, decimal_places=2)
+    responsible_person = models.CharField(max_length=100, blank=True, null=True)
+
+
+class FundsCommitmentItem(models.Model):
+    wbs = models.CharField(max_length=30, null=True, blank=True)
+    grant_number = models.CharField(max_length=20, null=True, blank=True)
+    fund = models.CharField(max_length=10, null=True, blank=True)
+    gl_account = models.CharField(max_length=15, null=True, blank=True)
+    overall_amount = models.DecimalField(default=0, max_digits=12, decimal_places=2)
+    overall_amount_dc = models.DecimalField(default=0, max_digits=12, decimal_places=2)
+    due_date = models.DateField(null=True, blank=True)
+    fr_number = models.CharField(max_length=20, null=True, blank=True)
+    commitment_amount = models.DecimalField(default=0, max_digits=12, decimal_places=2)
+    amount_changed = models.DecimalField(default=0, max_digits=12, decimal_places=2)
+    fc_item_text = models.CharField(max_length=255, null=True, blank=True)
+
+
+
+
