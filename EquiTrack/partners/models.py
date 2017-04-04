619e7c3995e6411e2335e3414b251d0515e3a57d
ntrncic <ntrncic@unicef.org>
2017-04-04 14:28:12 -0400

    undo change

--- EquiTrack/partners/models.py
+++ EquiTrack/partners/models.py
@@ -227,7 +227,7 @@ class PartnerOrganization(AdminURLMixin, models.Model):
     AGENCY_CHOICES = Choices(
         ('DPKO', 'DPKO'),
         ('ECA', 'ECA'),
-        ('ECL AC', 'ECLAC'),
+        ('ECLAC', 'ECLAC'),
         ('ESCWA', 'ESCWA'),
         ('FAO', 'FAO'),
         ('ILO', 'ILO'),
