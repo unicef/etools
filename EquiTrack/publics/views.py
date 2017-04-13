91b9d2467b84d741de61ee4a3f69954c8257b552
Simon Sagi <simon@pulilab.com>
2017-04-13 14:57:16 +0200

    - fix missing on static

--- EquiTrack/publics/views.py
+++ EquiTrack/publics/views.py
@@ -6,11 +6,13 @@ from rest_framework import viewsets, mixins, status
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 
+from funds.serializers import GrantSerializer
 from publics.models import Country, DSARegion, Currency, AirlineCompany, WBS, Grant, Fund, TravelExpenseType, \
     BusinessArea
 from publics.serializers import CountrySerializer, DSARegionSerializer, PublicStaticDataSerializer, \
     WBSGrantFundSerializer, WBSGrantFundParameterSerializer, CurrencySerializer, ExpenseTypeSerializer, \
-    BusinessAreaSerializer, GhostDataPKSerializer, MultiGhostDataSerializer, AirlineSerializer
+    BusinessAreaSerializer, GhostDataPKSerializer, MultiGhostDataSerializer, AirlineSerializer, FundSerializer, \
+    WBSSerializer
 from t2f.models import TravelType, ModeOfTravel
 
 
@@ -37,14 +39,6 @@ class CountryViewSet(mixins.ListModelMixin,
                      viewsets.GenericViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
-    permission_classes = (IsAdminUser,)
-
-
-class DSARegionViewSet(mixins.ListModelMixin,
-                       GhostDataMixin,
-                       viewsets.GenericViewSet):
-    queryset = DSARegion.objects.all()
-    serializer_class = DSARegionSerializer
     permission_classes = (IsAdminUser,)
 
 
@@ -116,12 +110,14 @@ class DSARegionsView(GhostDataMixin,
                      viewsets.GenericViewSet):
     serializer_class = DSARegionSerializer
 
-    def list(self, request):
-        workspace = request.user.profile.country
-
+    def get_queryset(self):
+        workspace = self.request.user.profile.country
         dsa_regions = DSARegion.objects.filter(country__business_area__code=workspace.business_area_code)
         dsa_regions = dsa_regions.select_related('country')
+        return dsa_regions
 
+    def list(self, request):
+        dsa_regions = self.get_queryset()
         serializer = self.get_serializer(dsa_regions, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
 
@@ -138,10 +134,11 @@ class BusinessAreasView(GhostDataMixin,
 
 class CurrenciesView(GhostDataMixin,
                      viewsets.GenericViewSet):
+    queryset = Currency.objects.all()
     serializer_class = CurrencySerializer
 
     def list(self, request):
-        currencies = Currency.objects.all()
+        currencies = self.get_queryset()
         serializer = self.get_serializer(currencies, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
 
@@ -168,16 +165,57 @@ class WBSGrantFundView(GhostDataMixin,
     serializer_class = WBSGrantFundSerializer
 
     def list(self, request):
-        parameter_serializer = WBSGrantFundParameterSerializer(data=request.GET, context=self.get_serializer_context())
-        parameter_serializer.is_valid(raise_exception=True)
-
-        business_area = parameter_serializer.validated_data['business_area']
-
-        wbs_qs = WBS.objects.filter(business_area=business_area).prefetch_related('grants')
-        grant_qs = Grant.objects.filter(wbs__in=wbs_qs).prefetch_related('funds')
-        funds_qs = Fund.objects.filter(grants__in=grant_qs)
+        wbs_qs = self.get_wbs_queryset()
+        grant_qs = self.get_grants_queryset()
+        funds_qs = self.get_funds_queryset()
         data = {'wbs': wbs_qs,
                 'funds': funds_qs,
                 'grants': grant_qs}
         serializer = self.get_serializer(data)
         return Response(serializer.data, status.HTTP_200_OK)
+
+    def missing(self, request):
+        context = {'allowed_categories': ['wbs', 'grants', 'funds']}
+        parameter_serializer = MultiGhostDataSerializer(data=request.GET, context=context)
+        parameter_serializer.is_valid(raise_exception=True)
+
+        category = parameter_serializer.data['category']
+
+        if category == 'wbs':
+            queryset = self.get_wbs_queryset()
+            serializer_class = WBSSerializer
+        elif category == 'grants':
+            queryset = self.get_grants_queryset()
+            serializer_class = GrantSerializer
+        elif category == 'funds':
+            queryset = self.get_funds_queryset()
+            serializer_class = FundSerializer
+        else:
+            raise ValueError('Invalid category')
+
+        model = queryset.model
+
+        try:
+            obj = model.admin_objects.get(id=parameter_serializer.data['value'])
+        except ObjectDoesNotExist:
+            return Response({'non_field_errors': ['Invalid PK value']},
+                            status.HTTP_400_BAD_REQUEST)
+
+        serializer = serializer_class(obj, context=self.get_serializer_context())
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def get_wbs_queryset(self):
+        parameter_serializer = WBSGrantFundParameterSerializer(data=self.request.GET,
+                                                               context=self.get_serializer_context())
+        parameter_serializer.is_valid(raise_exception=True)
+
+        business_area = parameter_serializer.validated_data['business_area']
+        return WBS.objects.filter(business_area=business_area).prefetch_related('grants')
+
+    def get_grants_queryset(self):
+        wbs_qs = self.get_wbs_queryset()
+        return Grant.objects.filter(wbs__in=wbs_qs).prefetch_related('funds')
+
+    def get_funds_queryset(self):
+        grant_qs = self.get_grants_queryset()
+        return Fund.objects.filter(grants__in=grant_qs)
