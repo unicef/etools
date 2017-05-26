5fa070eaf774075e957ff85a01b55756b2aba79d
Zoltan Ilcsik <ilcsik@gmail.com>
2017-05-26 19:44:51 +0200

    Use relative path

--- EquiTrack/publics/tasks.py
+++ EquiTrack/publics/tasks.py
@@ -223,9 +223,9 @@ class DSARateUploader(object):
         self.warnings = {}
         self.dsa_rate_upload = dsa_rate_upload
 
-    def read_input_file(self, input_file_path):
+    def read_input_file(self, filename):
         storage = AzureStorage()
-        with storage.open(input_file_path) as input_file:
+        with storage.open("publics/dsa_rate/{}".format(filename)) as input_file:
             utf_8_file = codecs.iterencode(input_file, encoding='utf-8')
             return [dict(r) for r in csv.DictReader(
                                             utf_8_file,
@@ -253,7 +253,7 @@ class DSARateUploader(object):
          'Finalization_Date': '1/2/16',
          'DSA_Eff_Date': '1/3/17'
         """
-        rows = self.read_input_file(self.dsa_rate_upload.dsa_file.path)
+        rows = self.read_input_file(self.dsa_rate_upload.dsa_file.filename)
 
         def process_number(field):
             try:
