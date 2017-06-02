46e521e934a2945245dc72ba518da207b0338a0d
robertavram <robert.avram@gmail.com>
2017-06-02 15:05:42 -0400

    Add api for updating results / country

---
+++ EquiTrack/management/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsSuperUser(BasePermission):
+    """
+    Allows access only to admin users.
+    """
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_superuser
+
