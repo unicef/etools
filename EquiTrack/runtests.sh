de6fca5c44af42663e3d77d167cce2b3a64e9d8d
Vinod Kurup <vinod@kurup.com>
2017-06-15 17:06:22 -0400

    Lower threshold

--- EquiTrack/runtests.sh
+++ EquiTrack/runtests.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -ex
 
+# Ensure there are no missing migrations
+python manage.py makemigrations --dry-run | grep 'No changes detected' || (echo 'There are changes which require migrations.' && exit 1)
+
+# Run unittests and coverage report
 coverage erase
 coverage run manage.py test --noinput --keepdb --settings=EquiTrack.settings.test "$@"
 coverage report
