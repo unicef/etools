26c6088c75126524b7682e95d981e9a0f77578fa
Maxim Lomakin <lom.tik.com@gmail.com>
2017-06-22 19:31:57 +0300

    "Disableable" pagination mixins renamed.

--- EquiTrack/utils/common/pagination.py
+++ EquiTrack/utils/common/pagination.py
@@ -1,7 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 
 
-class DisableablePaginationMixin(object):
+class OptionalPaginationMixin(object):
     def get_page_size(self, request):
         if self.page_size_query_param:
             try:
@@ -9,10 +9,10 @@ class DisableablePaginationMixin(object):
                     return None
             except KeyError:
                 pass
-        return super(DisableablePaginationMixin, self).get_page_size(request)
+        return super(OptionalPaginationMixin, self).get_page_size(request)
 
 
-class DynamicPageNumberPagination(DisableablePaginationMixin, PageNumberPagination):
+class DynamicPageNumberPagination(OptionalPaginationMixin, PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 1000
