e31db233a0cdd6067ce86d1a8839dc39a7a2fd88
Maxim Lomakin <lom.tik.com@gmail.com>
2017-07-06 14:05:14 +0300

    Disallow to write TPM Partner not PME users.

---
+++ EquiTrack/tpm/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+from .models import PME
+
+
+class IsPMEorReadonlyPermission(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS or
+            request.user and
+            PME.as_group() in request.user.groups.all()
+        )
