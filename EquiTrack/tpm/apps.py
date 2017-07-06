e5b31588ac1196945b58d3913541d1166c6f73c7
Roman Karpovich <roman@razortheory.com>
2017-07-06 14:44:20 +0300

    fixed tpm signals

--- EquiTrack/tpm/apps.py
+++ EquiTrack/tpm/apps.py
@@ -8,5 +8,6 @@ class AppConfig(BaseAppConfig):
     verbose_name = 'TPM'
 
     def ready(self):
+        from . import signals
         from utils.permissions import signals
         signals.prepare_permission_choices(self.get_models())
