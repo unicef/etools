e1a1e8765a02357e25918215d8989acbb8f36a2c
Roman Karpovich <roman@razortheory.com>
2017-07-11 17:48:42 +0300

    added url for editing profiles

--- EquiTrack/users/urls_v2.py
+++ EquiTrack/users/urls_v2.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 
 from .views import (
     MyProfileAPIView,
+    UserProfileAPIView,
     CountryView
 )
 
@@ -10,6 +11,7 @@ from .views import (
 urlpatterns = patterns(
     '',
     url(r'^myprofile/$', MyProfileAPIView.as_view(), name="myprofile-detail"),
+    url(r'^profile/(?P<pk>\d+)/$', UserProfileAPIView.as_view(), name="profile-detail"),
     url(r'^country/$', CountryView.as_view(http_method_names=['get']), name="country-detail"),
 
 )
