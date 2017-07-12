7975c2d44c2f99edfa9404755e86660b7ee13991
Philip Semanchuk <psemanchuk@caktusgroup.com>
2017-07-12 13:41:12 -0400

    Add names to URLs so they're reversable.
    Also make file flake8/PEP8 clean.

--- EquiTrack/management/urls.py
+++ EquiTrack/management/urls.py
@@ -1,5 +1,3 @@
-__author__ = 'RobertAvram'
-
 from django.conf.urls import patterns, url
 
 from management.views.reports import (
@@ -17,12 +15,10 @@ from management.views.v1 import (
 
 urlpatterns = patterns(
     '',
-    url(r'^$', PortalDashView.as_view()),
-    url(r'^load-results/$', LoadResultStructure.as_view()),
-    url(r'^invalidate-cache/$', InvalidateCache.as_view()),
-    url(r'^api/stats/usercounts/$', ActiveUsersSection.as_view()),
-    url(r'^api/stats/trips/$', TripsStatisticsView.as_view()),
-    url(r'^api/stats/agreements/$', AgreementsStatisticsView.as_view()),
-
+    url(r'^$', PortalDashView.as_view(), name='dashboard'),
+    url(r'^load-results/$', LoadResultStructure.as_view(), name='load-results'),
+    url(r'^invalidate-cache/$', InvalidateCache.as_view(), name='invalidate-cache'),
+    url(r'^api/stats/usercounts/$', ActiveUsersSection.as_view(), name='stats-usercounts'),
+    url(r'^api/stats/trips/$', TripsStatisticsView.as_view(), name='stats-trips'),
+    url(r'^api/stats/agreements/$', AgreementsStatisticsView.as_view(), name='stats-agreements'),
 )
-
