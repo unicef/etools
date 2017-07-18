447dae24684806eddd6c10293e6ccef30a921926
robertavram <robert.avram@gmail.com>
2017-07-18 13:32:28 -0400

    Tweak niks tasks

--- EquiTrack/partners/tasks.py
+++ EquiTrack/partners/tasks.py
@@ -1,22 +1,18 @@
 from __future__ import unicode_literals, print_function
 
 import datetime
-import time
 
+import itertools
 from django.conf import settings
 from django.db import connection
-from django.db.models import F
+from django.db.models import F, Sum
 from celery.utils.log import get_task_logger
-from EquiTrack.celery import app, send_to_slack
-
-from partners.models import PartnerOrganization, Agreement, Intervention
-from funds.models import FundsReservationHeader
+from EquiTrack.celery import app,
+from partners.models import Agreement, Intervention
 from partners.validation.agreements import AgreementValid
 from partners.validation.interventions import InterventionValid
-from notification.email import send_mail
 from users.models import Country, User
 from notification.models import Notification
-from t2f.models import Travel, TravelActivity, ActionPoint
 
 logger = get_task_logger(__name__)
 
@@ -41,8 +37,7 @@ def task_decorator(funct):
             try:
                 funct(*args, **kwargs)
             except BaseException:
-                #cleanup
-                pass
+                raise
 
     return wrapper
 
@@ -51,20 +46,19 @@ def task_decorator(funct):
 @task_decorator
 def agreement_status_automatic_transition(admin=None, workspace=None, **kwargs):
 
-        logger.info('Starting agreement auto status transition for country {}'.format(
-            workspace.name
-        ))
+        logger.info('Starting agreement auto status transition for country {}'.format(workspace.name))
 
         # these are agreements that are not even valid within their own status
         # compiling a list of them to send to an admin or save somewhere in the future
         bad_agreements = []
 
-        signed_ended_agrs = Agreement.objects.filter(status=Agreement.SIGNED, end__gt=datetime.date.today())
+        # SSFAs don't transition automatically unless they transition based on the intervention.
+        signed_ended_agrs = Agreement.objects.filter(status=Agreement.SIGNED, end__gt=datetime.date.today())\
+            .exclude(agreement_type=Agreement.SSFA)
         processed = 0
 
         for agr in signed_ended_agrs:
             old_status = agr.status
-
             # this function mutates agreement
             validator = AgreementValid(agr, user=admin, disable_rigid_check=True)
             if validator.is_valid:
@@ -74,31 +68,43 @@ def agreement_status_automatic_transition(admin=None, workspace=None, **kwargs):
                     processed += 1
             else:
                 bad_agreements.append(agr)
-                logger.info("Agreement i".format(agr.id), validator.errors)
 
-        logger.info("processed {} agreements".format(processed))
+        # print(bad_agreements)
+        print('Bad agreements {}'.format(len(bad_agreements)))
+        print('Total agreements {}'.format(signed_ended_agrs.count()))
+        logger.info("Transitioned agreements {} ".format(processed))
 
 
 @app.task
 @task_decorator
 def intervention_status_automatic_transition(admin=None, workspace=None, **kwargs):
 
-        logger.info('Starting agreement auto status transition for country {}'.format(
-            workspace.name
-        ))
+        logger.info('Starting agreement auto status transition for country {}'.format(workspace.name))
 
         # these are agreements that are not even valid within their own status
         # compiling a list of them to send to an admin or save somewhere in the future
         bad_interventions = []
 
-        active_ended = Intervention.objects.filter(status=Intervention.ACTIVE,
+        active_ended = Intervention.objects.filter(status__in=Intervention.ACTIVE,
                                                    end=datetime.date.today() - datetime.timedelta(days=1))
+
+        # get all the interventions for which their status is endend and total otustanding_amt is 0 and
+        # actual_amt is the same as the total_amt
+
+        qs = Intervention.objects\
+            .prefetch_related('frs')\
+            .filter(status=Intervention.ENDED)\
+            .annotate(frs_total_outstanding=Sum('frs__outstanding_amt'),
+                      frs_total_actual_amt=Sum('frs__actual_amt'),
+                      frs_total_amt=Sum('frs__total_amt'))\
+            .filter(frs_total_outstanding=0, frs_total_actual_amt=F('frs_total_amt'))
+
         processed = 0
 
-        for intervention in active_ended:
+        for intervention in itertools.chain(active_ended, qs):
             old_status = intervention.status
 
-            # this function mutates agreement
+            # this function mutates the intervention
             validator = InterventionValid(intervention, user=admin, disable_rigid_check=True)
             if validator.is_valid:
                 if intervention.status != old_status:
@@ -107,51 +113,20 @@ def intervention_status_automatic_transition(admin=None, workspace=None, **kwarg
                     processed += 1
             else:
                 bad_interventions.append(intervention)
-                logger.info("Intervention is not valid {}".format(intervention.id), validator.errors)
 
-        logger.info("processed {} agreements".format(processed))
-
-
-@app.task
-@task_decorator
-def intervention_status_closed_automatic_transition(admin=None, workspace=None, **kwargs):
-
-        logger.info('Starting intervention auto status transition for country {}'.format(workspace.name))
-        funds_satisfied = FundsReservationHeader.objects.filter(outstanding_amt=0,
-                                                                actual_amt=F('total_amt'),
-                                                                intervention__status=Intervention.ENDED)
-
-        # TODO add action point query when action points will have a relationship to interventions
-        bad_interventions = []
-        processed = 0
-        for intervention in funds_satisfied:
-            intervention.status = Intervention.closed
-            old_status = intervention.status
-
-            # this function mutates agreement
-            validator = InterventionValid(intervention, user=admin, disable_rigid_check=True)
-            if validator.is_valid:
-                if intervention.status != old_status:
-                    # this one transitioned forward
-                    intervention.save()
-                    processed += 1
-            else:
-                bad_interventions.append(intervention)
-                logger.info("Intervention is not valid {}".format(intervention.id), validator.errors)
-
-        logger.info("processed {} interventions".format(processed))
-
+        logger.info('Bad agreements {}'.format(len(bad_interventions)))
+        logger.info('Total agreements {}'.format(active_ended.count()))
+        logger.info("Transitioned agreements {} ".format(processed))
 
 
 
 @app.task
 @task_decorator
 def intervention_notification_signed_no_frs(admin=None, workspace=None, **kwargs):
-
+    '''This should only run once a week'''
     logger.info('Starting intervention signed but no FRs notifications for country {}'.format(workspace.name))
 
-    signed_interventions = Intervention.objects.filter(document_type__in=[Intervention.PD, Intervention.SHPD],
-                                                       status=Intervention.SIGNED,
+    signed_interventions = Intervention.objects.filter(status=Intervention.SIGNED,
                                                        start__gte=datetime.date.today(),
                                                        frs__isnull=True)
 
@@ -169,12 +144,11 @@ def intervention_notification_signed_no_frs(admin=None, workspace=None, **kwargs
 @app.task
 @task_decorator
 def intervention_notification_ended_fr_outstanding(admin=None, workspace=None, **kwargs):
-
+    '''This will run every 2 weeks'''
     logger.info('Starting intervention signed but FRs Amount and actual '
                 'do not match notifications for country {}'.format(workspace.name))
 
-    ended_interventions = Intervention.objects.filter(document_type__in=[Intervention.PD, Intervention.SHPD],
-                                                      status=Intervention.ENDED)
+    ended_interventions = Intervention.objects.filter(status=Intervention.ENDED)
     for intervention in ended_interventions:
         if intervention.total_frs['total_actual_amt'] != intervention.total_frs['total_frs_amt']:
             email_context = get_intervention_context(intervention)
@@ -190,22 +164,20 @@ def intervention_notification_ended_fr_outstanding(admin=None, workspace=None, *
 @app.task
 @task_decorator
 def intervention_notification_ending(admin=None, workspace=None, **kwargs):
-
+    '''This will run every 24 hours'''
     qs_results = {}
 
     logger.info('Starting interventions almost ending notifications for country {}'.format(workspace.name))
 
     qs_results["30"] = Intervention.objects.filter(
-        document_type__in=[Intervention.PD, Intervention.SHPD],
         status=Intervention.ACTIVE,
         end=datetime.datetime.today()+datetime.timedelta(days=30)
-    )
+    ).prefetch_related('unicef_focal_points', 'agreement', 'agreement__partner')
 
     qs_results["15"] = Intervention.objects.filter(
-        document_type__in=[Intervention.PD, Intervention.SHPD],
         status=Intervention.ACTIVE,
         end=datetime.datetime.today()+datetime.timedelta(days=15)
-    )
+    ).prefetch_related('unicef_focal_points', 'agreement', 'agreement__partner')
 
     for k in qs_results:
         for intervention in qs_results[k]:
