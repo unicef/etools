9ef58ccd2de73684fd0ec118f46017e440123f4f
Roman Karpovich <roman@razortheory.com>
2017-08-03 18:29:45 +0300

    removed unused tpm serializers tests. added tpm partners api tests.

--- EquiTrack/tpm/tests/test_views.py
+++ EquiTrack/tpm/tests/test_views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 
 from EquiTrack.tests.mixins import APITenantTestCase
 from .base import TPMTestCaseMixin
-from .factories import TPMVisitFactory
+from .factories import TPMVisitFactory, TPMPartnerFactory
 
 
 class TestTPMVisitViewSet(TPMTestCaseMixin, APITenantTestCase):
@@ -67,42 +67,33 @@ class TestTPMVisitViewSet(TPMTestCaseMixin, APITenantTestCase):
         self.assertEquals(assign_response.data['tpm_partner'], _('This field is required.'))
 
     def test_assign(self):
-        partner = self.tpm_visit.tpm_partner
-        tpm_activity = self.tpm_visit.tpm_activities.first()
-        tpm_sector = tpm_activity.tpm_sectors.first()
-        tpm_low_result = tpm_sector.tpm_low_results.first()
-        tpm_location = tpm_low_result.tpm_locations.first()
-
         create_response = self.forced_auth_req(
             'post',
             '/api/tpm/visits/',
             user=self.pme_user,
             data={
-                'tpm_partner': partner.id,
-                'tpm_activities': [
-                    {
-                        'tpm_sectors': [
-                            {
-                                'tpm_low_results': [
-                                    {
-                                        'tpm_locations': [
-                                            {
-                                                'location': tpm_location.location.id
-                                            }
-                                        ],
-                                        'result': tpm_low_result.result.id
-                                    }
-                                ],
-                                'sector': tpm_sector.sector.id,
-                            }
-                        ],
-                        'partnership': tpm_activity.partnership.id,
-                        'unicef_focal_points': tpm_activity.unicef_focal_points.values_list('id', flat=True)
-                    }
-                ]
+                'tpm_partner': self.tpm_visit.tpm_partner_id,
+                'tpm_activities': [{
+                    'partnership': activity.partnership_id,
+                    'unicef_focal_points': activity.unicef_focal_points.values_list('id', flat=True),
+                    'tpm_sectors': [{
+                        'sector': sector.sector_id,
+                        'tpm_low_results': [{
+                            'result': low_result.result_id,
+                            'tpm_locations': [{
+                                'location': tpm_location.location_id,
+                                'start_date': tpm_location.start_date.isoformat(),
+                                'end_date': tpm_location.end_date.isoformat(),
+                            } for tpm_location in low_result.tpm_locations.all()]
+                        } for low_result in sector.tpm_low_results.all()]
+                    } for sector in activity.tpm_sectors.all()]
+                } for activity in self.tpm_visit.tpm_activities.all()]
             }
         )
         self.assertEquals(create_response.status_code, status.HTTP_201_CREATED)
+
+        self.assertEqual(create_response.data['start_date'], self.tpm_visit.start_date)
+        self.assertEqual(create_response.data['end_date'], self.tpm_visit.end_date)
 
         assign_response = self.forced_auth_req(
             'post',
@@ -114,7 +105,35 @@ class TestTPMVisitViewSet(TPMTestCaseMixin, APITenantTestCase):
         self.assertEquals(assign_response.status_code, status.HTTP_200_OK)
 
 
-class TestAuditorStaffMembersViewSet(TPMTestCaseMixin, APITenantTestCase):
+class TestTPMFirmViewSet(TPMTestCaseMixin, APITenantTestCase):
+    def setUp(self):
+        super(TestTPMFirmViewSet, self).setUp()
+        self.second_tpm_partner = TPMPartnerFactory()
+
+    def _test_list_view(self, user, expected_firms):
+        response = self.forced_auth_req(
+            'get',
+            '/api/tpm/partners/',
+            user=user
+        )
+
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertListEqual(
+            sorted(map(lambda x: x['id'], response.data['results'])),
+            sorted(map(lambda x: x.id, expected_firms))
+        )
+
+    def test_unicef_list_view(self):
+        self._test_list_view(self.unicef_user, [self.tpm_partner, self.second_tpm_partner])
+
+    def test_auditor_list_view(self):
+        self._test_list_view(self.tpm_user, [self.tpm_partner])
+
+    def test_usual_user_list_view(self):
+        self._test_list_view(self.usual_user, [])
+
+
+class TestTPMStaffMembersViewSet(TPMTestCaseMixin, APITenantTestCase):
     def test_list_view(self):
         response = self.forced_auth_req(
             'get',
