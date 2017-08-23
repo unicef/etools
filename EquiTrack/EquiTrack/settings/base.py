5b6426dece82bdd1dc2f7962ec78afd82d2c5425
Philip Semanchuk <psemanchuk@caktusgroup.com>
2017-08-23 11:41:51 -0400

    No functional change. Change references to django.core.context_processors to django.template.context_processors. The former is gone in Django 1.10.

--- EquiTrack/EquiTrack/settings/base.py
+++ EquiTrack/EquiTrack/settings/base.py
@@ -238,12 +238,12 @@ TEMPLATES = [
 
                 # `allauth` needs this from django
                 'django.contrib.auth.context_processors.auth',
-                'django.core.context_processors.request',
-                'django.core.context_processors.debug',
-                'django.core.context_processors.i18n',
-                'django.core.context_processors.media',
-                'django.core.context_processors.static',
-                'django.core.context_processors.tz',
+                'django.template.context_processors.request',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
