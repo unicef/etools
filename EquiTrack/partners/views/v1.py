3407ef95b19c128a5558685624627d4c26907446
denes csaba <csaba.denes@nordlogic.com>
2017-08-30 20:00:24 +0300

    Revising englist PCA agreement template - work in progress

--- EquiTrack/partners/views/v1.py
+++ EquiTrack/partners/views/v1.py
@@ -125,6 +125,8 @@ class PcaPDFView(PDFTemplateView):
             officers_list.append(
                 {'first_name': officer.first_name,
                  'last_name': officer.last_name,
+                 'last_name': officer.last_name,
+                 'email': officer.email,
                  'title': officer.title}
             )
 
