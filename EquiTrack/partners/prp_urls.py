b86d7827b39b243fa87e233a8c90dd29c7c51c32
Cory Zue <czue@dimagi.com>
2017-09-08 10:13:43 +0200

    hook up list view and tweak url configs

--- EquiTrack/partners/prp_urls.py
+++ EquiTrack/partners/prp_urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from partners.views.prp_v1 import PRPInterventionDetailAPIView
+from partners.views.prp_v1 import PRPInterventionDetailAPIView, PRPInterventionListAPIView
 
 
 urlpatterns = (
+    url(r'^interventions/$',
+        view=PRPInterventionListAPIView.as_view(http_method_names=['get']),
+        name='prp-intervention-list'),
     url(r'^interventions/(?P<pk>\d+)/$',
-        view=PRPInterventionDetailAPIView.as_view(http_method_names=['get', 'patch']),
-        name='intervention-detail'),
+        view=PRPInterventionDetailAPIView.as_view(http_method_names=['get']),
+        name='prp-intervention-detail'),
 )
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'csv'])
