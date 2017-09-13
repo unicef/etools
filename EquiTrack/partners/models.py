5d1883f3b4c73f361cc10d952c9931c6f000eb1f
Marko Bilal <marko.barca10@gmail.com>
2017-09-13 18:09:18 -0400

    spot checks fix

--- EquiTrack/partners/models.py
+++ EquiTrack/partners/models.py
@@ -608,7 +608,7 @@ class PartnerOrganization(AdminURLMixin, models.Model):
                 travel_type=TravelType.SPOT_CHECK,
                 travels__traveler=F('primary_traveler'),
                 travels__status__in=[Travel.COMPLETED],
-                travels__completed_at__in=datetime.datetime.now().year(),
+                travels__completed_at__year=datetime.datetime.now().year(),
                 partner=partner,
             ).count() or 0
 
