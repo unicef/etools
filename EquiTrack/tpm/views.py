580daccda5dba943b694e4f07c7b100ca2837efe
Maxim Lomakin <lom.tik.com@gmail.com>
2017-09-26 18:06:55 +0300

    Added missed columns to tpm visit export.

--- EquiTrack/tpm/views.py
+++ EquiTrack/tpm/views.py
@@ -90,7 +90,7 @@ class TPMPartnerViewSet(
         tpm_partners = TPMPartner.objects.all().order_by('vendor_number')
         serializer = TPMPartnerExportSerializer(tpm_partners, many=True)
         return Response(serializer.data, headers={
-            'Content-Disposition': 'attachment;filename=tpm_vendors_{}.csv'.format(timezone.now())
+            'Content-Disposition': 'attachment;filename=tpm_vendors_{}.csv'.format(timezone.now().date())
         })
 
 
@@ -177,7 +177,7 @@ class TPMVisitViewSet(
         ).order_by('tpm_visit', 'id')
         serializer = TPMActivityExportSerializer(tpm_activities, many=True)
         return Response(serializer.data, headers={
-            'Content-Disposition': 'attachment;filename=tpm_attachments_{}.csv'.format(timezone.now())
+            'Content-Disposition': 'attachment;filename=tpm_attachments_{}.csv'.format(timezone.now().date())
         })
 
     @list_route(methods=['get'], url_path='locations/export', renderer_classes=(TPMLocationCSVRenderer,))
@@ -189,7 +189,7 @@ class TPMVisitViewSet(
         ).order_by('tpmactivity__tpm_visit', 'tpmactivity', 'id')
         serializer = TPMLocationExportSerializer(tpm_locations, many=True)
         return Response(serializer.data, headers={
-            'Content-Disposition': 'attachment;filename=tpm_locations_{}.csv'.format(timezone.now())
+            'Content-Disposition': 'attachment;filename=tpm_locations_{}.csv'.format(timezone.now().date())
         })
 
     @detail_route(methods=['get'])
