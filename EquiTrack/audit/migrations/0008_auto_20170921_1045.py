6c87ceb4911bdf719d94e17a40a9d600697298d9
Roman Karpovich <roman@razortheory.com>
2017-10-06 12:21:57 +0300

    reverted back 0006 migration. data migration for action points choice renaming

--- EquiTrack/audit/migrations/0008_auto_20170921_1045.py
+++ EquiTrack/audit/migrations/0008_auto_20170921_1045.py
@@ -6,6 +6,16 @@ import django_fsm
 from django.db import migrations, models
 
 
+def update_action_points_choices(apps, schema_editor):
+    apps.get_model('audit', 'EngagementActionPoint').objects\
+        .filter(description='Invoice and recieve reimbursement of ineligible expenditure')\
+        .update(description='Invoice and receive reimbursement of ineligible expenditure')
+
+
+def do_nothing(*args):
+    pass
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -63,4 +73,10 @@ class Migration(migrations.Migration):
             name='status',
             field=django_fsm.FSMField(choices=[('partner_contacted', 'IP Contacted'), ('report_submitted', 'Report Submitted'), ('final', 'Final Report'), ('cancelled', 'Cancelled')], default='partner_contacted', max_length=30, protected=True, verbose_name='status'),
         ),
+        migrations.AlterField(
+            model_name='engagementactionpoint',
+            name='description',
+            field=models.CharField(choices=[('Invoice and receive reimbursement of ineligible expenditure', 'Invoice and receive reimbursement of ineligible expenditure'), ('Change cash transfer modality (DCT, reimbursement or direct payment)', 'Change cash transfer modality (DCT, reimbursement or direct payment)'), ('IP to incur and report on additional expenditure', 'IP to incur and report on additional expenditure'), ('Review and amend ICE or budget', 'Review and amend ICE or budget'), ('IP to correct FACE form or Statement of Expenditure', 'IP to correct FACE form or Statement of Expenditure'), ('Schedule a programmatic visit', 'Schedule a programmatic visit'), ('Schedule a follow-up spot check', 'Schedule a follow-up spot check'), ('Schedule an audit', 'Schedule an audit'), ('Block future cash transfers', 'Block future cash transfers'), ('Block or mark vendor for deletion', 'Block or mark vendor for deletion'), ('Escalate to Chief of Operations, Dep Rep, or Rep', 'Escalate to Chief of Operations, Dep Rep, or Rep'), ('Escalate to Investigation', 'Escalate to Investigation'), ('Capacity building / Discussion with partner', 'Capacity building / Discussion with partner'), ('Other', 'Other')], max_length=100),
+        ),
+        migrations.RunPython(update_action_points_choices, do_nothing),
     ]
