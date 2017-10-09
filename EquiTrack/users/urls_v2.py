3ee160ade10bd2ba7e3f2c9305f186e754413611
denes csaba <csaba.denes@nordlogic.com>
2017-10-09 17:28:49 +0300

    workspaces list API - change how the URL is defined

--- EquiTrack/users/urls_v2.py
+++ EquiTrack/users/urls_v2.py
@@ -8,9 +8,6 @@ from .views import (
 
 
 urlpatterns = (
-    # list all of the countries
-    url(r'^workspaces/$', CountriesViewSet.as_view(http_method_names=['get']), name="list-workspaces"),
-
     url(r'^myprofile/$', MyProfileAPIView.as_view(), name="myprofile-detail"),
     url(r'^country/$', CountryView.as_view(http_method_names=['get']), name="country-detail"),
 )
