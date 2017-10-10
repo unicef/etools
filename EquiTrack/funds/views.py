307b4803986da4afe820154926d9c89465ba5cc9
Greg Reinbach <greg@reinbach.com>
2017-10-10 12:57:31 -0400

    Add FundsReserverationHeaders list API endpoint
    New endpoint that does not require value query parameter
    Includes csv, and csv_flat format exports

--- EquiTrack/funds/views.py
+++ EquiTrack/funds/views.py
@@ -1,14 +1,30 @@
+import operator
+import functools
+
 from django.utils.translation import ugettext as _
 from django.db.models import Q
 from rest_framework import (
     permissions,
     status
 )
+from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_csv import renderers as r
 
-from .models import FundsReservationHeader
-from .serializers import FRsSerializer
+from funds.models import FundsReservationHeader
+from funds.renderers import (
+    FundsReservationHeaderCsvRenderer,
+    FundsReservationHeaderCsvFlatRenderer,
+)
+from funds.serializers import (
+    FRHeaderSerializer,
+    FRsSerializer,
+    FundsReservationHeaderExportFlatSerializer,
+    FundsReservationHeaderExportSerializer,
+)
+from partners.filters import PartnerScopeFilter
+from partners.permissions import PartneshipManagerPermission
 
 
 class FRsView(APIView):
@@ -41,3 +57,47 @@ class FRsView(APIView):
         serializer = FRsSerializer(qs)
 
         return Response(serializer.data)
+
+
+class FundsReservationHeaderListAPIView(ListAPIView):
+    """
+    Returns a list of FundsReservationHeaders.
+    """
+    serializer_class = FRHeaderSerializer
+    permission_classes = (PartneshipManagerPermission,)
+    filter_backends = (PartnerScopeFilter,)
+    renderer_classes = (
+        r.JSONRenderer,
+        FundsReservationHeaderCsvRenderer,
+        FundsReservationHeaderCsvFlatRenderer,
+    )
+
+    def get_serializer_class(self):
+        """
+        Use different serilizers for methods
+        """
+        query_params = self.request.query_params
+        if "format" in query_params.keys():
+            if query_params.get("format") == 'csv':
+                return FundsReservationHeaderExportSerializer
+            if query_params.get("format") == 'csv_flat':
+                return FundsReservationHeaderExportFlatSerializer
+        return super(FundsReservationHeaderListAPIView, self).get_serializer_class()
+
+    def get_queryset(self, format=None):
+        q = FundsReservationHeader.objects.all()
+        query_params = self.request.query_params
+
+        if query_params:
+            queries = []
+            if "search" in query_params.keys():
+                queries.append(
+                    Q(intervention__number__icontains=query_params.get("search")) |
+                    Q(vendor_code__icontains=query_params.get("search")) |
+                    Q(fr_number__icontains=query_params.get("search"))
+                )
+            if queries:
+                expression = functools.reduce(operator.and_, queries)
+                q = q.filter(expression)
+
+        return q
