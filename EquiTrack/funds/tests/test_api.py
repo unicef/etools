41c676ba1e5c7145701f577f19ee993d348f9e6a
Greg Reinbach <greg@reinbach.com>
2017-10-10 13:51:17 -0400

    Add FundsReservationItem list api endpoint
    Includes csv, and csv_flat format export

--- EquiTrack/funds/tests/test_api.py
+++ EquiTrack/funds/tests/test_api.py
@@ -8,7 +8,11 @@ from rest_framework import status
 from tablib.core import Dataset
 from unittest import TestCase
 
-from EquiTrack.factories import FundsReservationHeaderFactory, UserFactory
+from EquiTrack.factories import (
+    FundsReservationHeaderFactory,
+    FundsReservationItemFactory,
+    UserFactory,
+)
 from EquiTrack.tests.mixins import APITenantTestCase, URLAssertionMixin
 
 
@@ -19,6 +23,7 @@ class UrlsTestCase(URLAssertionMixin, TestCase):
         names_and_paths = (
             ('frs', 'frs/', {}),
             ('funds-reservation-header', 'reservation-header/', {}),
+            ('funds-reservation-item', 'reservation-item/', {}),
         )
         self.assertReversal(names_and_paths, 'funds:', '/api/v2/funds/')
 
@@ -122,4 +127,101 @@ class TestFundsReservationHeaderExportList(APITenantTestCase):
             u"{}".format(self.frs.outstanding_amt),
             u"{}".format(self.frs.start_date),
             u"{}".format(self.frs.end_date),
+        ))
+
+
+class TestFundsReservationItemExportList(APITenantTestCase):
+    def setUp(self):
+        super(TestFundsReservationItemExportList, self).setUp()
+        self.unicef_staff = UserFactory(is_staff=True)
+        self.frs = FundsReservationHeaderFactory()
+        self.item = FundsReservationItemFactory(
+            fund_reservation=self.frs
+        )
+
+    def test_invalid_format_export_api(self):
+        response = self.forced_auth_req(
+            'get',
+            '/api/v2/funds/reservation-item/',
+            user=self.unicef_staff,
+            data={"format": "unknown"},
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_csv_export_api(self):
+        response = self.forced_auth_req(
+            'get',
+            '/api/v2/funds/reservation-item/',
+            user=self.unicef_staff,
+            data={"format": "csv"},
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        dataset = Dataset().load(response.content, 'csv')
+        self.assertEqual(dataset.height, 1)
+        self.assertEqual(dataset._get_headers(), [
+            "Reference Number",
+            "Fund Reservation Number",
+            "Item Number",
+            "Line Item",
+            "Description",
+            "WBS",
+            "Grant Number",
+            "Fund",
+            "Overall Amount",
+            "Overall Amount DC",
+            "Due Date",
+        ])
+        self.assertEqual(dataset[0], (
+            u"{}".format(self.frs.intervention.pk),
+            unicode(self.frs.pk),
+            u"{}".format(self.item.fr_ref_number),
+            u"{}".format(self.item.line_item),
+            u"",
+            u"",
+            u"",
+            u"",
+            u"{0:.2f}".format(self.item.overall_amount),
+            u"{0:.2f}".format(self.item.overall_amount_dc),
+            u"",
+        ))
+
+    def test_csv_flat_export_api(self):
+        response = self.forced_auth_req(
+            'get',
+            '/api/v2/funds/reservation-item/',
+            user=self.unicef_staff,
+            data={"format": "csv_flat"},
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        dataset = Dataset().load(response.content, 'csv')
+        self.assertEqual(dataset.height, 1)
+        self.assertEqual(dataset._get_headers(), [
+            "Id",
+            "Reference Number",
+            "Fund Reservation Number",
+            "Item Number",
+            "Line Item",
+            "Description",
+            "WBS",
+            "Grant Number",
+            "Fund",
+            "Overall Amount",
+            "Overall Amount DC",
+            "Due Date",
+        ])
+        self.assertEqual(dataset[0], (
+            u"{}".format(self.item.pk),
+            u"{}".format(self.frs.intervention.number),
+            unicode(self.frs.fr_number),
+            u"{}".format(self.item.fr_ref_number),
+            u"{}".format(self.item.line_item),
+            u"",
+            u"",
+            u"",
+            u"",
+            u"{0:.2f}".format(self.item.overall_amount),
+            u"{0:.2f}".format(self.item.overall_amount_dc),
+            u"",
         ))
--- EquiTrack/funds/urls.py
+++ EquiTrack/funds/urls.py
@@ -2,11 +2,18 @@ from __future__ import unicode_literals
 
 from django.conf.urls import url
 
-from views import FRsView, FundsReservationHeaderListAPIView
+from views import (
+    FRsView,
+    FundsReservationHeaderListAPIView,
+    FundsReservationItemListAPIView
+)
 
 urlpatterns = (
     url(r'^frs/$', view=FRsView.as_view(), name='frs'),
     url(r'^reservation-header/$',
         view=FundsReservationHeaderListAPIView.as_view(),
         name='funds-reservation-header'),
+    url(r'^reservation-item/$',
+        view=FundsReservationItemListAPIView.as_view(),
+        name='funds-reservation-item'),
 )
