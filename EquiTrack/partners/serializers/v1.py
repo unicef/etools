16e270dc7c448761be8f79f1ae2c883927fdff68
Philip Semanchuk <psemanchuk@caktusgroup.com>
2017-10-18 09:22:46 -0400

    remove a couple more unused serializers

--- EquiTrack/partners/serializers/v1.py
+++ EquiTrack/partners/serializers/v1.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 
-from locations.models import Location
-from reports.models import LowerResult
 from partners.models import FileType
 
 
@@ -12,24 +10,3 @@ class FileTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = FileType
         fields = '__all__'
-
-
-class LocationSerializer(serializers.Serializer):
-
-    latitude = serializers.CharField(source='geo_point.y')
-    longitude = serializers.CharField(source='geo_point.x')
-    location_name = serializers.CharField(source='name')
-    location_type = serializers.CharField(source='gateway.name')
-    gateway_id = serializers.CharField(source='gateway.id')
-    p_code = serializers.CharField()
-
-    class Meta:
-        model = Location
-        fields = '__all__'
-
-
-class LowerOutputStructuredSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = LowerResult
-        fields = ('id', 'name')
