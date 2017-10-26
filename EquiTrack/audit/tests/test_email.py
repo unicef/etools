80bd61b25665853333560fa695c3af5d3c537a16
Philip Semanchuk <psemanchuk@caktusgroup.com>
2017-10-26 16:25:36 -0400

    Add a test to challenge the loading of fixtures in the shared schema

---
+++ EquiTrack/audit/tests/test_email.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from post_office.models import EmailTemplate
+
+from EquiTrack.tests.mixins import FastTenantTestCase
+
+
+class TestEmail(FastTenantTestCase):
+    fixtures = ('emails.json', )
+
+    def test_expected_email_templates_exist(self):
+        '''Ensure the email templates for this app exist and have content'''
+        for name in ('audit/engagement/submit_to_auditor',
+                     'audit/engagement/reported_by_auditor',
+                     'audit/engagement/action_point_assigned', ):
+            q = EmailTemplate.objects.filter(name=name)
+            # There's a migration that creates these EmailTemplate objects, but with empty content. They're pretty
+            # useless, so I want to ensure the fixture versions (with non-null content) were created.
+            q = q.exclude(content__isnull=True).exclude(content__exact='')
+            self.assertTrue(q.exists())
