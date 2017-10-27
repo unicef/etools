e5e5492687d021a13fd6ba2258fb0752e446f6e1
Greg Reinbach <greg@reinbach.com>
2017-10-27 13:28:32 -0400

    Add relevant __future__ imports

--- EquiTrack/snapshot/tests/test_utils.py
+++ EquiTrack/snapshot/tests/test_utils.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 from EquiTrack.tests.mixins import FastTenantTestCase as TenantTestCase
 from EquiTrack.factories import (
     FundsReservationHeaderFactory,
