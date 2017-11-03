df56d12f7cc69ef014be5293607a6e60bbc1f033
Vinod Kurup <vinod@kurup.com>
2017-11-02 21:11:43 -0400

    Future-proof imports

--- EquiTrack/features/urls.py
+++ EquiTrack/features/urls.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 from django.conf.urls import url
 
 from features.views import get_flags
