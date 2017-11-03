d17edc2cb270fb3bb9e8da73b01ea507c8353d10
Greg Reinbach <greg@reinbach.com>
2017-11-03 15:34:15 -0400

    Add pmp view tests

---
+++ EquiTrack/partners/tests/test_api_pmp.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from unittest import TestCase
+
+from EquiTrack.factories import UserFactory
+from EquiTrack.tests.mixins import APITenantTestCase, URLAssertionMixin
+
+
+class URLsTestCase(URLAssertionMixin, TestCase):
+    '''Simple test case to verify URL reversal'''
+    def test_urls(self):
+        '''Verify URL pattern names generate the URLs we expect them to.'''
+        names_and_paths = (
+            ('dropdown-pmp-list', 'pmp/', {}),
+            ('dropdown-static-list', 'static/', {}),
+        )
+        self.assertReversal(
+            names_and_paths,
+            'partners_api:',
+            '/api/v2/dropdowns/'
+        )
+        self.assertIntParamRegexes(names_and_paths, 'partners_api:')
+
+
+class TestPMPStaticDropdownsListApiView(APITenantTestCase):
+    def setUp(self):
+        self.user = UserFactory(is_staff=True)
+        self.url = reverse("partners_api:dropdown-static-list")
+
+    def test_get(self):
+        response = self.forced_auth_req('get', self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
