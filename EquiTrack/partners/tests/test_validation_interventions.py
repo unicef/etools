d3fb14e6e51caf11bc6a66c70a15637b448af752
Greg Reinbach <greg@reinbach.com>
2017-11-09 10:32:34 -0500

    Add more intervention validation tests

--- EquiTrack/partners/tests/test_validation_interventions.py
+++ EquiTrack/partners/tests/test_validation_interventions.py
@@ -5,19 +5,31 @@ from __future__ import unicode_literals
 
 import datetime
 
+from mock import patch, Mock
+
 from EquiTrack.factories import (
     AgreementFactory,
+    InterventionAmendmentFactory,
     FundsReservationHeaderFactory,
     GroupFactory,
     InterventionAttachmentFactory,
     InterventionFactory,
+    PartnerStaffFactory,
     UserFactory,
 )
 from EquiTrack.tests.mixins import FastTenantTestCase
-from EquiTrack.validation_mixins import TransitionError
-from partners.models import Agreement, FileType, Intervention
+from EquiTrack.validation_mixins import BasicValidationError, TransitionError
+from partners.models import (
+    Agreement,
+    FileType,
+    Intervention,
+    InterventionAmendment,
+)
 from partners.validation.interventions import (
+    amendments_valid,
     partnership_manager_only,
+    signed_date_valid,
+    ssfa_agreement_has_no_other_intervention,
     start_date_related_agreement_valid,
     start_date_signed_valid,
     transition_ok,
@@ -343,3 +355,218 @@ class TestStateDateRelatedAgreementValid(FastTenantTestCase):
             document_type=Intervention.PD,
         )
         self.assertTrue(start_date_related_agreement_valid(intervention))
+
+
+class TestSignedDateValid(FastTenantTestCase):
+    def setUp(self):
+        super(TestSignedDateValid, self).setUp()
+        self.unicef_user = UserFactory()
+        self.partner_user = PartnerStaffFactory()
+        self.future_date = datetime.date.today() + datetime.timedelta(days=2)
+
+    def test_valid(self):
+        """Valid if nothing signed"""
+        intervention = Intervention()
+        self.assertTrue(signed_date_valid(intervention))
+
+    def test_valid_unicef_signed(self):
+        """Valid if signed unicef date and unicef signatory
+        and date prior to today
+        """
+        intervention = Intervention(
+            signed_by_unicef_date=datetime.date(2001, 1, 1),
+            unicef_signatory=self.unicef_user,
+        )
+        self.assertTrue(signed_date_valid(intervention))
+
+    def test_invalid_unicef_signed(self):
+        """invalid if signed unicef date and unicef signatory
+        and date after to today
+        """
+        intervention = Intervention(
+            signed_by_unicef_date=self.future_date,
+            unicef_signatory=self.unicef_user,
+        )
+        self.assertFalse(signed_date_valid(intervention))
+
+    def test_valid_partner_signed(self):
+        """Valid if signed partner date and partner signatory
+        and date prior to today
+        """
+        intervention = Intervention(
+            signed_by_partner_date=datetime.date(2001, 1, 1),
+            partner_authorized_officer_signatory=self.partner_user,
+        )
+        self.assertTrue(signed_date_valid(intervention))
+
+    def test_invalid_partner_signed(self):
+        """invalid if signed partner date and partner signatory
+        and date after to today
+        """
+        intervention = Intervention(
+            signed_by_partner_date=self.future_date,
+            partner_authorized_officer_signatory=self.partner_user,
+        )
+        self.assertFalse(signed_date_valid(intervention))
+
+    def test_valid_all_signed(self):
+        """Valid if both signed unicef/partner date and
+        unicef/partner signatory and date prior to today
+        """
+        intervention = Intervention(
+            signed_by_unicef_date=datetime.date(2001, 1, 1),
+            unicef_signatory=self.unicef_user,
+            signed_by_partner_date=datetime.date(2001, 1, 1),
+            partner_authorized_officer_signatory=self.partner_user,
+        )
+        self.assertTrue(signed_date_valid(intervention))
+
+    def test_invalid_all_signed_unicef_late(self):
+        """invalid if both signed unicef/partner date and
+        unicef/partner signatory and unicef date after to today
+        """
+        intervention = Intervention(
+            signed_by_unicef_date=self.future_date,
+            unicef_signatory=self.unicef_user,
+            signed_by_partner_date=datetime.date(2001, 1, 1),
+            partner_authorized_officer_signatory=self.partner_user,
+        )
+        self.assertFalse(signed_date_valid(intervention))
+
+    def test_invalid_all_signed_partner_late(self):
+        """invalid if both signed unicef/partner date and
+        unicef/partner signatory and partner date after to today
+        """
+        intervention = Intervention(
+            signed_by_unicef_date=datetime.date(2001, 1, 1),
+            unicef_signatory=self.unicef_user,
+            signed_by_partner_date=self.future_date,
+            partner_authorized_officer_signatory=self.partner_user,
+        )
+        self.assertFalse(signed_date_valid(intervention))
+
+    def test_invalid_all_signed_both_late(self):
+        """invalid if both signed unicef/partner date and
+        unicef/partner signatory and unicef/partner date after to today
+        """
+        intervention = Intervention(
+            signed_by_unicef_date=self.future_date,
+            unicef_signatory=self.unicef_user,
+            signed_by_partner_date=self.future_date,
+            partner_authorized_officer_signatory=self.partner_user,
+        )
+        self.assertFalse(signed_date_valid(intervention))
+
+
+class TestAmendmentsInvalid(FastTenantTestCase):
+    def setUp(self):
+        super(TestAmendmentsInvalid, self).setUp()
+        self.intervention = InterventionFactory(
+            status=Intervention.DRAFT,
+        )
+        self.intervention.old_instance = self.intervention
+        self.amendment = InterventionAmendmentFactory(
+            intervention=self.intervention,
+            signed_date=datetime.date(2001, 1, 1),
+            signed_amendment="random.pdf",
+        )
+
+    def test_valid(self):
+        self.assertTrue(amendments_valid(self.intervention))
+
+    def test_change_invalid(self):
+        """If not active/signed and amendment changes then invalid"""
+        self.amendment.signed_date = datetime.date.today()
+        self.amendment.save()
+        mock_check = Mock(return_value=False)
+        with patch(
+                "partners.validation.interventions.check_rigid_related",
+                mock_check
+        ):
+            self.assertFalse(amendments_valid(self.intervention))
+
+    def test_change_active_valid(self):
+        """If active and amendment changes then valid"""
+        self.intervention.status = Intervention.ACTIVE
+        self.amendment.signed_date = datetime.date.today()
+        self.amendment.save()
+        self.assertTrue(amendments_valid(self.intervention))
+
+    def test_change_signed_valid(self):
+        """If signed and amendment changes then valid"""
+        self.intervention.status = Intervention.SIGNED
+        self.amendment.signed_date = datetime.date.today()
+        self.assertTrue(amendments_valid(self.intervention))
+
+    def test_other_no_description(self):
+        """If amendment is type other, and no other description then invalid"""
+        self.amendment.types = [InterventionAmendment.OTHER]
+        self.amendment.other_description = None
+        self.amendment.save()
+        self.assertIsNone(self.amendment.other_description)
+        self.assertFalse(amendments_valid(self.intervention))
+
+    def test_other_with_description(self):
+        """If amendment is type other, and no other description then valid"""
+        self.amendment.types = [InterventionAmendment.OTHER]
+        self.amendment.other_description = "Description"
+        self.amendment.save()
+        self.assertIsNotNone(self.amendment.other_description)
+        self.assertTrue(amendments_valid(self.intervention))
+
+    def test_no_signed_date(self):
+        """If amendment has no signed date then invalid"""
+        self.amendment.signed_date = None
+        self.amendment.save()
+        self.assertFalse(amendments_valid(self.intervention))
+
+    def test_no_signed_amendment(self):
+        """If amendment has no signed amendment then invalid"""
+        self.amendment.signed_amendment = None
+        self.amendment.save()
+        self.assertFalse(amendments_valid(self.intervention))
+
+
+class TestSSFAgreementHasNoOtherIntervention(FastTenantTestCase):
+    def setUp(self):
+        super(TestSSFAgreementHasNoOtherIntervention, self).setUp()
+        self.agreement = AgreementFactory(
+            agreement_type=Agreement.SSFA,
+        )
+        self.intervention = InterventionFactory(
+            document_type=Intervention.SSFA,
+            agreement=self.agreement,
+        )
+
+    def test_valid(self):
+        """If document type not SSFA, then valid"""
+        self.intervention.document_type = Intervention.PD
+        self.assertTrue(
+            ssfa_agreement_has_no_other_intervention(self.intervention)
+        )
+
+    def test_ssfa_valid(self):
+        """If document type SSFA, and agreement type is SSFA ensure
+        agreement interventions count is <= 1
+        """
+        self.assertTrue(
+            ssfa_agreement_has_no_other_intervention(self.intervention)
+        )
+
+    def test_ssfa_invalid(self):
+        """If document type SSFA, and agreement type is SSFA invalid
+        if agreement interventions count is > 1
+        """
+        InterventionFactory(agreement=self.agreement)
+        self.assertFalse(
+            ssfa_agreement_has_no_other_intervention(self.intervention)
+        )
+
+    def test_agreement_not_ssfa(self):
+        """If document type SSFA, and agreement is not then invalid"""
+        self.agreement.agreement_type = Agreement.MOU
+        with self.assertRaisesRegexp(
+                BasicValidationError,
+                "Agreement selected is not of type SSFA"
+        ):
+            ssfa_agreement_has_no_other_intervention(self.intervention)
