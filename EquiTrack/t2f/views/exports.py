880e7d136f537282618ec8adfb92ef0b2ec56ab9
Marko Bilal <marko.barca10@gmail.com>
2017-11-09 15:43:24 -0500

    removed prints

--- EquiTrack/t2f/views/exports.py
+++ EquiTrack/t2f/views/exports.py
@@ -49,6 +49,7 @@ class TravelActivityExport(ExportBaseView):
                 dto_list.append(self.SimpleDTO(travel, activity))
 
         serializer = self.get_serializer(dto_list, many=True)
+
         response = Response(data=serializer.data, status=status.HTTP_200_OK)
         response['Content-Disposition'] = 'attachment; filename="TravelActivityExport.csv"'
         return response
