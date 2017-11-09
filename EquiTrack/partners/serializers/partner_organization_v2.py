43a69542582ea2b6b571203fac69c56d90413901
Domenico DiNicola <domenico.dinicola@wfp.org>
2017-11-09 16:37:27 -0500

    fixed #758 changed partner organization serializer null message

--- EquiTrack/partners/serializers/partner_organization_v2.py
+++ EquiTrack/partners/serializers/partner_organization_v2.py
@@ -215,6 +215,13 @@ class PartnerOrganizationCreateUpdateSerializer(SnapshotModelSerializer):
     class Meta:
         model = PartnerOrganization
         fields = "__all__"
+        extra_kwargs = {
+            "partner_type": {
+                "error_messages": {
+                    "null": u'Vendor record must belong to PRG2 account group (start from 2500 series)'
+                }
+            }
+        }
 
 
 class PartnerOrganizationHactSerializer(serializers.ModelSerializer):
