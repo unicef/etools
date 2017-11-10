14cb288f597fb27f3b732454d1421b8ed84a518e
Maxim Lomakin <lom.tik.com@gmail.com>
2017-11-10 16:25:54 +0300

    Restructure audit migrations.

--- EquiTrack/audit/migrations/0014_auto_20171020_0849.py
+++ EquiTrack/audit/migrations/0014_auto_20171020_0849.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('audit', '0013_merge'),
+        ('audit', '0013_auto_20171018_1345'),
     ]
 
     operations = [
