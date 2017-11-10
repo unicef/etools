cc7c0aed6ad19d9f189cda7ab36d1afd66ab06bd
Maxim Lomakin <lom.tik.com@gmail.com>
2017-11-10 18:14:36 +0300

    Removed choices duplication with partner module.

--- EquiTrack/audit/models.py
+++ EquiTrack/audit/models.py
@@ -28,7 +28,7 @@ from audit.transitions.conditions import (
 from audit.transitions.serializers import EngagementCancelSerializer
 from EquiTrack.utils import get_environment
 from firms.models import BaseFirm, BaseStaffMember
-from partners.models import PartnerStaffMember
+from partners.models import PartnerStaffMember, PartnerOrganization
 from utils.common.models.fields import CodedGenericRelation
 from utils.common.urlresolvers import build_frontend_url
 from utils.groups.wrappers import GroupWrapper
@@ -148,32 +148,6 @@ class Engagement(TimeStampedModel, models.Model):
         DISPLAY_STATUSES.cancelled: 'date_of_cancel'
     }
 
-    SHARED_IP_WITH_CHOICES = Choices(
-        _('DPKO'),
-        _('ECA'),
-        _('ECLAC'),
-        _('ESCWA'),
-        _('FAO'),
-        _('ILO'),
-        _('IOM'),
-        _('OHCHR'),
-        _('UN Women'),
-        _('UNAIDS'),
-        _('UNDP'),
-        _('UNESCO'),
-        _('UNFPA'),
-        _('UN - Habitat'),
-        _('UNHCR'),
-        _('UNODC'),
-        _('UNOPS'),
-        _('UNRWA'),
-        _('UNSC'),
-        _('UNU'),
-        _('WB'),
-        _('WFP'),
-        _('WHO'),
-    )
-
     status = FSMField(_('status'), max_length=30, choices=STATUSES, default=STATUSES.partner_contacted, protected=True)
 
     # auditor - partner organization from agreement
@@ -215,7 +189,8 @@ class Engagement(TimeStampedModel, models.Model):
     )
 
     joint_audit = models.BooleanField(_('Joint Audit'), default=False, blank=True)
-    shared_ip_with = models.CharField(_('Shared IP with'), max_length=12, choices=SHARED_IP_WITH_CHOICES, blank=True)
+    shared_ip_with = models.CharField(_('Shared IP with'), max_length=20, choices=PartnerOrganization.AGENCY_CHOICES,
+                                      blank=True)
 
     staff_members = models.ManyToManyField(AuditorStaffMember, verbose_name=_('staff members'))
 
