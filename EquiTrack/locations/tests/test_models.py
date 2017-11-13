4d30f17714d7bbe3099231a0a217d00ac4f7d4fb
Domenico DiNicola <domenico.dinicola@wfp.org>
2017-11-13 10:57:50 -0500

    removed command

--- EquiTrack/locations/tests/test_models.py
+++ EquiTrack/locations/tests/test_models.py
@@ -1,16 +1,9 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import sys
 from unittest import skipIf, TestCase
 
-from EquiTrack.factories import (
-    CartoDBTableFactory,
-    GatewayTypeFactory,
-    LocationFactory,
-    )
+from EquiTrack.factories import CartoDBTableFactory, GatewayTypeFactory, LocationFactory
 
 
 @skipIf(sys.version_info.major == 3, "This test can be deleted under Python 3")
