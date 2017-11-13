5590477ffd652e4c59d411de0d8e52ce5ae8458e
Greg Reinbach <greg@reinbach.com>
2017-11-13 09:42:12 -0500

    Add tests for UserMapper methods
    - _set_special_attr
    - _set_attribute

--- EquiTrack/users/tests/test_tasks.py
+++ EquiTrack/users/tests/test_tasks.py
@@ -6,10 +6,15 @@ from __future__ import unicode_literals
 from tenant_schemas.utils import schema_context
 from unittest import skip
 
-from EquiTrack.factories import CountryFactory, GroupFactory, SectionFactory
+from EquiTrack.factories import (
+    CountryFactory,
+    GroupFactory,
+    SectionFactory,
+    ProfileFactory,
+)
 from EquiTrack.tests.mixins import SCHEMA_NAME, FastTenantTestCase
 from users import tasks
-from users.models import Section
+from users.models import Section, UserProfile
 
 
 class TestUserMapper(FastTenantTestCase):
@@ -33,12 +38,12 @@ class TestUserMapper(FastTenantTestCase):
         self.assertEqual(res, country_uat)
         self.assertEqual(self.mapper.countries, {"UAT": country_uat})
 
-    @skip("UAT country not found?!?!")
     def test_get_country_business_area_code(self):
         """Check that we get country that matches business area code"""
         area_code = "10101"
         with schema_context(SCHEMA_NAME):
             country_uat = CountryFactory(name="UAT")
+            self.mapper.countries = {"UAT": country_uat}
             country = CountryFactory(business_area_code=area_code)
             res = self.mapper._get_country(area_code)
         self.assertEqual(res, country)
@@ -112,3 +117,85 @@ class TestUserMapper(FastTenantTestCase):
         res = self.mapper._set_simple_attr(section, "name", "Change")
         self.assertTrue(res)
         self.assertEqual(section.name, "Change")
+
+    def test_set_special_attr_not_country(self):
+        """Return False if attr is not country"""
+        section = Section(name="Name")
+        res = self.mapper._set_special_attr(section, "name", "Change")
+        self.assertFalse(res)
+        self.assertEqual(section.name, "Name")
+
+    def test_set_special_attr_country_override(self):
+        """If country attribute, but override is set then False"""
+        country = CountryFactory()
+        profile = UserProfile(country_override=country)
+        res = self.mapper._set_special_attr(profile, "country", "Change")
+        self.assertFalse(res)
+
+    def test_set_special_attr_country_match(self):
+        """If country attribute matches, then False"""
+        code = "test"
+        country = CountryFactory(code=code)
+        profile = UserProfile(country=country)
+        self.mapper.countries = {code: country, "UAT": country}
+        res = self.mapper._set_special_attr(profile, "country", country)
+        self.assertEqual(profile.country, country)
+        self.assertFalse(res)
+
+    def test_set_special_attr(self):
+        """If country attribute, no override and country does not
+        match current county, then set and return True
+        """
+        code = "test"
+        country = CountryFactory(code=code)
+        self.mapper.countries = {code: country, "UAT": country}
+        profile = ProfileFactory(country=None)
+        self.assertIsNone(profile.country)
+        self.assertFalse(profile.countries_available.count())
+        res = self.mapper._set_special_attr(profile, "country", code)
+        self.assertTrue(res)
+        self.assertEqual(profile.country, country)
+        self.assertTrue(profile.countries_available.count())
+
+    def test_set_attribute_char(self):
+        section = Section(name="Initial")
+        res = self.mapper._set_attribute(section, "name", "Change")
+        self.assertTrue(res)
+        self.assertEqual(section.name, "Change")
+
+    def test_set_attribute_char_truncate(self):
+        """If char field and value provided longer than allowed,
+        then truncated
+        """
+        section = Section(name="Initial")
+        res = self.mapper._set_attribute(section, "name", "1" * 70)
+        self.assertTrue(res)
+        self.assertEqual(section.name, "1" * 64)
+
+    def test_set_attribute_blank(self):
+        """Blank values are set to None"""
+        section = Section(name="Initial")
+        res = self.mapper._set_attribute(section, "name", "")
+        self.assertTrue(res)
+        self.assertIsNone(section.name)
+
+    def test_set_attribute_section_code(self):
+        """If section_code attribute, then set to last 4 chars of value"""
+        profile = ProfileFactory()
+        self.assertIsNone(profile.section_code)
+        res = self.mapper._set_attribute(profile, "section_code", "12345678")
+        self.assertTrue(res)
+        self.assertEqual(profile.section_code, "5678")
+
+    def test_set_attribute_special_field(self):
+        """If special field, use _set_special_attr method"""
+        code = "test"
+        country = CountryFactory(code=code)
+        self.mapper.countries = {code: country, "UAT": country}
+        profile = ProfileFactory(country=None)
+        self.assertIsNone(profile.country)
+        self.assertFalse(profile.countries_available.count())
+        res = self.mapper._set_attribute(profile, "country", code)
+        self.assertTrue(res)
+        self.assertEqual(profile.country, country)
+        self.assertTrue(profile.countries_available.count())
