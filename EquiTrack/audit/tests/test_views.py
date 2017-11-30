6131fc09d7dbc9e8839d61a14426afec38f1fa6c
Greg Reinbach <greg@reinbach.com>
2017-11-30 05:58:28 -0500

    Fix audit view test

--- EquiTrack/audit/tests/test_views.py
+++ EquiTrack/audit/tests/test_views.py
@@ -649,4 +649,4 @@ class TestEngagementPartnerView(AuditTestCaseMixin, APITenantTestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]["id"], engagement.pk)
+        self.assertEqual(response.data[0]["id"], engagement.partner.pk)
