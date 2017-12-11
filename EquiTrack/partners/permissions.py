4db6e8457502869844243c27bfef53e2db31cc14
robertavram <robert.avram@gmail.com>
2017-12-11 15:48:49 -0500

    Add permissions prp_mode flag check
    Reporting_periods edit/required are based on prp_mode flag
    result_links edit linked to prp_mode flag
    Refactor conditions to use underscore in naming

--- EquiTrack/partners/permissions.py
+++ EquiTrack/partners/permissions.py
@@ -101,18 +101,26 @@ class InterventionPermissions(PMPPermissions):
         super(InterventionPermissions, self).__init__(**kwargs)
         inbound_check = kwargs.get('inbound_check', False)
 
+        # TODO: fix this after "in amendment" flag is turned on for interventions
         def user_added_amendment(instance):
             assert inbound_check, 'this function cannot be called unless instantiated with inbound_check=True'
             # check_rigid_related checks if there were any changes from the previous
             # amendments if there were changes it returns False
             return not check_rigid_related(instance, 'amendments')
 
+        def prp_mode_off(instance):
+            # TODO: add switch check after the tenant switches are added.
+            return True
+
         self.condition_map = {
             'condition1': self.user in self.instance.unicef_focal_points.all(),
             'condition2': self.user in self.instance.partner_focal_points.all(),
             'contingency on': self.instance.contingency_pd is True,
             # this condition can only be checked on data save
-            'user adds amendment': False if not inbound_check else user_added_amendment(self.instance)
+            'user_adds_amendment': False if not inbound_check else user_added_amendment(self.instance),
+            'prp_mode_off': prp_mode_off(self.instance),
+            'user_adds_amendment+prp_mode_off': (user_added_amendment(self.instance) if inbound_check else False)
+                                                   and prp_mode_off(self.instance)
         }
 
 
