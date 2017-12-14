cb896363bedbd710cd86d5abb8307b142c231c71
Philip Semanchuk <psemanchuk@caktusgroup.com>
2017-12-14 14:14:46 -0500

    add test for creating loader w/a country

--- EquiTrack/vision/tests/test_synchronizer.py
+++ EquiTrack/vision/tests/test_synchronizer.py
@@ -10,28 +10,29 @@ import mock
 
 from vision.vision_data_synchronizer import VisionDataLoader, VisionDataSynchronizer
 from EquiTrack.tests.mixins import FastTenantTestCase
+from users.models import Country
 
 FAUX_VISION_URL = 'https://api.example.com/foo.svc/'
 
 class TestVisionDataLoader(FastTenantTestCase):
     '''Exercise VisionDataLoader class'''
+    # Note - I don't understand why, but @override_settings(VISION_URL=FAUX_VISION_URL) doesn't work when I apply
+    # it on TestVisionDataLoader instead of each individual test case.
     @override_settings(VISION_URL=FAUX_VISION_URL)
     def test_instantiation_no_country(self):
         '''Ensure I can create a loader without specifying a country'''
         loader = VisionDataLoader(endpoint='GetSomeStuff_JSON')
         self.assertEqual(loader.url, FAUX_VISION_URL + '/GetSomeStuff_JSON')
 
-    # def test_instantiation_with_country(self):
-    #     '''Ensure I can create a loader without specifying a country'''
-    #     test_country = Country.objects.all()[0]
-    #     test_country.business_area_code = 'ABC'
-    #     test_country.save()
-    #     # import pprint; pp=pprint.pprint
-    #     # import pdb; pdb.set_trace()
+    @override_settings(VISION_URL=FAUX_VISION_URL)
+    def test_instantiation_with_country(self):
+        '''Ensure I can create a loader that specifies a country'''
+        test_country = Country.objects.all()[0]
+        test_country.business_area_code = 'ABC'
+        test_country.save()
 
-    #     #z=z
-    #     loader = VisionDataLoader(country=test_country, endpoint='GetSomeStuff_JSON')
-    #     self.assertEqual(loader.url, FAUX_VISION_URL + '/GetSomeStuff_JSON/ABC')
+        loader = VisionDataLoader(country=test_country, endpoint='GetSomeStuff_JSON')
+        self.assertEqual(loader.url, FAUX_VISION_URL + '/GetSomeStuff_JSON/ABC')
 
 
 
