1762330d4eb6486e9dc177dcfd986de9799e4a1b
Greg Reinbach <greg@reinbach.com>
2018-01-05 15:40:21 -0500

    Add str unicode tests for management models

--- EquiTrack/management/tests/test_models.py
+++ EquiTrack/management/tests/test_models.py
@@ -1,6 +1,36 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import sys
+from unittest import skipIf
+
 from EquiTrack.factories import PartnerFactory
 from EquiTrack.tests.mixins import FastTenantTestCase
 from management.models import FlaggedIssue
+from management.tests.factories import FlaggedIssueFactory
+
+
+@skipIf(sys.version_info.major == 3, "This test can be deleted under Python 3")
+class TestStrUnicode(FastTenantTestCase):
+    def test_flagged_issue(self):
+        partner = PartnerFactory()
+        issue = FlaggedIssueFactory(
+            content_object=partner,
+            issue_id="321",
+            message='test message'
+        )
+        self.assertEqual(str(issue), b"test message")
+        self.assertEqual(unicode(issue), u"test message")
+
+        issue = FlaggedIssueFactory(
+            content_object=partner,
+            issue_id="321",
+            message=u"R\xe4dda Barnen"
+        )
+        self.assertEqual(str(issue), b"R\xc3\xa4dda Barnen")
+        self.assertEqual(unicode(issue), u"R\xe4dda Barnen")
 
 
 class FlaggedIssueTest(FastTenantTestCase):
@@ -19,7 +49,11 @@ class FlaggedIssueTest(FastTenantTestCase):
     def test_get_or_new_returns_saved(self):
         issue_id = 'test-return-saved'
         partner = PartnerFactory()
-        issue = FlaggedIssue.objects.create(content_object=partner, issue_id=issue_id, message='test message')
+        issue = FlaggedIssueFactory(
+            content_object=partner,
+            issue_id=issue_id,
+            message='test message'
+        )
         self.assertTrue(issue.pk is not None)
         issue_back = FlaggedIssue.get_or_new(partner, issue_id)
         # make sure we got the same one back
