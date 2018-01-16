ff2392f54eccbd7684fdfe50e5f214f493db090a
Greg Reinbach <greg@reinbach.com>
2018-01-16 13:31:58 -0500

    Update report urls
    Move common reports/ to include url call

--- EquiTrack/EquiTrack/urls.py
+++ EquiTrack/EquiTrack/urls.py
@@ -71,7 +71,7 @@ urlpatterns = [
         name='locations_detail_pcode'),
     url(r'^api/t2f/', include(t2f_patterns)),
     url(r'^api/audit/', include('audit.urls', namespace='audit')),
-    url(r'^api/v2/', include('reports.urls_v2')),
+    url(r'^api/v2/reports/', include('reports.urls_v2')),
     url(r'^api/v2/', include('partners.urls_v2', namespace='partners_api')),
     url(r'^api/prp/v1/', include('partners.prp_urls', namespace='prp_api_v1')),
     url(r'^api/v2/hact/', include('hact.urls', namespace='hact_api')),
