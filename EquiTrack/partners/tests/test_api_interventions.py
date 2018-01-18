3e232febbe7bd7044a406a0c65f2081efcb43d04
robertavram <robert.avram@gmail.com>
2018-01-18 09:09:18 -0500

    Fix tests

--- EquiTrack/partners/tests/test_api_interventions.py
+++ EquiTrack/partners/tests/test_api_interventions.py
@@ -82,8 +82,7 @@ class TestInterventionsAPI(APITenantTestCase):
                   "result_links", "contingency_pd", "unicef_signatory", "agreement_id", "signed_by_unicef_date",
                   "partner_authorized_officer_signatory_id", "created", "planned_visits",
                   "planned_budget", "modified", "signed_pd_document", "submission_date_prc", "document_type",
-                  "offices", "population_focus", "country_programme_id", "engagement", "sections", "reporting_periods",
-                  "in_amendment"],
+                  "offices", "population_focus", "country_programme_id", "engagement", "sections", "reporting_periods"],
         'signed': [],
         'active': ['']
     }
