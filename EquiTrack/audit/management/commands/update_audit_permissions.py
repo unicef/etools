dd71a50868a441228b075ca711223ce59e2bbea2
Roman Karpovich <fpm.th13f@gmail.com>
2018-01-19 17:21:56 +0300

    disallow engagement cancel in report_submitted status

--- EquiTrack/audit/management/commands/update_audit_permissions.py
+++ EquiTrack/audit/management/commands/update_audit_permissions.py
@@ -192,7 +192,6 @@ class Command(BaseCommand):
         # report submitted. focal point can finalize. all can view
         self.add_permissions(self.report_submitted, self.everybody, 'view', self.everything)
         self.add_permissions(self.report_submitted, self.focal_point, 'action', [
-            'engagement.cancel',
             'engagement.finalize',
         ])
 
