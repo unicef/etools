bfe9afa45afd0c3c54092f0cf1619a10d4d2c6de
Roman Karpovich <fpm.th13f@gmail.com>
2018-01-22 11:19:52 +0300

    using intervention instead of partnership in activities serializer

--- EquiTrack/activities/serializers.py
+++ EquiTrack/activities/serializers.py
@@ -9,9 +9,9 @@ class ActivitySerializer(serializers.ModelSerializer):
             return self.instance.implementing_partner
         return None
 
-    def _get_partnership(self):
+    def _get_intervention(self):
         if self.instance:
-            return self.instance.partnership
+            return self.instance.intervention
         return None
 
     def _get_cp_output(self):
@@ -19,42 +19,42 @@ class ActivitySerializer(serializers.ModelSerializer):
             return self.instance.cp_output
         return None
 
-    def _validate_partnership_and_partner(self, attrs):
+    def _validate_intervention_and_partner(self, attrs):
         implementing_partner = attrs.get('implementing_partner', serializers.empty)
-        partnership = attrs.get('partnership', serializers.empty)
+        intervention = attrs.get('intervention', serializers.empty)
 
-        if implementing_partner is serializers.empty and partnership is serializers.empty:
+        if implementing_partner is serializers.empty and intervention is serializers.empty:
             return attrs
 
         if implementing_partner is serializers.empty:
             implementing_partner = self._get_implementing_partner()
 
-        if partnership is serializers.empty:
-            partnership = self._get_partnership()
+        if intervention is serializers.empty:
+            intervention = self._get_intervention()
 
-        self.Meta.model._validate_partnership(implementing_partner, partnership)
+        self.Meta.model._validate_intervention(implementing_partner, intervention)
 
         return attrs
 
-    def _validate_cp_output_and_partnership(self, attrs):
-        partnership = attrs.get('partnership', serializers.empty)
+    def _validate_cp_output_and_intervention(self, attrs):
+        intervention = attrs.get('intervention', serializers.empty)
         cp_output = attrs.get('cp_output', serializers.empty)
 
-        if partnership is serializers.empty and cp_output is serializers.empty:
+        if intervention is serializers.empty and cp_output is serializers.empty:
             return attrs
 
-        if partnership is serializers.empty:
-            partnership = self._get_partnership()
+        if intervention is serializers.empty:
+            intervention = self._get_intervention()
 
         if cp_output is serializers.empty:
             cp_output = self._get_cp_output()
 
-        self.Meta.model._validate_cp_output(partnership, cp_output)
+        self.Meta.model._validate_cp_output(intervention, cp_output)
 
         return attrs
 
     def validate(self, attrs):
         attrs = super(ActivitySerializer, self).validate(attrs)
-        attrs = self._validate_partnership_and_partner(attrs)
-        attrs = self._validate_cp_output_and_partnership(attrs)
+        attrs = self._validate_intervention_and_partner(attrs)
+        attrs = self._validate_cp_output_and_intervention(attrs)
         return attrs
