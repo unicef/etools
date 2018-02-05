9258a199511a22c8ece8f8871ee4783a93bf23c3
Greg Reinbach <greg@reinbach.com>
2018-02-05 12:52:16 -0500

    Refactor attachments generate_file_path function
    Update function to handle all get_*_file_path functions in partner models

--- EquiTrack/attachments/models.py
+++ EquiTrack/attachments/models.py
@@ -5,7 +5,7 @@ import os
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
-from django.db import models
+from django.db import models, connection
 from django.utils import six
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.text import slugify
@@ -32,12 +32,93 @@ class FileType(OrderedModel, models.Model):
 
 
 def generate_file_path(attachment, filename):
-    return 'files/{}/{}/{}/{}'.format(
-        attachment.content_type.app_label,
-        slugify(attachment.content_type.model),
-        attachment.object_id,
-        os.path.split(filename)[-1]
-    )
+    # updating other models to use this function,
+    # for now maintaining their previous upload
+    # paths
+    # TODO move all files to standard file path setup?
+    app = attachment.content_type.app_label
+    model_name = attachment.content_type.model
+    obj_pk = str(attachment.object_id)
+    obj = attachment.content_object
+
+    if app == "partners":
+        file_path = [
+            connection.schema_name,
+            'file_attachments',
+            "partner_organization",
+        ]
+        if model_name == "agreement":
+            file_path = file_path + [
+                str(obj.partner.pk),
+                "agreements",
+                str(obj.agreement_number)
+            ]
+        elif model_name == "assessment":
+            file_path = file_path + [
+                str(obj.partner.pk),
+                # maintain spelling mistake
+                # until such time as files are moved
+                # TODO move all files to standard file path setup?
+                'assesments',
+                obj_pk,
+            ]
+        elif model_name in [
+                "interventionamendment",
+                "interventionattachment"
+        ]:
+            file_path = file_path + [
+                str(obj.intervention.agreement.partner.pk),
+                'agreements',
+                str(obj.intervention.agreement.pk),
+                'interventions',
+            ]
+            if model_name == "interventionamendment":
+                # this is an issue with the previous function
+                # it has partner pk twice in the file path
+                # TODO move all files to standard file path setup?
+                file_path = file_path[:3] + [
+                    str(obj.intervention.agreement.partner.pk),
+                ] + file_path[3:] + [
+                    str(obj.intervention.pk),
+                    "amendments",
+                    obj_pk
+                ]
+            else:
+                file_path = file_path + [
+                    str(obj.intervention.pk),
+                    "attachments",
+                    obj_pk
+                ]
+        elif model_name == "intervention":
+            file_path = file_path + [
+                str(obj.agreement.partner.pk),
+                'agreements',
+                str(obj.agreement.pk),
+                'interventions',
+                obj_pk,
+                "prc"
+            ]
+        elif model_name == "agreementamendment":
+            file_path = file_path[:-1] + [
+                'partner_org',
+                str(obj.agreement.partner.pk),
+                'agreements',
+                obj.agreement.base_number,
+                'amendments',
+                str(obj.number),
+            ]
+        else:
+            raise Exception("Unknown file path")
+    else:
+        file_path = [
+            "files",
+            app,
+            slugify(model_name),
+            obj_pk,
+        ]
+
+    file_path.append(os.path.split(filename)[-1])
+    return '/'.join(file_path)
 
 
 @python_2_unicode_compatible
--- EquiTrack/attachments/tests/test_models.py
+++ EquiTrack/attachments/tests/test_models.py
@@ -2,9 +2,165 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from django.core.exceptions import ValidationError
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.db import connection
 
+from attachments import models
 from attachments.tests.factories import AttachmentFactory, FileTypeFactory
+from EquiTrack.factories import (
+    AgreementAmendmentFactory,
+    AgreementFactory,
+    AssessmentFactory,
+    InterventionAmendmentFactory,
+    InterventionAttachmentFactory,
+    InterventionFactory,
+    PartnerFactory,
+)
 from EquiTrack.tests.mixins import FastTenantTestCase
+from partners import models as partner_models
+
+
+class TestGenerateFilePath(FastTenantTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.partner = PartnerFactory()
+        cls.agreement = AgreementFactory(partner=cls.partner)
+        cls.intervention = InterventionFactory(agreement=cls.agreement)
+
+    def assert_file_path(self, file_path, file_path_list, path_type):
+        if path_type == "partners":
+            file_path_list = [
+                connection.schema_name,
+                "file_attachments",
+                "partner_organization",
+                str(self.partner.pk),
+            ] + file_path_list
+        elif path_type == "agreementamendment":
+            file_path_list = [
+                connection.schema_name,
+                "file_attachments",
+                "partner_org",
+            ] + file_path_list
+        self.assertEqual(file_path, "/".join(file_path_list))
+
+    def test_agreement(self):
+        attachment = AttachmentFactory(content_object=self.agreement)
+        file_path = models.generate_file_path(attachment, "test.pdf")
+        self.assert_file_path(file_path, [
+            "agreements",
+            self.agreement.agreement_number,
+            "test.pdf"
+        ], "partners")
+        # check against old file path function
+        file_path_old = partner_models.get_agreement_path(
+            self.agreement,
+            "test.pdf"
+        )
+        self.assertEqual(file_path, file_path_old)
+
+    def test_assessment(self):
+        assessment = AssessmentFactory(partner=self.partner)
+        attachment = AttachmentFactory(content_object=assessment)
+        file_path = models.generate_file_path(attachment, "test.pdf")
+        self.assert_file_path(file_path, [
+            "assesments",
+            str(assessment.pk),
+            "test.pdf"
+        ], "partners")
+        # check against old file path function
+        file_path_old = partner_models.get_assesment_path(
+            assessment,
+            "test.pdf"
+        )
+        self.assertEqual(file_path, file_path_old)
+
+    def test_intervention_amendment(self):
+        amendment = InterventionAmendmentFactory(
+            intervention=self.intervention
+        )
+        attachment = AttachmentFactory(content_object=amendment)
+        file_path = models.generate_file_path(attachment, "test.pdf")
+        self.assert_file_path(file_path, [
+            str(self.partner.pk),
+            "agreements",
+            str(self.agreement.pk),
+            "interventions",
+            str(self.intervention.pk),
+            "amendments",
+            str(amendment.pk),
+            "test.pdf"
+        ], "partners")
+        # check against old file path function
+        file_path_old = partner_models.get_intervention_amendment_file_path(
+            amendment,
+            "test.pdf"
+        )
+        self.assertEqual(file_path, file_path_old)
+
+    def test_intervention_attachment(self):
+        intervention_attachment = InterventionAttachmentFactory(
+            intervention=self.intervention
+        )
+        attachment = AttachmentFactory(content_object=intervention_attachment)
+        file_path = models.generate_file_path(attachment, "test.pdf")
+        self.assert_file_path(file_path, [
+            "agreements",
+            str(self.agreement.pk),
+            "interventions",
+            str(self.intervention.pk),
+            "attachments",
+            str(intervention_attachment.pk),
+            "test.pdf"
+        ], "partners")
+        # check against old file path function
+        file_path_old = partner_models.get_intervention_attachments_file_path(
+            intervention_attachment,
+            "test.pdf"
+        )
+        self.assertEqual(file_path, file_path_old)
+
+    def test_intervention(self):
+        attachment = AttachmentFactory(
+            content_object=self.intervention
+        )
+        file_path = models.generate_file_path(attachment, "test.pdf")
+        self.assert_file_path(file_path, [
+            "agreements",
+            str(self.agreement.pk),
+            "interventions",
+            str(self.intervention.pk),
+            "prc",
+            "test.pdf"
+        ], "partners")
+        # check against old file path function
+        file_path_old = partner_models.get_prc_intervention_file_path(
+            self.intervention,
+            "test.pdf"
+        )
+        self.assertEqual(file_path, file_path_old)
+
+    def test_agreement_amendment(self):
+        amendment = AgreementAmendmentFactory(agreement=self.agreement)
+        attachment = AttachmentFactory(content_object=amendment)
+        file_path = models.generate_file_path(attachment, "test.pdf")
+        self.assert_file_path(file_path, [
+            str(self.partner.pk),
+            "agreements",
+            self.agreement.base_number,
+            "amendments",
+            str(amendment.number),
+            "test.pdf"
+        ], "agreementamendment")
+        # check against old file path function
+        file_path_old = partner_models.get_agreement_amd_file_path(
+            amendment,
+            "test.pdf"
+        )
+        self.assertEqual(file_path, file_path_old)
+
+    def test_exception(self):
+        attachment = AttachmentFactory(content_object=self.partner)
+        with self.assertRaisesRegexp(Exception, "Unknown file path"):
+            models.generate_file_path(attachment, "test.pdf")
 
 
 class TestAttachmentsModels(FastTenantTestCase):
