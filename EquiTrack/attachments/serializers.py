cffc2702d65643ec2f89fa0fd747d380122e4429
Greg Reinbach <greg@reinbach.com>
2018-03-07 08:05:14 -0500

    Update attachment serializer
    Add upload link field is request type is patch, post, or put

--- EquiTrack/attachments/serializers.py
+++ EquiTrack/attachments/serializers.py
@@ -90,28 +90,50 @@ class AttachmentFileUploadSerializer(serializers.ModelSerializer):
 
 
 class AttachmentSerializerMixin(object):
-    def save(self, **kwargs):
-        super(AttachmentSerializerMixin, self).save(**kwargs)
+    ATTACHMENT_SUFFIX = "_upload_link"
 
+    def __init__(self, *args, **kwargs):
+        super(AttachmentSerializerMixin, self).__init__(*args, **kwargs)
+        if self.instance:
+            self.add_upload_links()
+
+    def get_upload_field_name(self, field_name):
+        """Ensure we get valid field name
+        Need to prevent field being added mutliple times with
+        suffix appended each time
+        """
+        if not field_name.endswith(self.ATTACHMENT_SUFFIX):
+            upload_name = "{}{}".format(field_name, self.ATTACHMENT_SUFFIX)
+            if upload_name not in self.fields.keys():
+                return upload_name
+        return None
+
+    def add_upload_links(self):
+        # if request is post, put, or patch
         # check if attachment field types exist
         # ensure attachment record exists for instance
         # set upload link value
-        for field_name, field in self.fields.items():
-            if isinstance(field, AttachmentSingleFileField):
-                attachments = getattr(self.instance, field.source)
-                if not attachments.exists():
-                    file_type = FileType.objects.get(
-                        code=attachments.core_filters["code"]
-                    )
-                    Attachment.objects.create(
-                        file_type=file_type,
-                        file=None,
-                        code=attachments.core_filters["code"],
-                        content_object=self.instance
-                    )
-                upload_field_name = "{}_upload_link".format(field_name)
-                self.fields[upload_field_name] = AttachmentUploadLinkField(
-                    source=field.source
-                )
+        if self.context["request"].method in ["PATCH", "POST", "PUT"]:
+            for field_name, field in self.fields.items():
+                if isinstance(field, AttachmentSingleFileField):
+                    upload_field_name = self.get_upload_field_name(field_name)
+                    if upload_field_name is not None:
+                        attachments = getattr(self.instance, field.source)
+                        if not attachments.exists():
+                            file_type = FileType.objects.get(
+                                code=attachments.core_filters["code"]
+                            )
+                            Attachment.objects.create(
+                                file_type=file_type,
+                                file=None,
+                                code=attachments.core_filters["code"],
+                                content_object=self.instance
+                            )
+                        self.fields[upload_field_name] = AttachmentUploadLinkField(
+                            source=field.source
+                        )
 
+    def save(self, *args, **kwargs):
+        super(AttachmentSerializerMixin, self).save(*args, **kwargs)
+        self.add_upload_links()
         return self.instance
