369545937d483c42c113d0cf034b94d7acf32ca1
Roman Karpovich <fpm.th13f@gmail.com>
2018-03-13 13:51:14 +0300

    minor code refactoring in fsm mixin determine_actions

--- EquiTrack/utils/common/metadata.py
+++ EquiTrack/utils/common/metadata.py
@@ -49,15 +49,16 @@ class FSMTransitionActionMetadataMixin(object):
 
     def determine_actions(self, request, view):
         actions = super(FSMTransitionActionMetadataMixin, self).determine_actions(request, view)
+
         instance = self._get_instance(view)
         if not instance:
             return actions
 
-        allowed_FSM_transitions = []
         current_state = getattr(instance, 'status', None)
         if current_state is None:
             return actions
 
+        allowed_FSM_transitions = []
         for action in self._collect_actions(instance):
             meta = action._django_fsm
 
