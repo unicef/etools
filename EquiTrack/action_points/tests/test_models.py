817a7f5397536ea2ca5ff62835fefdf7bb9429c7
Roman Karpovich <fpm.th13f@gmail.com>
2018-03-22 18:27:56 +0300

    using basetenanttestcase for action points

--- EquiTrack/action_points/tests/test_models.py
+++ EquiTrack/action_points/tests/test_models.py
@@ -3,13 +3,13 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import factory.fuzzy
 from rest_framework.exceptions import ValidationError
 
-from EquiTrack.tests.cases import EToolsTenantTestCase
+from EquiTrack.tests.cases import BaseTenantTestCase
 from action_points.models import ActionPoint
 from action_points.tests.factories import ActionPointFactory
 from audit.tests.factories import MicroAssessmentFactory
 
 
-class TestActionPointModel(EToolsTenantTestCase):
+class TestActionPointModel(BaseTenantTestCase):
     def test_str(self):
         action_point = ActionPointFactory()
         self.assertEqual(str(action_point), '{0}/{1}/ACTP'.format(action_point.created.year, action_point.id))
