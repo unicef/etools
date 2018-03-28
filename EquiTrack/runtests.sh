32f53234822876a5a55676e5d8455268522c0c56
Roman Karpovich <fpm.th13f@gmail.com>
2018-03-28 13:28:18 +0300

    clear db cause to model updates

--- EquiTrack/runtests.sh
+++ EquiTrack/runtests.sh
@@ -13,7 +13,7 @@ python manage.py makemigrations --dry-run  --settings="$TEST_SETTINGS" | grep 'N
 
 # Run unittests and coverage report
 coverage erase
-coverage run manage.py test --noinput --keepdb --settings="$TEST_SETTINGS" "$@"
+coverage run manage.py test --noinput --settings="$TEST_SETTINGS" "$@"
 coverage report --include "$@/*.py"
 
 # Check code style
