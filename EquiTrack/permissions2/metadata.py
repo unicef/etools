80e9ca194a64f6403ae5a3f1a1803ae599176ebe
Roman Karpovich <fpm.th13f@gmail.com>
2018-04-04 16:13:21 +0300

    fixed missing instance on fsm metadata.

--- EquiTrack/permissions2/metadata.py
+++ EquiTrack/permissions2/metadata.py
@@ -5,6 +5,6 @@ from utils.common.metadata import FSMTransitionActionMetadataMixin
 
 class PermittedFSMTransitionActionMetadataMixin(FSMTransitionActionMetadataMixin):
     def determine_actions(self, request, view):
-        request.user._permission_context = view._collect_permission_context()
+        request.user._permission_context = view._collect_permission_context(instance=self._get_instance(view))
 
         return super(PermittedFSMTransitionActionMetadataMixin, self).determine_actions(request, view)
