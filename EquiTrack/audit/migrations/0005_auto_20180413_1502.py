bc4a23ff22acbb707ca9ca3b6b56a785237b1317
Maxim Lomakin <lom.tik.com@gmail.com>
2018-04-13 18:06:44 +0300

    Updated label for sc finding recommendation.

--- EquiTrack/audit/migrations/0005_auto_20180413_1502.py
+++ EquiTrack/audit/migrations/0005_auto_20180413_1502.py
@@ -18,4 +18,9 @@ class Migration(migrations.Migration):
             name='shared_ip_with',
             field=django.contrib.postgres.fields.ArrayField(base_field=models.CharField(choices=[('DPKO', 'DPKO'), ('ECA', 'ECA'), ('ECLAC', 'ECLAC'), ('ESCWA', 'ESCWA'), ('FAO', 'FAO'), ('ILO', 'ILO'), ('IOM', 'IOM'), ('OHCHR', 'OHCHR'), ('UN', 'UN'), ('UN Women', 'UN Women'), ('UNAIDS', 'UNAIDS'), ('UNDP', 'UNDP'), ('UNESCO', 'UNESCO'), ('UNFPA', 'UNFPA'), ('UN - Habitat', 'UN - Habitat'), ('UNHCR', 'UNHCR'), ('UNODC', 'UNODC'), ('UNOPS', 'UNOPS'), ('UNRWA', 'UNRWA'), ('UNSC', 'UNSC'), ('UNU', 'UNU'), ('WB', 'WB'), ('WFP', 'WFP'), ('WHO', 'WHO')], max_length=20), blank=True, default=[], size=None, verbose_name='Shared Audit with'),
         ),
+        migrations.AlterField(
+            model_name='finding',
+            name='recommendation',
+            field=models.TextField(blank=True, verbose_name='Finding and Recommendation'),
+        ),
     ]
