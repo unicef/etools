b126bb60db8dfe99f8243baee7de58523a91003d
Domenico DiNicola <domenico.dinicola@wfp.org>
2018-04-18 15:45:18 -0400

    added app name for all apps

--- EquiTrack/email_auth/urls.py
+++ EquiTrack/email_auth/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import url
 from email_auth import views
 
 
+app_name = 'email_auth'
 urlpatterns = [
     url(r"^login/$", views.TokenAuthView.as_view(), name="login"),
 ]
--- EquiTrack/monitoring/urls.py
+++ EquiTrack/monitoring/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from monitoring.views import CheckView
 
+app_name = 'monitoring'
 urlpatterns = (
     url(r'^$', CheckView.as_view(), name="monitoring"),
 )
