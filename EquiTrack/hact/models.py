3a6f1b65114679571796f41224d94e05e1ad53d2
Domenico DiNicola <domenico.dinicola@wfp.org>
2018-04-19 09:45:15 -0400

    flake

--- EquiTrack/hact/models.py
+++ EquiTrack/hact/models.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import decimal
 import json
-from datetime import date, datetime, timedelta
+from datetime import date, datetime
 from decimal import Decimal
 
 from django.contrib.postgres.fields import JSONField
