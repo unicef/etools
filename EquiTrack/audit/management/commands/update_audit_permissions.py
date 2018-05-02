0512ae120894d3317b5c647a26f0be6c5da3f953
Roman Karpovich <fpm.th13f@gmail.com>
2018-05-02 17:37:53 +0300

    fixed missing face form dates on spot check for auditor

--- EquiTrack/audit/management/commands/update_audit_permissions.py
+++ EquiTrack/audit/management/commands/update_audit_permissions.py
@@ -32,6 +32,9 @@ class Command(BaseCommand):
         'audit.engagement.status',
         'audit.engagement.status_date',
 
+        'audit.spotcheck.face_form_start_date',
+        'audit.spotcheck.face_form_end_date',
+
         'purchase_order.purchaseorder.*',
         'purchase_order.auditorfirm.*',
     ]
