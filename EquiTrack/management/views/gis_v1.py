de8d04531fbdc6c622303426c303b6a5d43dc8d9
denes csaba <csaba.denes@nordlogic.com>
2018-05-02 20:58:08 +0300

    change coding style

--- EquiTrack/management/views/gis_v1.py
+++ EquiTrack/management/views/gis_v1.py
@@ -32,37 +32,37 @@ class GisLocationsInUseViewset(ListAPIView):
                 connection.set_tenant(Country.objects.get(pk=country_id))
             except Country.DoesNotExist:
                 return Response(status=400, data={'error': 'Country not found'})
+            else:
+                interventions = Intervention.objects.all()
+                location_ids = set()
 
-            interventions = Intervention.objects.all()
-            location_ids = set()
+                for intervention in interventions:
+                    for loc in intervention.flat_locations.all():
+                        location_ids.add(loc.id)
 
-            for intervention in interventions:
-                for loc in intervention.flat_locations.all():
-                    location_ids.add(loc.id)
+                indicators = AppliedIndicator.objects.prefetch_related(
+                    'locations'
+                ).all()
 
-            indicators = AppliedIndicator.objects.prefetch_related(
-                'locations'
-            ).all()
+                for indicator in indicators:
+                    for iloc in indicator.locations.all():
+                        location_ids.add(iloc.id)
 
-            for indicator in indicators:
-                for iloc in indicator.locations.all():
-                    location_ids.add(iloc.id)
+                travel_activities = TravelActivity.objects.prefetch_related(
+                    'locations'
+                ).all()
 
-            travel_activities = TravelActivity.objects.prefetch_related(
-                'locations'
-            ).all()
+                for travel_activity in travel_activities:
+                    for t2f_loc in travel_activity.locations.all():
+                        location_ids.add(t2f_loc.id)
 
-            for travel_activity in travel_activities:
-                for t2f_loc in travel_activity.locations.all():
-                    location_ids.add(t2f_loc.id)
+                locations = Location.objects.filter(
+                    pk__in=list(location_ids),
+                ).all()
 
-            locations = Location.objects.filter(
-                pk__in=list(location_ids),
-            ).all()
+                serializer = GisLocationListSerializer(locations, many=True, context={'request': request})
 
-            serializer = GisLocationListSerializer(locations, many=True, context={'request': request})
-
-            return Response(serializer.data)
+                return Response(serializer.data)
         else:
             return Response(status=400, data={'error': 'Country is required'})
 
@@ -90,9 +90,9 @@ class GisLocationsGeomListViewset(ListAPIView):
                 connection.set_tenant(Country.objects.get(pk=country_id))
             except Country.DoesNotExist:
                 return Response(status=400, data={'error': 'Country not found'})
-
-            locations = Location.objects.filter(geom__isnull=False).all()
-            serializer = GisLocationGeoDetailSerializer(locations, many=True, context={'request': request})
+            else:
+                locations = Location.objects.filter(geom__isnull=False).all()
+                serializer = GisLocationGeoDetailSerializer(locations, many=True, context={'request': request})
 
             return Response(serializer.data)
         else:
@@ -127,8 +127,8 @@ class GisLocationsGeomDetailsViewset(RetrieveAPIView):
                     location = Location.objects.get(**lookup)
                 except Location.DoesNotExist:
                     return Response(status=400, data={'error': 'Location not found'})
-
-                serializer = GisLocationGeoDetailSerializer(location, context={'request': request})
-                return Response(serializer.data)
+                else:
+                    serializer = GisLocationGeoDetailSerializer(location, context={'request': request})
+                    return Response(serializer.data)
         else:
             return Response(status=400, data={'error': 'Country is required'})
