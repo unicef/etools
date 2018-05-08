c402931f4c8a261e2bc230035ee0e7812dab5560
denes csaba <csaba.denes@nordlogic.com>
2018-05-08 16:23:31 +0300

    Refactor api view

--- EquiTrack/management/views/gis_v1.py
+++ EquiTrack/management/views/gis_v1.py
@@ -26,45 +26,45 @@ class GisLocationsInUseViewset(ListAPIView):
         '''
         country_id = request.query_params.get('country_id')
 
-        if country_id:
-            try:
-                # we need to set the workspace before making any query
-                connection.set_tenant(Country.objects.get(pk=country_id))
-            except Country.DoesNotExist:
-                return Response(status=400, data={'error': 'Country not found'})
-            else:
-                interventions = Intervention.objects.all()
-                location_ids = set()
+        if not country_id:
+            return Response(status=400, data={'error': 'Country id is required'})
 
-                for intervention in interventions:
-                    for loc in intervention.flat_locations.all():
-                        location_ids.add(loc.id)
-
-                indicators = AppliedIndicator.objects.prefetch_related(
-                    'locations'
-                ).all()
-
-                for indicator in indicators:
-                    for iloc in indicator.locations.all():
-                        location_ids.add(iloc.id)
-
-                travel_activities = TravelActivity.objects.prefetch_related(
-                    'locations'
-                ).all()
-
-                for travel_activity in travel_activities:
-                    for t2f_loc in travel_activity.locations.all():
-                        location_ids.add(t2f_loc.id)
-
-                locations = Location.objects.filter(
-                    pk__in=list(location_ids),
-                ).all()
-
-                serializer = GisLocationListSerializer(locations, many=True, context={'request': request})
-
-                return Response(serializer.data)
+        try:
+            # we need to set the workspace before making any query
+            connection.set_tenant(Country.objects.get(pk=country_id))
+        except Country.DoesNotExist:
+            return Response(status=400, data={'error': 'Country not found'})
         else:
-            return Response(status=400, data={'error': 'Country is required'})
+            interventions = Intervention.objects.all()
+            location_ids = set()
+
+            for intervention in interventions:
+                for loc in intervention.flat_locations.all():
+                    location_ids.add(loc.id)
+
+            indicators = AppliedIndicator.objects.prefetch_related(
+                'locations'
+            ).all()
+
+            for indicator in indicators:
+                for iloc in indicator.locations.all():
+                    location_ids.add(iloc.id)
+
+            travel_activities = TravelActivity.objects.prefetch_related(
+                'locations'
+            ).all()
+
+            for travel_activity in travel_activities:
+                for t2f_loc in travel_activity.locations.all():
+                    location_ids.add(t2f_loc.id)
+
+            locations = Location.objects.filter(
+                pk__in=list(location_ids),
+            ).all()
+
+            serializer = GisLocationListSerializer(locations, many=True, context={'request': request})
+
+            return Response(serializer.data)
 
 
 class GisLocationsGeomListViewset(ListAPIView):
@@ -84,19 +84,19 @@ class GisLocationsGeomListViewset(ListAPIView):
 
         country_id = request.query_params.get('country_id')
 
-        if country_id:
-            try:
-                # we need to set the workspace before making any query
-                connection.set_tenant(Country.objects.get(pk=country_id))
-            except Country.DoesNotExist:
-                return Response(status=400, data={'error': 'Country not found'})
-            else:
-                locations = Location.objects.filter(geom__isnull=False).all()
-                serializer = GisLocationGeoDetailSerializer(locations, many=True, context={'request': request})
+        if not country_id:
+            return Response(status=400, data={'error': 'Country id is required'})
 
-            return Response(serializer.data)
+        try:
+            # we need to set the workspace before making any query
+            connection.set_tenant(Country.objects.get(pk=country_id))
+        except Country.DoesNotExist:
+            return Response(status=400, data={'error': 'Country not found'})
         else:
-            return Response(status=400, data={'error': 'Country is required'})
+            locations = Location.objects.filter(geom__isnull=False).all()
+            serializer = GisLocationGeoDetailSerializer(locations, many=True, context={'request': request})
+
+        return Response(serializer.data)
 
 
 class GisLocationsGeomDetailsViewset(RetrieveAPIView):
@@ -114,21 +114,21 @@ class GisLocationsGeomDetailsViewset(RetrieveAPIView):
 
         country_id = self.request.query_params.get('country_id')
 
-        if country_id:
-            try:
-                # we need to set the workspace before making any query
-                connection.set_tenant(Country.objects.get(pk=country_id))
-            except Country.DoesNotExist:
-                return Response(status=400, data={'error': 'Country not found'})
-
-            if pcode is not None or id is not None:
-                try:
-                    lookup = {'p_code': pcode} if id is None else {'pk': id}
-                    location = Location.objects.get(**lookup)
-                except Location.DoesNotExist:
-                    return Response(status=400, data={'error': 'Location not found'})
-                else:
-                    serializer = GisLocationGeoDetailSerializer(location, context={'request': request})
-                    return Response(serializer.data)
-        else:
+        if not country_id:
             return Response(status=400, data={'error': 'Country is required'})
+
+        try:
+            # we need to set the workspace before making any query
+            connection.set_tenant(Country.objects.get(pk=country_id))
+        except Country.DoesNotExist:
+            return Response(status=400, data={'error': 'Country not found'})
+
+        if pcode is not None or id is not None:
+            try:
+                lookup = {'p_code': pcode} if id is None else {'pk': id}
+                location = Location.objects.get(**lookup)
+            except Location.DoesNotExist:
+                return Response(status=400, data={'error': 'Location not found'})
+            else:
+                serializer = GisLocationGeoDetailSerializer(location, context={'request': request})
+                return Response(serializer.data)
