80233efa223f69d3e3bcc678f68bb12604209081
denes csaba <csaba.denes@nordlogic.com>
2018-05-14 16:20:35 +0300

    Break up multiple bank details in separate tables

--- src/etools/applications/partners/views/v1.py
+++ src/etools/applications/partners/views/v1.py
@@ -45,6 +45,7 @@ class PCAPDFView(PDFTemplateView):
         if not agreement.partner.vendor_number:
             return {"error": "Partner Organization has no vendor number stored, please report to an etools focal point"}
 
+        # 2500232146
         valid_response, response = get_data_from_insight('GetPartnerDetailsInfo_json/{vendor_code}',
                                                          {"vendor_code": agreement.partner.vendor_number})
 
@@ -60,7 +61,8 @@ class PCAPDFView(PDFTemplateView):
             ('bank_name', 'BANK_NAME'),
             ('account_title', "ACCT_HOLDER"),
             ('routing_details', "SWIFT_CODE"),
-            ('account_number', "BANK_ACCOUNT_NO")
+            ('account_number', "BANK_ACCOUNT_NO"),
+            ('account_currency', "BANK_ACCOUNT_CURRENCY"),
         ]
         Bank = namedtuple('Bank', ' '.join([i[0] for i in bank_key_values]))
         bank_objects = []
@@ -68,6 +70,7 @@ class PCAPDFView(PDFTemplateView):
             if isinstance(b, dict):
                 b["BANK_ADDRESS"] = ', '.join(b[key] for key in ['STREET', 'CITY'] if key in b)
                 b["ACCT_HOLDER"] = b["ACCT_HOLDER"] if "ACCT_HOLDER" in b else ""
+                b["BANK_ACCOUNT_CURRENCY"] = b["BANK_ACCOUNT_CURRENCY"] if "BANK_ACCOUNT_CURRENCY" in b else ""
                 bank_objects.append(Bank(*[b[i[1]] for i in bank_key_values]))
 
         officers_list = []
