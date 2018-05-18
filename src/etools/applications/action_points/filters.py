bbc9a3e7916d7f874a689aee9b6d18a05388f361
Roman Karpovich <fpm.th13f@gmail.com>
2018-05-18 18:59:43 +0300

    simplified action points reference number ordering

--- src/etools/applications/action_points/filters.py
+++ src/etools/applications/action_points/filters.py
@@ -11,10 +11,9 @@ class ReferenceNumberOrderingFilter(BaseFilterBackend):
         if not ordering.lstrip('-') == 'reference_number':
             return queryset
 
-        ordering_params = ['created_year', 'id']
-
-        return queryset.annotate(created_year=TruncYear('created'))\
-            .order_by(*map(lambda param: ('' if ordering == 'reference_number' else '-') + param, ordering_params))
+        asc_desc = "-" if ordering.startswith("-") else ""
+        ordering_params = ["{}{}".format(asc_desc, param) for param in ["created_year", "id"]]
+        return queryset.annotate(created_year=TruncYear("created")).order_by(*ordering_params)
 
 
 class RelatedModuleFilter(BaseFilterBackend):
