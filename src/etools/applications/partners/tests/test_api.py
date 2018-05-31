269e71691b143a9f97f58be539f2c19d40b54824
sax <s.apostolico@gmail.com>
2018-05-31 13:19:48 +0200

    add ability to create custom asserter for nested objects

--- src/etools/applications/partners/tests/test_api.py
+++ src/etools/applications/partners/tests/test_api.py
@@ -1,22 +1,15 @@
 import datetime
 
-from django.db import connection
 from django.urls import reverse
 
 from etools.applications.EquiTrack.tests.cases import BaseTenantTestCase
 from etools.applications.partners.models import PartnerType
 from etools.applications.partners.tests.factories import (AgreementFactory, PartnerFactory, InterventionFactory,
                                                           InterventionAmendmentFactory, InterventionResultLinkFactory)
-from etools.libraries.utils.test.api_checker import ApiChecker, ViewSetChecker
+from etools.libraries.utils.test.api_checker import ApiChecker, ViewSetChecker, AssertTimeStampedMixin
 
 
-class TestAPIAgreements(ApiChecker, BaseTenantTestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-
-    # recorder = StandardAPIRecorder(__file__)
+class TestAPIAgreements(ApiChecker, AssertTimeStampedMixin, BaseTenantTestCase):
 
     def get_fixtures(self):
         return {'agreement': AgreementFactory(signed_by_unicef_date=datetime.date.today())}
@@ -30,85 +23,41 @@ class TestAPIAgreements(ApiChecker, BaseTenantTestCase):
         self.assertAPI(url)
 
 
-class TestAPIIntervention(ApiChecker, BaseTenantTestCase, metaclass=ViewSetChecker):
+class TestAPIIntervention(BaseTenantTestCase, metaclass=ViewSetChecker):
 
     def get_fixtures(cls):
-        return {'intervention': InterventionFactory(),
+        return {'intervention': InterventionFactory(id=101),
                 'amendment': InterventionAmendmentFactory(),
                 'result': InterventionResultLinkFactory(),
                 }
 
-
     @classmethod
     def get_urls(self):
         return [
-            reverse("partners_api:intervention-list")
+            reverse("partners_api:intervention-list"),
+            reverse("partners_api:intervention-detail", args=[101]),
+            reverse("partners_api:intervention-indicators"),
+            reverse("partners_api:intervention-amendments"),
+            reverse("partners_api:intervention-map"),
         ]
 
 
-#
-#     @recorder.record(InterventionFactory)
-#     def intervention(self):
-#         return {}
-#
-#     @recorder.record(InterventionAmendmentFactory)
-#     def amendment(self):
-#         return {}
-#
-#     @recorder.record(InterventionResultLinkFactory)
-#     def result(self):
-#         return {}
-#
-#     def setUp(self):
-#         connection.set_tenant(self.tenant)
-#         assert self.intervention
-#         assert self.result
-#         assert self.amendment
-#
-#     def test_intervention_detail(self):
-#         url = reverse("partners_api:intervention-detail",
-#                       args=[self.intervention.pk])
-#         self.assertAPI(url)
-#
-#     def test_intervention_list(self):
-#         url = reverse("partners_api:intervention-list")
-#         self.assertAPI(url)
-#
-#     def test_intervention_indicator(self):
-#         url = reverse("partners_api:intervention-indicators")
-#         self.assertAPI(url)
-#
-#     def test_intervention_map(self):
-#         url = reverse("partners_api:intervention-map")
-#         self.assertAPI(url)
-#
-#     def test_intervention_amendments(self):
-#         assert self.result
-#         url = reverse("partners_api:intervention-amendments")
-#         self.assertAPI(url)
-#
-#
-# class TestPartners(ApiChecker, BaseTenantTestCase):
-#     recorder = StandardAPIRecorder(__file__)
-#
-#     @recorder.record(PartnerFactory)
-#     def partner(self):
-#         return dict(
-#             partner_type=PartnerType.CIVIL_SOCIETY_ORGANIZATION,
-#             cso_type="International",
-#             hidden=False,
-#             vendor_number="DDD",
-#             short_name="Short name",
-#             modified=datetime.datetime.today()
-#         )
-#
-#     def test_detail(self):
-#         # PartnerOrganizationDetailSerializer
-#         url = reverse("partners_api:partner-detail", args=[self.partner.pk])
-#         self.assertAPI(url)
-#
-#     def test_list(self):
-#         # PartnerOrganizationDetailSerializer
-#         assert self.partner
-#         url = reverse("partners_api:partner-list")
-#         self.assertAPI(url)
+class TestPartners(ApiChecker, BaseTenantTestCase):
+
+    def get_fixtures(self):
+        return {'partner': PartnerFactory(hidden=False,
+                                          partner_type=PartnerType.CIVIL_SOCIETY_ORGANIZATION,
+                                          cso_type="International",
+                                          vendor_number="DDD",
+                                          short_name="Short name",
+                                          modified=datetime.datetime.today()
+                                          )}
+
+    def test_detail(self):
+        url = reverse("partners_api:partner-detail",
+                      args=[self.get_fixture('partner').pk])
+        self.assertAPI(url)
+
+    def test_list(self):
+        url = reverse("partners_api:partner-list")
+        self.assertAPI(url)
--- src/etools/applications/users/tests/factories.py
+++ src/etools/applications/users/tests/factories.py
@@ -45,7 +45,6 @@ class CountryFactory(factory.django.DjangoModelFactory):
 class ProfileFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.UserProfile
-        django_get_or_create = ('user',)
 
     country = factory.SubFactory(CountryFactory)
     office = factory.SubFactory(OfficeFactory)
@@ -64,7 +63,6 @@ class ProfileFactory(factory.django.DjangoModelFactory):
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = get_user_model()
-        django_get_or_create = ('username',)
 
     username = FuzzyText()
     email = factory.Sequence(lambda n: "user{}@example.com".format(n))
