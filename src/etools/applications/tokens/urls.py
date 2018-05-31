b535790ffbce0e951a2ed7244e3eac2bb8d8d494
Greg Reinbach <greg@reinbach.com>
2018-05-31 08:51:53 -0400

    Add api endpoints for token management

--- src/etools/applications/tokens/urls.py
+++ src/etools/applications/tokens/urls.py
@@ -4,5 +4,8 @@ from etools.applications.tokens import views
 
 app_name = 'tokens'
 urlpatterns = [
-    url(r"^login/$", views.TokenAuthView.as_view(), name="login"),
+    url(r"^email/login/$", views.TokenEmailAuthView.as_view(), name="login"),
+    url(r"^$", views.TokenGetView.as_view(), name="get"),
+    url(r"^reset/$", views.TokenResetView.as_view(), name="reset"),
+    url(r"^delete/$", views.TokenDeleteView.as_view(), name="delete"),
 ]
--- src/etools/applications/tokens/views.py
+++ src/etools/applications/tokens/views.py
@@ -2,13 +2,16 @@ from django.conf import settings
 from django.shortcuts import redirect
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import FormView
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from etools.applications.tokens.forms import EmailLoginForm
 from etools.applications.tokens.utils import get_token_auth_link, update_url_with_kwargs
 from etools.applications.notification.utils import send_notification_using_email_template
 
 
-class TokenAuthView(FormView):
+class TokenEmailAuthView(FormView):
     form_class = EmailLoginForm
     template_name = 'tokens/login.html'
 
@@ -16,15 +19,15 @@ class TokenAuthView(FormView):
         if request.user.is_authenticated:
             return redirect(request.GET.get('next', 'dashboard'))
 
-        return super(TokenAuthView, self).get(request, *args, **kwargs)
+        return super().get(request, *args, **kwargs)
 
     def get_initial(self):
-        initial = super(TokenAuthView, self).get_initial()
+        initial = super().get_initial()
         initial.update({'next': self.request.GET.get('next')})
         return initial
 
     def get_context_data(self, **kwargs):
-        context = super(TokenAuthView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         if settings.EMAIL_AUTH_TOKEN_NAME in self.request.GET:
             context['form'].errors['__all__'] = [_('Couldn\'t log you in. Invalid token.')]
 
@@ -49,3 +52,36 @@ class TokenAuthView(FormView):
         )
 
         return self.render_to_response(self.get_context_data(email=form.get_user().email))
+
+
+class TokenGetView(APIView):
+    """Expects user to be logged in already"""
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect(request.GET.get('next', 'dashboard'))
+        token, _ = Token.objects.get_or_create(user=request.user)
+        return Response({"token": token.key})
+
+
+class TokenResetView(APIView):
+    """Reset users token"""
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect(request.GET.get('next', 'dashboard'))
+        token, created = Token.objects.get_or_create(user=request.user)
+        if not created:
+            token.delete()
+            token = Token.objects.create(user=request.user)
+        return Response({"token": token.key})
+
+
+class TokenDeleteView(APIView):
+    """Delete users token"""
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect(request.GET.get('next', 'dashboard'))
+        try:
+            Token.objects.get(user=request.user).delete()
+        except Token.DoesNotExist:
+            pass
+        return Response({"message": "Token has been deleted."})
