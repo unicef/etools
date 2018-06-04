5ab452965b2ce6833816ec3342a16572c115379c
Greg Reinbach <greg@reinbach.com>
2018-06-04 06:38:44 -0400

    Fix merge conflict

--- src/etools/applications/EquiTrack/tests/test_utils.py
+++ src/etools/applications/EquiTrack/tests/test_utils.py
@@ -9,7 +9,6 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from django.test import RequestFactory, SimpleTestCase
 
-import mock
 from freezegun import freeze_time
 
 from etools.applications.EquiTrack import utils
@@ -30,17 +29,6 @@ class TestUtils(SimpleTestCase):
 
         current_year = utils.get_current_year()
         self.assertEqual(current_year, 2013)
-
-    @freeze_time("2013-05-26")
-    def test_get_quarter_default(self):
-        """test current quarter function"""
-        quarter = utils.get_quarter()
-        self.assertEqual(quarter, 'q2')
-
-    def test_get_quarter(self):
-        """test current quarter function"""
-        quarter = utils.get_quarter(datetime(2016, 10, 1))
-        self.assertEqual(quarter, 'q4')
 
 
 class TestGetAllFieldNames(SimpleTestCase):
