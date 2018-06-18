8171a07b8f69d655d4c55b325287788140c1b085
Roman Karpovich <fpm.th13f@gmail.com>
2018-06-18 14:47:55 +0300

    configured permissions for nested resources in case of new root object
    (cherry picked from commit daabc8a)

--- src/etools/applications/audit/views.py
+++ src/etools/applications/audit/views.py
@@ -61,16 +61,8 @@ class BaseAuditViewSet(
     permission_classes = [IsAuthenticated, ]
 
     def get_permission_context(self):
-        context = [
-            AuditModuleCondition(),
-            GroupCondition(self.request.user),
-        ]
-
-        if getattr(self, 'action', None) == 'create':
-            context.append(
-                NewObjectCondition(self.queryset.model),
-            )
-
+        context = super().get_permission_context()
+        context.append(AuditModuleCondition())
         return context
 
 
@@ -133,9 +125,11 @@ class AuditorFirmViewSet(
         return context
 
     def get_obj_permission_context(self, obj):
-        return [
+        context = super().get_obj_permission_context(obj)
+        context.extend([
             AuditStaffMemberCondition(obj, self.request.user),
-        ]
+        ])
+        return context
 
     @list_route(methods=['get'], url_path='users')
     def users(self, request, *args, **kwargs):
@@ -250,6 +244,17 @@ class EngagementViewSet(
         },
     }
 
+    def get_object(self):
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+        if lookup_url_kwarg not in self.kwargs:
+            return
+
+        # generate fake object in case of accessing nested
+        if self.kwargs[lookup_url_kwarg] == 'new':
+            return self.queryset.model()
+
+        return super().get_object()
+
     def get_serializer_class(self):
         serializer_class = None
 
@@ -290,11 +295,13 @@ class EngagementViewSet(
         return context
 
     def get_obj_permission_context(self, obj):
-        return [
+        context = super().get_obj_permission_context(obj)
+        context.extend([
             ObjectStatusCondition(obj),
             AuditStaffMemberCondition(obj.agreement.auditor_firm, self.request.user),
             EngagementStaffMemberCondition(obj, self.request.user),
-        ]
+        ])
+        return context
 
     @list_route(methods=['get'], url_path='partners')
     def partners(self, request, *args, **kwargs):
@@ -411,9 +418,11 @@ class AuditorStaffMembersViewSet(
         return context
 
     def get_obj_permission_context(self, obj):
-        return [
+        context = super().get_obj_permission_context(obj)
+        context.extend([
             AuditStaffMemberCondition(obj.auditor_firm, self.request.user),
-        ]
+        ])
+        return context
 
 
 class BaseAuditAttachmentsViewSet(BaseAuditViewSet,
