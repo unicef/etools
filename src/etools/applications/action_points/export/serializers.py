7afba92996a32c176177e51a78654584bc57cfd1
Domenico DiNicola <domenico.dinicola@wfp.org>
2018-06-22 13:30:08 -0400

    drf 3.8

--- src/etools/applications/action_points/export/serializers.py
+++ src/etools/applications/action_points/export/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 
 class ActionPointExportSerializer(serializers.Serializer):
-    ref = serializers.CharField(source='reference_number')
+    ref = serializers.CharField(source='reference_number', read_only=True)
     cp_output = serializers.CharField(source='cp_output.__str__')
     partner = serializers.CharField(source='partner.name')
     office = serializers.CharField(source='office.name')
@@ -12,12 +12,12 @@ class ActionPointExportSerializer(serializers.Serializer):
     due_date = serializers.DateField(format='%d/%m/%Y')
     status = serializers.CharField(source='get_status_display')
     description = serializers.CharField()
-    intervention = serializers.CharField(source='intervention.reference_number')
+    intervention = serializers.CharField(source='intervention.reference_number', read_only=True)
     pd_ssfa = serializers.CharField(source='intervention.title')
     location = serializers.CharField(source='location.__str__')
     related_module = serializers.CharField()
     assigned_by = serializers.CharField(source='assigned_by.get_full_name')
     date_of_completion = serializers.DateTimeField(format='%d/%m/%Y')
-    related_ref = serializers.CharField(source='related_object.reference_number')
+    related_ref = serializers.CharField(source='related_object.reference_number', read_only=True)
     related_object_str = serializers.CharField()
     related_object_url = serializers.CharField()
