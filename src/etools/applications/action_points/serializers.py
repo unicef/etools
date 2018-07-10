ad68dadb6a2fe9955ed80b1e0ec3576a2ead4106
Roman Karpovich <fpm.th13f@gmail.com>
2018-07-10 10:33:06 +0300

    renamed related module to related app & set correct label for related_object_str

--- src/etools/applications/action_points/serializers.py
+++ src/etools/applications/action_points/serializers.py
@@ -64,7 +64,7 @@ class ActionPointBaseSerializer(UserContextSerializerMixin, SnapshotModelSeriali
 
 
 class ActionPointListSerializer(PermissionsBasedSerializerMixin, ActionPointBaseSerializer):
-    related_module = serializers.ChoiceField(label=_('Related Module'), choices=ActionPoint.MODULE_CHOICES,
+    related_module = serializers.ChoiceField(label=_('Related App'), choices=ActionPoint.MODULE_CHOICES,
                                              read_only=True)
 
     partner = SeparatedReadWriteField(
@@ -143,7 +143,7 @@ class ActionPointSerializer(WritableNestedSerializerMixin, ActionPointListSerial
     comments = CommentSerializer(many=True, label=_('Actions Taken'))
     history = HistorySerializer(many=True, label=_('History'), read_only=True, source='get_meaningful_history')
 
-    related_object_str = serializers.SerializerMethodField(label=_('Reference'))
+    related_object_str = serializers.SerializerMethodField(label=_('Related Document'))
     related_object_url = serializers.SerializerMethodField()
 
     class Meta(WritableNestedSerializerMixin.Meta, ActionPointListSerializer.Meta):
