533ba4f78892cd466b8509624198c1d578ca143c
denes csaba <csaba.denes@nordlogic.com>
2018-07-11 15:44:02 +0300

    make start and end dates not nullable

--- src/etools/applications/reports/models.py
+++ src/etools/applications/reports/models.py
@@ -776,14 +776,8 @@ class ReportingRequirement(TimeStampedModel):
         verbose_name=_("Intervention"),
         related_name="reporting_requirements"
     )
-    start_date = models.DateField(
-        null=True,
-        verbose_name=_('Start Date')
-    )
-    end_date = models.DateField(
-        null=True,
-        verbose_name=_('End Date')
-    )
+    start_date = models.DateField(verbose_name=_('Start Date'))
+    end_date = models.DateField(verbose_name=_('End Date'))
     due_date = models.DateField(verbose_name=_('Due Date'))
     report_type = models.CharField(max_length=50, choices=TYPE_CHOICES)
 
