7d47f2bab7eada14080580cb07afbaf65d75582f
Greg Reinbach <greg@reinbach.com>
2018-08-06 05:54:01 -0400

    Add data migrations for attachments, and file types

--- src/etools/applications/attachments/migrations/0010_auto_20180802_1245.py
+++ src/etools/applications/attachments/migrations/0010_auto_20180802_1245.py
@@ -7,10 +7,44 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
+def migrate_file_types(apps, schema_editor):
+    FileTypeOld = apps.get_model("attachments", "filetype")
+    FileType = apps.get_model("unicef_attachments", "filetype")
+    for f in FileTypeOld.objects.all():
+        FileType.objects.create(
+            id=f.pk,
+            name=f.name,
+            label=f.label,
+            code=f.code,
+            order=f.order,
+        )
+
+
+def migrate_attachments(apps, schema_editor):
+    AttachmentOld = apps.get_model("attachments", "attachment")
+    Attachment = apps.get_model("unicef_attachments", "attachment")
+    FileType = apps.get_model("unicef_attachments", "filetype")
+    for a in AttachmentOld.objects.all():
+        ft = FileType.objects.get(pk=a.file_type.pk)
+        Attachment.objects.create(
+            id=a.pk,
+            file_type=ft,
+            file=a.file,
+            hyperlink=a.hyperlink,
+            content_type=a.content_type,
+            object_id=a.object_id,
+            code=a.code,
+            uploaded_by=a.uploaded_by,
+            created=a.created,
+            modified=a.modified,
+        )
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
         ('attachments', '0009_attachmentflat_agreement_link'),
+        ('unicef_attachments', '0001_initial'),
     ]
 
     operations = [
@@ -29,7 +63,14 @@ class Migration(migrations.Migration):
             name='uploaded_by',
             field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='attachments_old', to=settings.AUTH_USER_MODEL, verbose_name='Uploaded By'),
         ),
-        # TODO add in data migrations here
+        migrations.RunPython(
+            migrate_file_types,
+            migrations.RunPython.noop
+        ),
+        migrations.RunPython(
+            migrate_attachments,
+            migrations.RunPython.noop
+        ),
         migrations.AlterField(
             model_name='attachmentflat',
             name='attachment',
