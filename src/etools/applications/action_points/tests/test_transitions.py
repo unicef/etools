f1ae7ce538bd808a0147159bdab750eb47c54687
Roman Karpovich <fpm.th13f@gmail.com>
2018-08-29 18:05:14 +0300

    allowed to author & assigner to complete action points

--- src/etools/applications/action_points/tests/test_transitions.py
+++ src/etools/applications/action_points/tests/test_transitions.py
@@ -104,7 +104,9 @@ class UnicefUserPermissionsForActionPointTransitionTestCase(ActionPointTransitio
 
 
 class AuthorPermissionsForActionPointTransitionTestCase(ActionPointTransitionPermissionsTestCase):
-    ALLOWED_TRANSITION = []
+    ALLOWED_TRANSITION = [
+        ('open', 'complete'),
+    ]
 
     @classmethod
     def setUpTestData(cls):
@@ -125,7 +127,9 @@ class AuthorPermissionsForActionPointTransitionTestCase(ActionPointTransitionPer
 
 
 class AssignedByPermissionsForActionPointTransitionTestCase(ActionPointTransitionPermissionsTestCase):
-    ALLOWED_TRANSITION = []
+    ALLOWED_TRANSITION = [
+        ('open', 'complete'),
+    ]
 
     @classmethod
     def setUpTestData(cls):
