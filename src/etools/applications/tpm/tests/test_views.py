23db30ff3b7d98e85f7dc3215e001583fd7356ad
Greg Reinbach <greg@reinbach.com>
2018-10-29 05:56:20 -0400

    Add tests for tpm visit filtering
    Sligh cleanup of filter view

--- src/etools/applications/tpm/tests/test_views.py
+++ src/etools/applications/tpm/tests/test_views.py
@@ -17,15 +17,17 @@ from etools.applications.reports.tests.factories import SectionFactory
 from etools.applications.tpm.models import ThirdPartyMonitor, TPMVisit
 from etools.applications.tpm.tests.base import TPMTestCaseMixin
 from etools.applications.tpm.tests.factories import _FUZZY_END_DATE, TPMPartnerFactory, TPMVisitFactory, UserFactory
+from etools.applications.tpm.views import TPMVisitViewSet
 from etools.applications.utils.common.tests.test_utils import TestExportMixin
 
 
 class TestTPMVisitViewSet(TestExportMixin, TPMTestCaseMixin, BaseTenantTestCase):
-    def _test_list_view(self, user, expected_visits):
+    def _test_list_view(self, user, expected_visits, filters=None):
         response = self.forced_auth_req(
             'get',
             reverse('tpm:visits-list'),
-            user=user
+            user=user,
+            data=filters
         )
 
         self.assertEquals(response.status_code, status.HTTP_200_OK)
@@ -52,6 +54,44 @@ class TestTPMVisitViewSet(TestExportMixin, TPMTestCaseMixin, BaseTenantTestCase)
                                 tpm_partner_focal_points=[self.tpm_user.tpmpartners_tpmpartnerstaffmember])
 
         self._test_list_view(self.tpm_user, [visit])
+
+    def test_list_view_filter_single(self):
+        staff = self.tpm_user.tpmpartners_tpmpartnerstaffmember
+        visit_assigned = TPMVisitFactory(
+            status=TPMVisit.ASSIGNED,
+            tpm_partner=staff.tpm_partner,
+            tpm_partner_focal_points=[staff]
+        )
+        visit_reported = TPMVisitFactory(
+            status=TPMVisit.REPORTED,
+            tpm_partner=staff.tpm_partner,
+            tpm_partner_focal_points=[staff]
+        )
+        self._test_list_view(
+            self.tpm_user,
+            [visit_assigned],
+            filters={"status": TPMVisit.ASSIGNED}
+        )
+
+    def test_list_view_filter_multiple(self):
+        staff = self.tpm_user.tpmpartners_tpmpartnerstaffmember
+        visit_assigned = TPMVisitFactory(
+            status=TPMVisit.ASSIGNED,
+            tpm_partner=staff.tpm_partner,
+            tpm_partner_focal_points=[staff]
+        )
+        visit_reported = TPMVisitFactory(
+            status=TPMVisit.REPORTED,
+            tpm_partner=staff.tpm_partner,
+            tpm_partner_focal_points=[staff]
+        )
+        self._test_list_view(
+            self.tpm_user,
+            [visit_assigned, visit_reported],
+            filters={
+                "status__in": ",".join([TPMVisit.ASSIGNED, TPMVisit.REPORTED])
+            }
+        )
 
     def test_list_view_without_tpm_organization(self):
         user = UserFactory(unicef_user=True)
