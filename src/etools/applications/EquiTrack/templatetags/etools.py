b31ba3bbc0b771bd509bfc0e5b55caa08cccf6b2
Domenico DiNicola <dom.dinicola@gmail.com>
2018-10-31 10:09:29 -0400

    git changeset

--- src/etools/applications/EquiTrack/templatetags/etools.py
+++ src/etools/applications/EquiTrack/templatetags/etools.py
@@ -1,3 +1,7 @@
+import os
+import subprocess
+from datetime import datetime
+
 from django import template
 from django.utils.safestring import mark_safe
 
@@ -9,3 +13,23 @@ register = template.Library()
 @register.simple_tag
 def etools_version():
     return mark_safe('{}: v{}'.format(NAME, VERSION))
+
+
+@register.simple_tag
+def git_changeset_date():
+    """Returns a numeric identifier of the latest git changeset.
+
+    The result is the UTC timestamp of the changeset in YYYYMMDDHHMMSS format.
+    This value isn't guaranteed to be unique, but collisions are very unlikely,
+    so it's sufficient for generating the development version numbers.
+    """
+    repo_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    git_log = subprocess.Popen('git log --pretty=format:%ct --quiet -1 HEAD',
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                               shell=True, cwd=repo_dir, universal_newlines=True)
+    timestamp = git_log.communicate()[0]
+    try:
+        timestamp = datetime.utcfromtimestamp(int(timestamp))
+    except ValueError:
+        return None
+    return timestamp.strftime('%Y-%m-%d %H:%M:%S')
