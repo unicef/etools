e83b2abf2448825e22262250dea09311554920e2
Roman Karpovich <fpm.th13f@gmail.com>
2018-10-31 17:44:11 +0300

    cleaned unused imports

--- src/etools/applications/field_monitoring/planning/serializers.py
+++ src/etools/applications/field_monitoring/planning/serializers.py
@@ -1,5 +1,3 @@
-from functools import reduce
-
 from django.core.exceptions import ValidationError as DjangoValidationError
 from django.db.models import Sum
 from django.db.models.expressions import RawSQL
