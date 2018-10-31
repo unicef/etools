d4ee9d55dccde7dd36dc05dc8658e9d4e9ac042f
Domenico DiNicola <dom.dinicola@gmail.com>
2018-10-31 11:04:36 -0400

    move sentry to tenant_support

--- src/etools/libraries/tenant_support/client.py
+++ src/etools/libraries/tenant_support/client.py
@@ -1,10 +1,10 @@
 from raven.contrib.django.raven_compat import DjangoClient
 
 
-class EToolsSentryClient(DjangoClient):
+class DjangoTenantClient(DjangoClient):
 
     def get_data_from_request(self, request):
-        result = super(EToolsSentryClient, self).get_data_from_request(request)
+        result = super().get_data_from_request(request)
         if getattr(request, 'tenant', None):
             if 'extra' not in result:
                 result['extra'] = {}
