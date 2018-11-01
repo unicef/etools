286805a8cd0fe6440293151d5849d75ad9f922f9
Roman Karpovich <fpm.th13f@gmail.com>
2018-11-01 14:56:54 +0300

    fixed urls ordering

--- src/etools/applications/field_monitoring/settings/urls.py
+++ src/etools/applications/field_monitoring/settings/urls.py
@@ -8,13 +8,13 @@ from etools.applications.field_monitoring.settings.views import (
     MethodsViewSet, MethodTypesViewSet, PlannedCheckListItemViewSet,)
 
 root_api = routers.SimpleRouter()
-root_api.register(r'methods', MethodsViewSet, base_name='methods')
 root_api.register(r'methods/types', MethodTypesViewSet, base_name='method-types')
+root_api.register(r'methods', MethodsViewSet, base_name='methods')
 root_api.register(r'sites', LocationSitesViewSet, base_name='sites')
-root_api.register(r'cp-outputs', CPOutputsViewSet, base_name='cp_outputs')
 root_api.register(r'cp-outputs/configs', CPOutputConfigsViewSet, base_name='cp_output-configs')
-root_api.register(r'checklist', CheckListViewSet, base_name='checklist-items')
+root_api.register(r'cp-outputs', CPOutputsViewSet, base_name='cp_outputs')
 root_api.register(r'checklist/categories', CheckListCategoriesViewSet, base_name='checklist-categories')
+root_api.register(r'checklist', CheckListViewSet, base_name='checklist-items')
 
 cp_outputs_configs_api = NestedComplexRouter(root_api, r'cp-outputs/configs', lookup='cp_output_config')
 cp_outputs_configs_api.register(r'planned-checklist', PlannedCheckListItemViewSet, base_name='planned-checklist-items')
