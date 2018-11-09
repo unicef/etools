05794842de5aab942ff3bc0330ba47074d92f2fc
Roman Karpovich <fpm.th13f@gmail.com>
2018-11-09 13:19:20 +0300

    using choices for active in sites

--- src/etools/applications/field_monitoring/settings/serializers/sites.py
+++ src/etools/applications/field_monitoring/settings/serializers/sites.py
@@ -1,3 +1,5 @@
+from django.utils.translation import ugettext_lazy as _
+
 from rest_framework import serializers
 
 from unicef_locations.serializers import LocationSerializer
@@ -7,6 +9,10 @@ from etools.applications.field_monitoring.settings.models import LocationSite
 
 class LocationSiteSerializer(serializers.ModelSerializer):
     parent = LocationSerializer(read_only=True)
+    is_active = serializers.ChoiceField(choices=(
+        (True, _('Active')),
+        (False, _('Inactive')),
+    ), label=_('Status'), required=False)
 
     class Meta:
         model = LocationSite
