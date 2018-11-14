d1266d472d2e257e103e10195559fa37fa1d2e7f
Roman Karpovich <fpm.th13f@gmail.com>
2018-11-14 14:06:36 +0300

    fixed swagger for year plan viewset

--- src/etools/applications/field_monitoring/planning/views.py
+++ src/etools/applications/field_monitoring/planning/views.py
@@ -45,7 +45,7 @@ class YearPlanViewSet(
         queryset = self.filter_queryset(self.get_queryset())
         lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
 
-        if self.kwargs[lookup_url_kwarg] not in self.get_years_allowed():
+        if lookup_url_kwarg not in self.kwargs or self.kwargs[lookup_url_kwarg] not in self.get_years_allowed():
             raise Http404
 
         filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
