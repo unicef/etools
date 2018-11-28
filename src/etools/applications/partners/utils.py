b2a74005db3c263ed74bcd4a23870b8c689c191e
Greg Reinbach <greg@reinbach.com>
2018-11-28 16:51:47 -0500

    Remove sender param in send agreement suspended notification, user default sender

--- src/etools/applications/partners/utils.py
+++ src/etools/applications/partners/utils.py
@@ -402,7 +402,6 @@ def send_agreement_suspended_notification(agreement, user):
         pd_list.append((sections, intervention.reference_number, url))
 
     send_notification_with_template(
-        sender=agreement,
         recipients=[user.email],  # person that initiated this update
         template_name="partners/agreement/suspended",
         context={
