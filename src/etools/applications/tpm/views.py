a9e088512cba399246ef580d4f420c0a0879be37
denes csaba <csaba.denes@nordlogic.com>
2018-12-04 20:57:58 +0200

    faster filter query

--- src/etools/applications/tpm/views.py
+++ src/etools/applications/tpm/views.py
@@ -190,8 +190,9 @@ class TPMPartnerViewSet(
 
     @action(detail=False, methods=['get'], url_path='export', renderer_classes=(TPMPartnerCSVRenderer,))
     def export(self, request, *args, **kwargs):
-        tpm_partners = self.filter_queryset(TPMPartner.objects.all().order_by('vendor_number')).\
-            filter(countries__id__contains=request.user.profile.country.id)
+        tpm_partners = self.filter_queryset(
+            TPMPartner.objects.filter(countries__id__contains=request.user.profile.country.id).order_by('vendor_number')
+        )
         serializer = TPMPartnerExportSerializer(tpm_partners, many=True)
         return Response(serializer.data, headers={
             'Content-Disposition': 'attachment;filename=tpm_vendors_{}.csv'.format(timezone.now().date())
