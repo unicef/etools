499e1d6f7bba9097f1f5bee3041d018293e34dd7
denes csaba <csaba.denes@nordlogic.com>
2018-12-11 22:19:31 +0200

    remove unneded files

--- src/etools/applications/management/management/commands/fix_bad_database_locations.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-from etools.libraries.locations.utils import fix_bad_database_locations
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        fix_bad_database_locations()
--- src/etools/applications/management/management/commands/validate_database_locations.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-from etools.libraries.locations.utils import validate_database_locations
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        validate_database_locations()
