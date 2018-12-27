1da85a4f167996f030ee635b12badc06cec0a168
Roman Karpovich <fpm.th13f@gmail.com>
2018-12-27 11:56:46 +0300

    using serializer for reading config sections

--- src/etools/applications/field_monitoring/fm_settings/serializers/cp_outputs.py
+++ src/etools/applications/field_monitoring/fm_settings/serializers/cp_outputs.py
@@ -13,6 +13,7 @@ from etools.applications.field_monitoring.fm_settings.models import CPOutputConf
 from etools.applications.partners.models import Intervention
 from etools.applications.permissions_simplified.serializers import SafeReadOnlySerializerMixin
 from etools.applications.reports.models import Result
+from etools.applications.reports.serializers.v1 import SectionSerializer
 from etools.applications.reports.serializers.v2 import OutputListSerializer
 from etools.applications.utils.common.urlresolvers import build_frontend_url
 
@@ -50,6 +51,7 @@ class CPOutputConfigSerializer(serializers.ModelSerializer):
         read_field=FMMethodTypeSerializer(many=True),
         label=_('Method(s)')
     )
+    sections = SeparatedReadWriteField(read_field=SectionSerializer())
 
     class Meta:
         model = CPOutputConfig
