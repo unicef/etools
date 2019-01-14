bd968db4e1624451c151b0ce3fbd18626a21444c
Greg Reinbach <greg@reinbach.com>
2019-01-14 05:33:34 -0500

    Exclude trips from document library
    Set source to Trips for travel attachment records

--- src/etools/applications/attachments/tests/test_models.py
+++ src/etools/applications/attachments/tests/test_models.py
@@ -1,11 +1,10 @@
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 from unicef_attachments.utils import get_attachment_flat_model
-from etools.applications.attachments.tests.factories import (
-    AttachmentFactory,
-    AttachmentFileTypeFactory,
-)
+
+from etools.applications.attachments.tests.factories import AttachmentFactory, AttachmentFileTypeFactory
 from etools.applications.EquiTrack.tests.cases import BaseTenantTestCase
+from etools.applications.t2f.tests.factories import TravelAttachmentFactory, TravelFactory
 
 
 class TestAttachmentFlat(BaseTenantTestCase):
@@ -26,3 +25,20 @@ class TestAttachmentFlat(BaseTenantTestCase):
         self.assertTrue(flat_qs.exists())
         flat = flat_qs.first()
         self.assertEqual(str(flat), str(attachment.file))
+
+    def test_trips_excluded(self):
+        travel_attachment = TravelAttachmentFactory(
+            travel=TravelFactory(),
+            type="Travel Attachment PDF",
+        )
+        flat_qs = get_attachment_flat_model().objects
+        flat_count  = flat_qs.count()
+        attachment = AttachmentFactory(
+            file="test_file.pdf",
+            file_type=AttachmentFileTypeFactory(code="t2f_travel_attachment"),
+            code="t2f_travel_attachment",
+            content_object=travel_attachment,
+        )
+
+        self.assertEqual(flat_qs.count(), flat_count)
+        self.assertTrue(attachment not in flat_qs.all())
