533368c75640aa128e776c4bdde72cc5f8b13196
Greg Reinbach <greg@reinbach.com>
2019-02-08 09:37:39 -0500

    Do not allow special characters in PD Indicator target/baseline fields

---
+++ src/etools/applications/reports/validators.py
@@ -0,0 +1,18 @@
+import json
+import re
+
+from django import forms
+
+
+re_allowed_chars = re.compile("^[0-9,\.]+$")
+
+def value_numbers(data):
+    """Ensure that each value in json object is a number"""
+    if isinstance(data, str):
+        try:
+            data = json.loads(data)
+        except ValueError:
+            raise forms.ValidationError("Invalid data")
+    for v in data.values():
+        if not re_allowed_chars.match(str(v)):
+            raise forms.ValidationError("Invalid number")
