735cb35e5aa0fc250ff4aa849039329d2e51e13c
sax <s.apostolico@gmail.com>
2019-02-20 17:11:46 +0100

    update Dockerfile.dev

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,12 @@ if [[ "$*" == "workers" ]];then
 elif [[ "$*" == "beat" ]];then
     celery beat -A etools.config.celery --loglevel=DEBUG --pidfile run/celerybeat.pid
 elif [[ "$*" == "etools" ]];then
+    if [[ -n "${DATABASE_CONN}" ]];then
+        wait-for-it.sh $DATABASE_CONN -t 30
+        if [[ "${RUN_MIGRATIONS}" == "1" ]];then
+            django-admin migrate
+        fi
+    fi
     django-admin collectstatic --noinput
     newrelic-admin run-program gunicorn etools.config.wsgi -b 0.0.0.0:8080 -w 6 --max-requests 100 --timeout=3200 --log-level info
 else
