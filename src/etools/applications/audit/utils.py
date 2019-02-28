9c6bbe1cf0d632aea0bebbe05ddc292ae9c62550
Greg Reinbach <greg@reinbach.com>
2019-02-28 10:20:38 -0500

    Update way generated file is saved
    Ensure file type created properly, with label

--- src/etools/applications/audit/utils.py
+++ src/etools/applications/audit/utils.py
@@ -1,10 +1,10 @@
 import tempfile
 
 from django.contrib.contenttypes.models import ContentType
-from django.core.files import File
+from django.core.files.base import ContentFile
 
 from easy_pdf.rendering import render_to_pdf
-from unicef_attachments.models import Attachment
+from unicef_attachments.models import Attachment, FileType
 
 from etools.applications.attachments.models import generate_file_path
 
@@ -18,14 +18,24 @@ def generate_final_report(obj, code, labels, pdf, template, filename):
     }
 
     content_type = ContentType.objects.get_for_model(obj)
+    file_type, __ = FileType.objects.get_or_create(
+        code=code,
+        defaults={
+            "label": code.replace("_", " ").title(),
+            "name": code.replace("_", " "),
+        }
+    )
     attachment, __ = Attachment.objects.get_or_create(
         code=code,
         content_type=content_type,
         object_id=obj.pk,
+        defaults={
+            "file_type": file_type,
+        }
     )
 
     file_path = generate_file_path(attachment, filename)
-    temp_filename = tempfile.NamedTemporaryFile(suffix="pdf")
-    with open(temp_filename.name, "wb") as fp:
-        attachment.file = File(fp, name=file_path)
-        attachment.file.write(render_to_pdf(template, context))
+    attachment.file.save(
+        file_path,
+        ContentFile(render_to_pdf(template, context)),
+    )
