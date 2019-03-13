510c7a346541c79ff62e0c060ad4822311710a55
Greg Reinbach <greg@reinbach.com>
2019-03-13 11:19:45 -0400

    Add value_none_or_numbersa validator and apply to baseline field

--- src/etools/applications/reports/tests/test_validators.py
+++ src/etools/applications/reports/tests/test_validators.py
@@ -19,9 +19,10 @@ class TestValueNumbers(BaseTenantTestCase):
         d = json.dumps({"v": 123.00, "d": 321})
         self.assertIsNone(validators.value_numbers(d))
 
-    def test_json_none_valid(self):
+    def test_json_none_invalid(self):
         d = json.dumps({"v": None, "d": 321})
-        self.assertIsNone(validators.value_numbers(d))
+        with self.assertRaises(forms.ValidationError):
+            validators.value_numbers(d)
 
     def test_json_invalid(self):
         d = json.dumps({"v": "123.00", "d": "$321,00"})
@@ -41,3 +42,40 @@ class TestValueNumbers(BaseTenantTestCase):
     def test_invalid(self):
         with self.assertRaises(forms.ValidationError):
             validators.value_numbers({"v": "$123.00", "d": "321,00"})
+
+
+class TestValueNoneOrNumbers(BaseTenantTestCase):
+    def test_empty_string(self):
+        with self.assertRaises(forms.ValidationError):
+            (validators.value_none_or_numbers(""))
+
+    def test_json_valid_str(self):
+        d = json.dumps({"v": "123.00", "d": "321,00"})
+        self.assertIsNone(validators.value_none_or_numbers(d))
+
+    def test_json_valid(self):
+        d = json.dumps({"v": 123.00, "d": 321})
+        self.assertIsNone(validators.value_none_or_numbers(d))
+
+    def test_json_none_valid(self):
+        d = json.dumps({"v": None, "d": 321})
+        self.assertIsNone(validators.value_none_or_numbers(d))
+
+    def test_json_invalid(self):
+        d = json.dumps({"v": "123.00", "d": "$321,00"})
+        with self.assertRaises(forms.ValidationError):
+            validators.value_none_or_numbers(d)
+
+    def test_valid_str(self):
+        self.assertIsNone(
+            validators.value_none_or_numbers({"v": "123.00", "d": "321,00"})
+        )
+
+    def test_valid(self):
+        self.assertIsNone(
+            validators.value_none_or_numbers({"v": 123.00, "d": 321})
+        )
+
+    def test_invalid(self):
+        with self.assertRaises(forms.ValidationError):
+            validators.value_none_or_numbers({"v": "$123.00", "d": "321,00"})
