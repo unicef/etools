676c16adaa0c317a467786081f805da801379caa
denes csaba <csaba.denes@nordlogic.com>
2019-04-09 16:48:02 +0300

    fix before_send hook call

--- src/etools/config/sentry.py
+++ src/etools/config/sentry.py
@@ -6,15 +6,17 @@ from sentry_sdk.integrations.django import DjangoIntegration
 from django.conf import settings
 
 if settings.SENTRY_DSN:
-    def _before_send(event):
-        if getattr(event.request, 'tenant', None):
-            with configure_scope() as scope:
-                scope.set_extra("tenant", event.request.tenant.name)
+    def _before_send(event, hint):
+        request = getattr(event, 'request', None)
+        if request:
+            if getattr(request, 'tenant', None):
+                with configure_scope() as scope:
+                    scope.set_extra("tenant", event.request.tenant.name)
         return event
 
     sentry_sdk.init(
         dsn=settings.SENTRY_DSN,
         send_default_pii=True,
         integrations=[DjangoIntegration(), CeleryIntegration()],
-        before_send=_before_send
+        before_send=_before_send,
     )
