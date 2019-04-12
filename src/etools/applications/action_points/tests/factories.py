63477d8c33d2ddb7d9c7686ab95ed60b144e4643
Domenico DiNicola <dom.dinicola@gmail.com>
2019-04-12 13:35:55 -0400

    11091

--- src/etools/applications/action_points/tests/factories.py
+++ src/etools/applications/action_points/tests/factories.py
@@ -4,11 +4,10 @@ from django.utils import timezone
 
 import factory.fuzzy
 from django_comments.models import Comment
-
-from etools.applications.action_points.models import ActionPoint
-from etools.applications.action_points.categories.models import Category
 from unicef_locations.tests.factories import LocationFactory
 
+from etools.applications.action_points.categories.models import Category
+from etools.applications.action_points.models import ActionPoint
 from etools.applications.partners.tests.factories import InterventionFactory, ResultFactory
 from etools.applications.reports.tests.factories import SectionFactory
 from etools.applications.users.tests.factories import UserFactory
