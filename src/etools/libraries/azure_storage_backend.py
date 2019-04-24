9e129149f4ec90e70eb9c5ea274303c8c6d4083d
Domenico DiNicola <dom.dinicola@gmail.com>
2019-04-24 10:48:20 -0400

    upgrade azure-storage

--- src/etools/libraries/azure_storage_backend.py
+++ src/etools/libraries/azure_storage_backend.py
@@ -1,9 +1,10 @@
 import os.path
 from datetime import datetime, timedelta
 
+from azure.storage.blob import BlobPermissions
 from django.utils.deconstruct import deconstructible
 
-from azure.storage import AccessPolicy, SharedAccessPolicy
+from azure.storage import AccessPolicy, SharedAccessSignature
 from storages.backends.azure_storage import AzureStorage
 from storages.utils import setting
 
@@ -27,26 +28,55 @@ class EToolsAzureStorage(AzureStorage):
         super().__init__(*args, **kwargs)
         self._connection = None
 
-    def url(self, name):
+    def url(self, name, expire=None):
         if hasattr(self.connection, 'make_blob_url'):
-            if self.auto_sign:
-                access_policy = AccessPolicy()
-                access_policy.start = (datetime.utcnow() + timedelta(seconds=-120)).strftime('%Y-%m-%dT%H:%M:%SZ')
-                access_policy.expiry = (datetime.utcnow() + timedelta(seconds=self.ap_expiry)).strftime('%Y-%m-%dT%H:%M:%SZ')
-                access_policy.permission = self.azure_access_policy_permission
-                sap = SharedAccessPolicy(access_policy)
-                sas_token = self.connection.generate_shared_access_signature(
-                    self.azure_container,
-                    blob_name=name,
-                    shared_access_policy=sap,
+            name = self._get_valid_path(name)
+
+            if expire is None:
+                expire = self.expiration_secs
+            make_blob_url_kwargs = {}
+            if expire:
+                sap = AccessPolicy(
+                    permission=self.azure_access_policy_permission,
+                    expiry=(datetime.utcnow() + timedelta(seconds=self.ap_expiry)).strftime('%Y-%m-%dT%H:%M:%SZ'),
+                    start=(datetime.utcnow() + timedelta(seconds=-120)).strftime('%Y-%m-%dT%H:%M:%SZ')
                 )
-            else:
-                sas_token = None
-            return self.connection.make_blob_url(
+
+                sas_token = self.service.generate_blob_shared_access_signature(
+                    self.azure_container, name, BlobPermissions.WRITE, expiry=self._expire_at(expire), sap)
+                make_blob_url_kwargs['sas_token'] = sas_token
+
+            if self.azure_protocol:
+                make_blob_url_kwargs['protocol'] = self.azure_protocol
+            return self.service.make_blob_url(
                 container_name=self.azure_container,
                 blob_name=name,
-                protocol=self.azure_protocol,
-                sas_token=sas_token,
-            )
+                **make_blob_url_kwargs)
+
         else:
             return "{}{}/{}".format(setting('MEDIA_URL'), self.azure_container, name)
+
+
+    # def url(self, name):
+    #     if hasattr(self.connection, 'make_blob_url'):
+    #         if self.auto_sign:
+    #             access_policy = AccessPolicy()
+    #             access_policy.start = (datetime.utcnow() + timedelta(seconds=-120)).strftime('%Y-%m-%dT%H:%M:%SZ')
+    #             access_policy.expiry = (datetime.utcnow() + timedelta(seconds=self.ap_expiry)).strftime('%Y-%m-%dT%H:%M:%SZ')
+    #             access_policy.permission = self.azure_access_policy_permission
+    #             sap = SharedAccessPolicy(access_policy)
+    #             sas_token = self.connection.generate_shared_access_signature(
+    #                 self.azure_container,
+    #                 blob_name=name,
+    #                 shared_access_policy=sap,
+    #             )
+    #         else:
+    #             sas_token = None
+    #         return self.connection.make_blob_url(
+    #             container_name=self.azure_container,
+    #             blob_name=name,
+    #             protocol=self.azure_protocol,
+    #             sas_token=sas_token,
+    #         )
+    #     else:
+    #         return "{}{}/{}".format(setting('MEDIA_URL'), self.azure_container, name)
