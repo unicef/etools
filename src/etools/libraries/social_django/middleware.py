3f575c285274a48f5544372f16e03998c7624a4d
Greg Reinbach <greg@reinbach.com>
2019-05-14 08:20:51 -0400

    Add middleware to catch auth cancel exceptions

---
+++ src/etools/libraries/social_django/middleware.py
@@ -0,0 +1,12 @@
+from django.shortcuts import HttpResponseRedirect
+
+from social_core import exceptions as social_exceptions
+from social_django.middleware import SocialAuthExceptionMiddleware
+
+
+class SocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
+    def process_exception(self, request, exception):
+        if hasattr(social_exceptions, 'AuthCanceled'):
+            return HttpResponseRedirect(reverse("dashboard"))
+        else:
+            raise exception
