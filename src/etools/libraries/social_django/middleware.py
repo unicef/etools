a83b4d7b0d599496d8930898d469bc4edc6c1124
Greg Reinbach <greg@reinbach.com>
2019-05-20 13:31:09 -0400

    Update SocialAuthException middleware to redirect to login url

--- src/etools/libraries/social_django/middleware.py
+++ src/etools/libraries/social_django/middleware.py
@@ -1,5 +1,6 @@
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse
+from django.conf import settings
 
 from social_core import exceptions as social_exceptions
 from social_django.middleware import SocialAuthExceptionMiddleware
@@ -8,6 +9,6 @@ from social_django.middleware import SocialAuthExceptionMiddleware
 class SocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
     def process_exception(self, request, exception):
         if hasattr(social_exceptions, 'AuthCanceled'):
-            return HttpResponseRedirect(reverse("dashboard"))
+            return HttpResponseRedirect(reverse(settings.LOGIN_URL))
         else:
             raise exception
