33f33356ec3b40ea96c7d9d63688e405d9d83c29
Greg Reinbach <greg@reinbach.com>
2019-11-12 14:05:51 -0500

    Correct tpm reference number migration

--- src/etools/applications/tpm/migrations/0009_tpmvisit_reference_number.py
+++ src/etools/applications/tpm/migrations/0009_tpmvisit_reference_number.py
@@ -9,7 +9,11 @@ def set_reference_number(apps, schema_editor):
     if connection.tenant.schema_name != SCHEMA_NAME:
         TPMVisit = apps.get_model("tpm", "tpmvisit")
         for visit in TPMVisit.objects.all():
-            visit.reference_number = visit.get_reference_number()
+            visit.reference_number = '{}/{}/{}/TPM'.format(
+                connection.tenant.country_short_code or '',
+                visit.created.year,
+                visit.pk,
+            )
             visit.save()
 
 
