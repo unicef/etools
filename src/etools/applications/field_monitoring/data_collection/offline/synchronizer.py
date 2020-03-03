355e35bbbba3ad5aa3cbe10ad20c913fc6853d76
Roman Karpovich <fpm.th13f@gmail.com>
2020-03-03 13:24:12 +0300

    add protocol to api response url

--- src/etools/applications/field_monitoring/data_collection/offline/synchronizer.py
+++ src/etools/applications/field_monitoring/data_collection/offline/synchronizer.py
@@ -1,5 +1,6 @@
 import json
 from typing import List, TYPE_CHECKING
+from urllib.parse import urljoin
 
 from django.conf import settings
 from django.db import connection
@@ -37,6 +38,11 @@ class MonitoringActivityOfflineSynchronizer:
         if not self.enabled:
             return
 
+        # absolute host should be provided, so we have to add protocol to domain
+        host = settings.HOST
+        if not host.startswith('http'):
+            host = 'https://' + host
+
         for method in self.activity.methods:
             blueprint = get_blueprint_for_activity_and_method(self.activity, method)
             OfflineCollect().add(data={
@@ -45,11 +51,17 @@ class MonitoringActivityOfflineSynchronizer:
                 "form_title": blueprint.title,
                 "form_instructions": json.dumps(blueprint.to_dict(), indent=2),
                 "accessible_by": self._get_data_collectors(),
-                "api_response_url": '{}{}?workspace={}'.format(
-                    settings.HOST,
-                    reverse('field_monitoring_data_collection:activities-offline', args=[self.activity.id, method.id]),
-                    connection.tenant.schema_name or ''
-                )
+                "api_response_url":
+                    urljoin(
+                        host,
+                        '{}?workspace={}'.format(
+                            reverse(
+                                'field_monitoring_data_collection:activities-offline',
+                                args=[self.activity.id, method.id]
+                            ),
+                            connection.tenant.schema_name or ''
+                        )
+                    )
             })
 
     def update_data_collectors_list(self) -> None:
