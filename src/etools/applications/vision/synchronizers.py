ecf1eada083d95f9b889942a9229e36ea57b7f36
Domenico DiNicola <dom.dinicola@gmail.com>
2020-07-14 14:31:24 -0400

    remove apim

--- src/etools/applications/vision/synchronizers.py
+++ src/etools/applications/vision/synchronizers.py
@@ -19,14 +19,6 @@ class VisionDataTenantSynchronizer(VisionDataSynchronizer):
         self.country = get_tenant_model().objects.get(business_area_code=self.business_area_code)
         connection.set_tenant(self.country)
 
-    def _get_kwargs(self):
-        kwargs = super()._get_kwargs()
-        kwargs.update({
-            'url': settings.INSIGHT_URL,
-            'headers': (('Ocp-Apim-Subscription-Key', settings.INSIGHT_SUB_KEY), )
-        })
-        return kwargs
-
     def logger_parameters(self):
         return {
             'handler_name': self.__class__.__name__,
