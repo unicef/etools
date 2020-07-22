4b7633618574416185cb879d60ce095216dc75fa
Greg Reinbach <greg@reinbach.com>
2020-07-22 09:00:03 -0400

    Add technical guidance etc section fields to intervention model

--- src/etools/applications/partners/models.py
+++ src/etools/applications/partners/models.py
@@ -1982,6 +1982,22 @@ class Intervention(TimeStampedModel):
         choices=REVIEW_TYPE_CHOICES,
         default=REVIEW_TYPE_NONE,
     )
+    capacity_development = models.TextField(
+        verbose_name=_("Capacity Development"),
+        blank=True,
+    )
+    other_info = models.TextField(
+        verbose_name=_("Other Info"),
+        blank=True,
+    )
+    other_partners_involved = models.TextField(
+        verbose_name=_("Other Partners Involved"),
+        blank=True,
+    )
+    technical_guidance = models.TextField(
+        verbose_name=_("Technical Guidance"),
+        blank=True,
+    )
 
     # Flag if this has been migrated to a status that is not correct
     # previous status
