3e953a0cd005916efd2f477eab63705dafc750e6
Roman Karpovich <fpm.th13f@gmail.com>
2020-10-01 14:14:06 +0300

    returned import back

--- src/etools/applications/users/tests/test_views_v3.py
+++ src/etools/applications/users/tests/test_views_v3.py
@@ -8,6 +8,7 @@ from rest_framework import status
 from etools.applications.audit.models import Auditor
 from etools.applications.audit.tests.factories import AuditorUserFactory
 from etools.applications.core.tests.cases import BaseTenantTestCase
+from etools.applications.partners.tests.factories import PartnerFactory
 from etools.applications.tpm.tests.factories import SimpleTPMPartnerFactory, TPMPartnerStaffMemberFactory
 from etools.applications.users.models import UserProfile
 from etools.applications.users.serializers_v3 import AP_ALLOWED_COUNTRIES
