8370253040b661fccb874a26e5fcbcdeb33221cb
Greg Reinbach <greg@reinbach.com>
2020-11-13 10:11:27 -0500

    Update migration dependency

--- src/etools/applications/partners/migrations/0074_interventionsupplyitem_unicef_product_number.py
+++ src/etools/applications/partners/migrations/0074_interventionsupplyitem_unicef_product_number.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('partners', '0073_merge_20201112_1522'),
+        ('partners', '0073_merge_20201112_0759'),
     ]
 
     operations = [
