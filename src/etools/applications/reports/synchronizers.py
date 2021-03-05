3127862e1baf7c240f58f70beebcf2234f8240d8
Domenico <dom.dinicola@gmail.com>
2021-03-05 16:03:15 +0100

    25163 fix sync

--- src/etools/applications/reports/synchronizers.py
+++ src/etools/applications/reports/synchronizers.py
@@ -326,7 +326,7 @@ class RAMSynchronizer(VisionDataTenantSynchronizer):
     def _filter_records(self, records):
         def is_valid_record(record):
             for key in self.REQUIRED_KEYS:
-                if key not in record:
+                if key not in record or not record[key]:
                     return False
             if record['INDICATOR_DESCRIPTION'] in ['', None] or record["INDICATOR_CODE"] in ['undefined', '', None]:
                 return False
