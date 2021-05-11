4c6d27c0e11db7f0ebb79f27158065a37c97651b
Roman Karpovich <fpm.th13f@gmail.com>
2021-05-11 18:17:08 +0300

    intervention amendment merge: simple fields + many to one

--- src/etools/applications/partners/models.py
+++ src/etools/applications/partners/models.py
@@ -2463,13 +2463,58 @@ def copy_simple_fields(instance, instance_copy, fields_map, exclude=None):
 
             if isinstance(value, datetime.datetime):
                 value = value.isoformat()
-            if isinstance(value, datetime.date):
+            elif isinstance(value, datetime.date):
                 value = value.isoformat()
             elif isinstance(value, decimal.Decimal):
                 value = value.to_eng_string()
             elif isinstance(value, FieldFile):
                 value = value.name
             fields_map[field.name] = value
+
+
+def merge_simple_fields(instance, instance_copy, fields_map, exclude=None):
+    exclude = exclude or []
+    for field in instance._meta.get_fields():
+        field_is_simple = not (field.many_to_many or field.many_to_one or field.one_to_many or field.one_to_one)
+        if field.name not in exclude and field.concrete and field_is_simple and not field.auto_created:
+            original_value = fields_map.get(field.name)
+            if not original_value:
+                # field added after amendment created
+                continue
+
+            current_value = getattr(instance, field.name)
+            modified_value = getattr(instance_copy, field.name)
+
+            # todo: field type should be checked better, not value
+            if original_value:
+                if isinstance(current_value, datetime.datetime):
+                    original_value = datetime.datetime.fromisoformat(original_value)
+                elif isinstance(current_value, datetime.date):
+                    original_value = datetime.date.fromisoformat(original_value)
+                elif isinstance(current_value, decimal.Decimal):
+                    original_value = decimal.Decimal(original_value)
+                elif isinstance(current_value, FieldFile):
+                    # todo: compare code should be modified also
+                    # original_value = original_value.name
+                    continue
+
+            if original_value == modified_value:
+                # nothing changed
+                continue
+
+            if modified_value == current_value:
+                # same field changes already performed
+                continue
+
+            if current_value != original_value:
+                # value changed in both objects, cannot be merged automatically
+                raise ValueError(f'field {field.name} was changed in both instances of {type(instance)}. '
+                                 f'was {original_value}, now {current_value} and {modified_value}')
+
+            setattr(instance, field.name, modified_value)
+
+        # todo: figure out best place for saving
+        instance.save()
 
 
 def copy_one_to_many(instance, instance_copy, related_name, fields_map, relations_to_copy, exclude_fields, kwargs):
@@ -2518,6 +2563,7 @@ def copy_instance(instance, relations_to_copy, exclude_fields, kwargs):
             # todo: not only simple fields can be required
             copy_map[field.name] = {}
             related_instance_copy = getattr(instance_copy, field.name)
+            # todo: use exclude
             copy_simple_fields(getattr(instance, field.name), related_instance_copy, copy_map[field.name])
             related_instance_copy.save()
 
@@ -2528,12 +2574,69 @@ def copy_instance(instance, relations_to_copy, exclude_fields, kwargs):
 
         if field.many_to_many:
             # link all related instances with copy
-            print(f'many_to_many {field.name}')
             copy_m2m_relations(instance, instance_copy, [field.name], copy_map)
 
     return instance_copy, copy_map
 
-    # for field in instance._meta.get_fields():
+
+def merge_instance(instance, instance_copy, fields_map, relations_to_copy, exclude_fields):
+    related_fields_to_copy = relations_to_copy.get(type(instance), [])
+    fields_to_exclude = exclude_fields.get(type(instance), [])
+
+    merge_simple_fields(instance, instance_copy, fields_map, exclude=fields_to_exclude)
+
+    for field in instance._meta.get_fields():
+        if field.name in fields_to_exclude or field.name not in related_fields_to_copy:
+            continue
+
+        if field.name not in fields_map:
+            # field added after amendment
+            continue
+
+        if field.many_to_one:
+            original_value = field.related_model.objects.get(pk=fields_map[field.name])
+            modified_value = getattr(instance_copy, field.name)
+            current_value = getattr(instance, field.name)
+            if original_value == modified_value:
+                # nothing changed
+                continue
+
+            if modified_value == current_value:
+                # same field changes already performed
+                continue
+
+            if current_value != original_value:
+                # value changed in both objects, cannot be merged automatically
+                raise ValueError(f'field {field.name} was changed in both instances of {type(instance)}. '
+                                 f'was {original_value}, now {current_value} and {modified_value}')
+
+            setattr(instance, field.name, modified_value)
+
+    instance.save()
+
+    for field in instance._meta.get_fields():
+        if field.name in fields_to_exclude or field.name not in related_fields_to_copy:
+            continue
+
+        if field.one_to_one:
+            # todo: if one of related objects is missing, raise error
+
+            related_instance = getattr(instance, field.name)
+            related_instance_copy = getattr(instance_copy, field.name)
+            merge_simple_fields(
+                related_instance,
+                related_instance_copy,
+                fields_map[field.name],
+                exclude=exclude_fields.get(type(related_instance), [])
+            )
+
+        if field.one_to_many:
+            # todo: merge all related instances
+            print(f'one_to_many {field.name}')
+
+        if field.many_to_many:
+            # todo: calculate diference and update m2m links
+            print(f'many_to_many {field.name}')
 
 
 class InterventionAmendment(TimeStampedModel):
@@ -2726,9 +2829,48 @@ class InterventionAmendment(TimeStampedModel):
                     activity_copy.time_frames.add(*self.amended_intervention.quarters.filter(quarter__in=quarters))
                     activity_data['quarters'] = quarters
 
-    def merge_amendment(self, amendment):
-        # todo
-        pass
+    def _merge_amendment(self):
+        merge_instance(
+            self.intervention,
+            self.amended_intervention,
+            self.related_objects_map,
+            {
+                Intervention: [
+                    # one to many
+                    'result_links',
+                    'supply_items',
+                    'risks',
+
+                    # 1 to 1
+                    'planned_budget',
+                    'management_budgets',
+
+                    # many to one
+                    'agreement',
+                    'budget_owner',
+
+                    # many to many
+                    'country_programmes', 'unicef_focal_points', 'partner_focal_points',
+                    'sections', 'offices', 'flat_locations'
+                ],
+                InterventionResultLink: [
+                    # one to many
+                    'll_results',
+
+                    # many to many
+                    'ram_indicators',
+                ],
+                LowerResult: [
+                    # one to many
+                    'activities',
+                ]
+            },
+            {
+                Intervention: ['number', 'status', 'created', 'modified'],
+                InterventionBudget: ['created', 'modified'],
+                InterventionManagementBudget: ['created', 'modified'],
+            }
+        )
 
 
 class InterventionPlannedVisits(TimeStampedModel):
