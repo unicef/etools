d4d9e30a2cdf67f4ab0ff5b07226dcdaff172a64
Roman Karpovich <fpm.th13f@gmail.com>
2021-07-22 14:56:03 +0300

    eface form admin panel

---
+++ src/etools/applications/eface/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from etools.applications.eface.models import EFaceForm, FormActivity
+
+
+class FormActivityAdmin(admin.StackedInline):
+    model = FormActivity
+
+
+@admin.register(EFaceForm)
+class EFaceFormAdmin(admin.ModelAdmin):
+    list_display = ('reference_number', 'title', 'request_type', 'status')
+    list_filter = ('status',)
+    search_fields = ('reference_number',)
