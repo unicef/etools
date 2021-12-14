f816374dd159664eba0b07e0ed4a9bf01f73f54a
Domenico <dom.dinicola@gmail.com>
2021-12-14 15:22:08 -0600

    fix coreapi check

--- src/etools/applications/audit/conditions.py
+++ src/etools/applications/audit/conditions.py
@@ -13,8 +13,10 @@ class AuditStaffMemberCondition(SimpleCondition):
         self.user = user
 
     def is_satisfied(self):
-        return self.partner.staff_members.exists() and self.user.pk in self.partner.staff_members.values_list(
-            'user', flat=True)
+        return \
+            self.partner and \
+            self.partner.staff_members.exists() and \
+            self.user.pk in self.partner.staff_members.values_list('user', flat=True)
 
 
 class EngagementStaffMemberCondition(SimpleCondition):
