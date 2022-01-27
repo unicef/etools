66e610add2ed2aea40c873c4064a2b8eb2dafd08
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2022-01-27 15:10:49 +0200

    [ch27982] Spike perms: update migration; flake8

--- src/etools/applications/users/admin.py
+++ src/etools/applications/users/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin, messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import Group
 from django.db import connection
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
