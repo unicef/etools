550c6fa91642fd4fabfee623e34670242ebf0a81
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2022-06-30 13:24:35 +0300

    [ch28948] Core: run test in parallel in circle CI

--- runtests.sh
+++ runtests.sh
@@ -20,6 +20,7 @@ fi
 
 # Run unittests and coverage report
 coverage erase
-time coverage run manage.py test --noinput --keepdb "$@"
+time coverage run --parallel-mode --concurrency=multiprocessing manage.py test --noinput --keepdb --parallel=6 "$@"
+coverage combine
 coverage report -m
 coverage html
