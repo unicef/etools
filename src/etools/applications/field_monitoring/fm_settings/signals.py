35040ffd968999669b92ad965abcfe618097e900
Roman Karpovich <fpm.th13f@gmail.com>
2022-10-12 09:33:31 +0800

    add tenant suffix into sites cache key

--- src/etools/applications/field_monitoring/fm_settings/signals.py
+++ src/etools/applications/field_monitoring/fm_settings/signals.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
@@ -10,10 +11,10 @@ from etools.libraries.views.cache import invalidate_view_cache
 @receiver(post_save, sender=LocationSite)
 def update_location_site_cache_on_save(instance, created, **kwargs):
     invalidate_cache()
-    invalidate_view_cache('fm-sites')
+    invalidate_view_cache('fm-sites.{}'.format(connection.tenant.country_short_code or ''))
 
 
 @receiver(post_delete, sender=LocationSite)
 def update_location_site_cache_on_delete(instance, **kwargs):
     invalidate_cache()
-    invalidate_view_cache('fm-sites')
+    invalidate_view_cache('fm-sites.{}'.format(connection.tenant.country_short_code or ''))
--- src/etools/libraries/views/cache.py
+++ src/etools/libraries/views/cache.py
@@ -6,7 +6,7 @@ def invalidate_view_cache(key_prefix, cache_alias=None):
     invalidates cache created by django.views.decorators.cache.cache_page
     url as well as context are variable, depending on url parameters, so wildcard them out and rely on key prefix only
     key example:
-    views.decorators.cache.cache_page.list.GET.ecb3b6ea0b1e4134ca2043e76e2ac5d5.d96d7cd887705ba571615e93962d710e.en-us.UTC
+    views.decorators.cache.cache_page.fm-sites.NPL.GET.26d04fa6a67a4cb34885a9c5b1206e39.d96d7cd887705ba571615e93962d710e.en-us.UTC
     """
     page_cache_pattern = 'views.decorators.cache.cache_page.{0}.*'.format(key_prefix)
     headers_cache_pattern = 'views.decorators.cache.cache_header.{0}.*'.format(key_prefix)
