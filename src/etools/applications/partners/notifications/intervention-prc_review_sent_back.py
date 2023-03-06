a16fe4610cb9d7aca918c7ac6c9100ca24ea74fa
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2023-03-06 14:54:37 +0200

    Updated partners email notifications

--- src/etools/applications/partners/notifications/intervention-prc_review_sent_back.py
+++ src/etools/applications/partners/notifications/intervention-prc_review_sent_back.py
@@ -4,7 +4,10 @@ defaults = {
     "subject": "[eTools] PD Sent Back by Secretary",
     "content": """
     Dear Colleague,
-    The PRC secretary has sent back the request for PRC Approval on PD {{reference_number}}. Please review the comments here: [[PD url to /review - url for unicef (includes pmp)]] {{pd_link}}
+
+    The PRC secretary has sent back the request for PRC Approval on PD {{reference_number}}. Please review and address the comments here: [[PD url to /review - url for unicef (includes pmp)]] {{pd_link}}
+
+    This action cancelled all the previous approvals and returned the PD to the `Development phase. Please complete all the desired/recommended changes. When all changes are completed, the PD will need to be "accepted as final" by both the Partner and UNICEF and resubmitted for "Review".
 
     Please do not reply to this email.
     """,
@@ -12,7 +15,9 @@ defaults = {
     {% extends "email-templates/base" %}
     {% block content %}
     Dear Colleague,<br />
-    The PRC secretary has sent back the request for PRC Approval on PD {{reference_number}}. Please review the comments here: [[PD url to /review - url for unicef (includes pmp)]]  {{pd_link}}<br /><br />
+    The PRC secretary has sent back the request for PRC Approval on PD {{reference_number}}. Please review and address the comments here: [[PD url to /review - url for unicef (includes pmp)]] {{pd_link}}<br />
+
+    This action cancelled all the previous approvals and returned the PD to the `Development phase. Please complete all the desired/recommended changes. When all changes are completed, the PD will need to be "accepted as final" by both the Partner and UNICEF and resubmitted for "Review".<br />
 
     Please do not reply to this email.<br />
     {% endblock content %}
