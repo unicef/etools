015e2ab2e391443004ee2809fd5900daaf362e85
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2023-04-24 16:13:54 +0300

    add INOPOERATIVE status filter for users/amp/

--- src/etools/applications/users/filters.py
+++ src/etools/applications/users/filters.py
@@ -13,14 +13,17 @@ class UserRoleFilter(BaseFilterBackend):
 class UserStatusFilter(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         if 'status' in request.query_params and request.query_params['status']:
-            status_list = request.query_params['status'].split(',')
+            queryset = queryset.prefetch_related('realms')
+            status_list = [status.strip().upper() for status in request.query_params['status'].split(',')]
             filters = models.Q()
             for status in status_list:
-                if status == 'ACTIVE':
-                    filters |= models.Q(is_active=True, last_login__isnull=False)
                 if status == 'INACTIVE':
                     filters |= models.Q(is_active=False)
+                if status == 'ACTIVE':
+                    filters |= models.Q(is_active=True, last_login__isnull=False)
                 if status == 'INVITED':
                     filters |= models.Q(is_active=True, last_login__isnull=True)
+                if status == 'INOPERATIVE':
+                    filters |= models.Q(is_active=True, realms__is_active=False)
             return queryset.filter(filters)
         return queryset
