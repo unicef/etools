c78bd6efaddbc6dcb494d1497e080ed25fc229a1
Robert Avram <robertavram@users.noreply.github.com>
2023-05-19 11:42:21 -0400

    Update apps.py

--- src/etools/applications/users/apps.py
+++ src/etools/applications/users/apps.py
@@ -2,6 +2,8 @@ from django.apps import AppConfig as BaseAppConfig
 
 
 class AppConfig(BaseAppConfig):
+    name = __name__.rpartition('.')[0]
+    verbose_name = 'Users'
 
     def ready(self):
         from . import signals  # NOQA
