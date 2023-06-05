675c978fd8c957106edbe25960c0bb0d5421e0d2
Robert Avram <robertavram@users.noreply.github.com>
2023-06-05 14:09:56 -0400

    isort

--- src/etools/applications/locations/admin.py
+++ src/etools/applications/locations/admin.py
@@ -3,7 +3,7 @@ from django.contrib.gis import admin
 
 from admin_extra_urls.decorators import button
 from celery import chain
-from unicef_locations.admin import CartoDBTableAdmin, LocationAdmin, ActiveLocationsFilter
+from unicef_locations.admin import ActiveLocationsFilter, CartoDBTableAdmin, LocationAdmin
 from unicef_locations.models import CartoDBTable
 
 from etools.applications.locations.models import Location
