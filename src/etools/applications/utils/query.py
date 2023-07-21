f7c2cf5757a7e5aaeb11ac7c8a91a4b8bfe00fda
Robert Avram <robertavram@users.noreply.github.com>
2023-07-21 10:22:15 -0400

    add comments

--- src/etools/applications/utils/query.py
+++ src/etools/applications/utils/query.py
@@ -3,12 +3,17 @@ from django.db import models
 
 
 def has_related_records(queryset, model, avoid_self=True):
+    '''
+    Function that returns a tuple, whether the queryset has records that other models foreign key into (or m2m)
+    (Bool - whether it has any records, [list of pks] - all records that other model instances relating to them)
+    '''
     all_impacted_records = []
     # Get all related objects' content types
     related_content_types = ContentType.objects.filter(
         models.Q(model__in=[field.related_model._meta.object_name.lower() for field in model._meta.get_fields()
                             if field.one_to_many or field.many_to_many])
     )
+    # filter out all deprecated models that no longer exit. rct.model_class would return None if they are no longer
     related_content_types = [rct for rct in related_content_types if rct.model_class()]
     if avoid_self:
         related_content_types = [rct for rct in related_content_types if rct.model_class() != model]
@@ -20,8 +25,7 @@ def has_related_records(queryset, model, avoid_self=True):
         rel_field_names = [f.name for f in rel_model_fields if f.remote_field and f.remote_field.model == model]
         # Check if there are any related records for the current queryset
         for related_field_name in rel_field_names:
-            print(f'model {related_model} field: {related_field_name}')
-
+            # (f'model {related_model} field: {related_field_name}') - "model Intervention field flat_locations"
             if related_model.objects.filter(**{f'{related_field_name}__in': queryset}).exists():
                 all_impacted_records += related_model.objects.filter(**{f'{related_field_name}__in': queryset}).\
                     values_list(f'{related_field_name}__pk', flat=True)
