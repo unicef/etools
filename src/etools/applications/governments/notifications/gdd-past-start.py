f3d16a8b8e60ebdde1b91c4c137ce446cad82e63
Mercea Emil <office@insightfeatures.com>
2025-05-05 11:17:14 +0300

    Update notifications texts from GDD to GPD, from Government Digital Document to Government Programme Document

--- src/etools/applications/governments/notifications/gdd-past-start.py
+++ src/etools/applications/governments/notifications/gdd-past-start.py
@@ -1,12 +1,12 @@
 name = 'governments/gdd/past-start'
 defaults = {
     'description': 'Intervention past start date.',
-    'subject': 'eTools GDD Past Start Notification',
+    'subject': 'eTools GPD Past Start Notification',
     'content': """
     Dear Colleague,
 
-    Please note that the Partnership ref. {{ reference_number }} {{ title }} with {{ partner_name }} is approved, the start date for the GDD is {{ start_date }}. However, there is no FR associated with this partnership in eTools.
-    Please log into eTools and add the FR number to the record, so that the Government Digital Document/GDD status can progress to active status.
+    Please note that the Partnership ref. {{ reference_number }} {{ title }} with {{ partner_name }} is approved, the start date for the GPD is {{ start_date }}. However, there is no FR associated with this partnership in eTools.
+    Please log into eTools and add the FR number to the record, so that the Government Programme Document/GPD status can progress to active status.
 
     Please follow the link below to add the FR information to your document.
 
