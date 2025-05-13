e3368ab4924eaf930cb4b21bf19501575f7199e2
Mercea Emil <office@insightfeatures.com>
2025-05-13 10:42:29 +0300

    [ch39959] Return to Unicef

--- src/etools/applications/governments/urls.py
+++ src/etools/applications/governments/urls.py
@@ -39,6 +39,7 @@ from etools.applications.governments.views.gdd_actions import (
     GDDSendBackViewReview,
     GDDSendToPartnerView,
     GDDSendToUNICEFView,
+    GDDReturnToUNICEFView,
     GDDSignatureView,
     GDDSuspendView,
     GDDTerminateView,
@@ -273,6 +274,13 @@ urlpatterns = [
             http_method_names=['patch'],
         ),
         name='gdd-send-unicef',
+    ),
+    path(
+        'gdds/<int:pk>/return_to_unicef/',
+        view=GDDReturnToUNICEFView.as_view(
+            http_method_names=['patch'],
+        ),
+        name='gdd-return-unicef',
     ),
     path(
         'gdds/<int:gdd_pk>/amendments/',
