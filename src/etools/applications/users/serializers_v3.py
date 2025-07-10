9e8ba581f352e67088c8a9ba1728c21bba86a4a5
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2025-07-10 12:13:01 +0300

    flake8 .

--- src/etools/applications/users/serializers_v3.py
+++ src/etools/applications/users/serializers_v3.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from rest_framework import serializers
-from rest_framework.exceptions import PermissionDenied, ValidationError
+from rest_framework.exceptions import ValidationError
 
 from etools.applications.audit.models import Auditor
 from etools.applications.environment.helpers import tenant_switch_is_active
