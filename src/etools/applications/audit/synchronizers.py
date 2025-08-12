aee32cd659c5ed97f4039e0904d28e8a72ef627a
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2025-08-12 18:26:29 +0300

    FaceFormsSynchronizer changes

--- src/etools/applications/audit/synchronizers.py
+++ src/etools/applications/audit/synchronizers.py
@@ -6,6 +6,7 @@ from unicef_vision.settings import INSIGHT_DATE_FORMAT
 from unicef_vision.utils import comp_decimals
 
 from etools.applications.audit.models import FaceForm
+from etools.applications.partners.models import PartnerOrganization
 from etools.applications.vision.synchronizers import VisionDataTenantSynchronizer
 
 
@@ -27,22 +28,32 @@ class FaceFormsSynchronizer(VisionDataTenantSynchronizer):
         "face_number": "FACE_FORM_NUMBER",
         "partner": "IMPLEMENTING_PARNTER_CODE",
         "start_date": "REPORTING_START_DATE",
-        "end_date": "REPORTING_END_DATE",
+        # "end_date": "REPORTING_END_DATE",
         "date_of_liquidation": "REPORTING_POSTING_DATE",
         "modality": "DOCUMENT_TYPE_DESC",
         "currency": "CURRENCY",
         "amount_usd": "OVERALL_AMOUNT_USD",
         "amount_local": "OVERALL_AMOUNT"
     }
-
-    FUNDING_FIELDS = ['OVERALL_AMOUNT', 'OVERALL_AMOUNT_USD', 'CURRENCY']
+    FACE_FORM_FIELDS = ["FACE_FORM_NUMBER", "IMPLEMENTING_PARNTER_CODE", "DOCUMENT_TYPE_DESC", "CURRENCY",
+                        "REPORTING_POSTING_DATE", "REPORTING_START_DATE"]
+    FUNDING_FIELDS = ['OVERALL_AMOUNT', 'OVERALL_AMOUNT_USD']
 
     def __init__(self, *args, **kwargs):
         self.face_records = {}
         self.funding_records = {}
         self.REVERSE_MAPPING = {v: k for k, v in self.MAPPING.items()}
+        self.REVERSE_FACE_FORM_FIELDS = [self.REVERSE_MAPPING[v] for v in self.FACE_FORM_FIELDS]
         self.REVERSE_FUNDING_FIELDS = [self.REVERSE_MAPPING[v] for v in self.FUNDING_FIELDS]
         super().__init__(*args, **kwargs)
+
+    def set_kwargs(self, **kwargs):
+        kwargs['endpoint'] = self.ENDPOINT
+        if self.detail:
+            kwargs['detail'] = self.detail
+        if self.business_area_code:
+            kwargs['businessareacode'] = self.business_area_code
+        return kwargs
 
     def _fill_required_keys(self, record):
         for req_key in self.REQUIRED_KEYS:
@@ -63,32 +74,44 @@ class FaceFormsSynchronizer(VisionDataTenantSynchronizer):
         return json_records
 
     def _filter_records(self, records):
-        def bad_record(record):
+        def is_valid_record(record):
             for key in self.REQUIRED_KEYS:
-                if record[key] is None or record[key] == "":
+                if key not in record or record[key] is None or record[key] == "":
                     return False
             return True
 
-        return [rec for rec in records if bad_record(rec)]
+        return [rec for rec in records if is_valid_record(rec)]
+
+    def set_mapping(self, records):
+        self.face_records = {}
+        for r in records:
+            self.face_records[r['FACE_FORM_NUMBER']] = self.map_face_from_record(r)
 
     @staticmethod
     def get_value_for_field(field, value):
         if field in ['start_date', 'end_date', 'date_of_liquidation']:
             return datetime.datetime.strptime(value, INSIGHT_DATE_FORMAT).date()
+        if field == 'partner':
+            return PartnerOrganization.objects.filter(vendor_number=value).first()
         return value
 
     def map_face_from_record(self, record):
-        return {k: self.get_value_for_field(k, record.get(self.MAPPING[k]))
-                for k in self.REQUIRED_KEYS}
-
-    def map_funding_item_record(self, record):
-        r = {k: self.get_value_for_field(k, record.get(self.MAPPING[k]))
-             for k in self.REVERSE_FUNDING_FIELDS}
-        return r
+        face_dict = {k: self.get_value_for_field(k, record.get(self.MAPPING[k]))
+                     for k in self.REVERSE_FACE_FORM_FIELDS}
+        face_dict['amount_local'] = 0
+        face_dict['amount_usd'] = 0
+        if ('HACT_FUNDINGS' in record and
+                'TYPE_HACT_FUNDING' in record['HACT_FUNDINGS'] and
+                isinstance(record['HACT_FUNDINGS']['TYPE_HACT_FUNDING'], list)):
+            funding_items = record['HACT_FUNDINGS']['TYPE_HACT_FUNDING']
+            for funding_item in funding_items:
+                face_dict['amount_local'] += Decimal(funding_item.get('OVERALL_AMOUNT', 0))
+                face_dict['amount_usd'] += Decimal(funding_item.get('OVERALL_AMOUNT_USD', 0))
+        return face_dict
 
     @staticmethod
     def equal_fields(field, obj_field, record_field):
-        if field in ['overall_amount', 'overall_amount_usd']:
+        if field in ['amount_local', 'amount_usd']:
             return comp_decimals(obj_field, record_field)
         return obj_field == record_field
 
@@ -101,30 +124,26 @@ class FaceFormsSynchronizer(VisionDataTenantSynchronizer):
         return updates
 
     def face_form_sync(self):
-        to_update, to_create = [], []
+        updated = 0
+        existing_face = FaceForm.objects.filter(face_number__in=self.face_records.keys())
+        for face in existing_face:
+            if self.update_obj(face, self.face_records.get(face.face_number)):
+                face.save()
+                updated += 1
+            self.face_records.pop(face.face_number)
 
-        return len(to_update), len(to_create)
+        to_create = [FaceForm(**new_face) for new_face in self.face_records.values() if new_face['partner'] is not None]
+        FaceForm.objects.bulk_create(to_create)
 
-    @staticmethod
-    def update_amounts():
-        totals_updated = 0
-        qs = FaceForm.objects
-        for fr in qs:
-            # Note that Sum() returns None, not 0, if there's nothing to sum.
-            total_li_sum = fr.my_li_total_sum or Decimal('0.00')
-            if not comp_decimals(total_li_sum, fr.total_amt_local):
-                fr.total_amt_local = total_li_sum
-                fr.save()
-                totals_updated += 1
-        return totals_updated
+        return updated, len(to_create)
 
     def _save_records(self, records):
         filtered_records = self._filter_records(records)
         self.set_mapping(filtered_records)
         _processed = self.face_form_sync()
 
-        logging.info('tocreate {}'.format(_processed[1]))
-        logging.info('toupdate {}'.format(_processed[0]))
+        logging.info('to create: {}'.format(_processed[1]))
+        logging.info('to update: {}'.format(_processed[0]))
         processed = _processed[0] + _processed[1]
 
         return processed
