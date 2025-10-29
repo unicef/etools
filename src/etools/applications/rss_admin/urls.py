de2542bcaf073e45057daf50a7bc6ec3ce5e4e74
alex-run-code <alex-run-code@users.noreply.github.com>
2025-10-29 16:17:27 +0200

    added attachements for action points

--- src/etools/applications/rss_admin/urls.py
+++ src/etools/applications/rss_admin/urls.py
@@ -6,12 +6,14 @@ from etools.applications.rss_admin.views import (
     AgreementRssViewSet,
     PartnerOrganizationRssViewSet,
     ProgrammeDocumentRssViewSet,
+    ActionPointRssViewSet,
 )
 
 router = DefaultRouter()
 router.register(r'partners', PartnerOrganizationRssViewSet, basename='rss-admin-partners')
 router.register(r'agreements', AgreementRssViewSet, basename='rss-admin-agreements')
 router.register(r'programme-documents', ProgrammeDocumentRssViewSet, basename='rss-admin-programme-documents')
+router.register(r'action-points', ActionPointRssViewSet, basename='rss-admin-action-points')
 
 app_name = 'rss_admin'
 
