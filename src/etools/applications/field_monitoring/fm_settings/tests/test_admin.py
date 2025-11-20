6aef22eecae622906b383850da8d0429482ab893
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2025-11-20 16:33:00 +0200

    adjust tests

--- src/etools/applications/field_monitoring/fm_settings/tests/test_admin.py
+++ src/etools/applications/field_monitoring/fm_settings/tests/test_admin.py
@@ -37,7 +37,7 @@ class TestLocationSiteAdmin(BaseTenantTestCase):
         self.assertFalse(self.admin.has_import_permission(self.request))
 
     @staticmethod
-    def create_test_xlsx():
+    def create_test_xlsx_with_errors():
         """Helper method to create a test XLSX file in memory"""
         wb = openpyxl.Workbook()
         ws = wb.active
@@ -49,18 +49,37 @@ class TestLocationSiteAdmin(BaseTenantTestCase):
         ws.append(['LOC: Mjaydel_LBN62079', '33.517090', '35.440041'])
         ws.append(['LOC: Majdel el Koura_LBN54029', '34.252449', '35.792671'])
         ws.append(['LOC: Kousba_LBN54044', '34.298401', '35.850361'])
-        ws.append(['', '34.298401', '35.850361'])  # invalid row, no name, will be skipped
-        ws.append(['LOC: Meftah es Sellom_LBN34165', '34.99e', '33.517090'])  # invalid Coordinates, will be skipped
+        ws.append(['', '34.298401', '35.850361'])  # invalid row, no name, will raise error
+        ws.append(['LOC: Meftah es Sellom_LBN34165', '34.99e', '33.517090'])  # invalid Coordinates, will raise error
 
         buffer = BytesIO()
         wb.save(buffer)
         buffer.seek(0)
         return buffer
 
-    def test_import_file(self):
+    @staticmethod
+    def create_test_xlsx_valid():
+        """Helper method to create a test XLSX file in memory"""
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        ws.title = "Data"
+
+        ws.append(['Site_Name', 'Latitude', 'Longitude'])
+
+        ws.append(['LOC: Braachit_LBN41011', '33.175289', '35.443100'])
+        ws.append(['LOC: Mjaydel_LBN62079', '33.517090', '35.440041'])
+        ws.append(['LOC: Majdel el Koura_LBN54029', '34.252449', '35.792671'])
+        ws.append(['LOC: Kousba_LBN54044', '34.298401', '35.850361'])
+
+        buffer = BytesIO()
+        wb.save(buffer)
+        buffer.seek(0)
+        return buffer
+
+    def test_import_file_invalid(self):
         self.sites_file = SimpleUploadedFile(
             'LocationSites.xlsx',
-            self.create_test_xlsx().read(),
+            self.create_test_xlsx_with_errors().read(),
             content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
         )
 
@@ -82,4 +101,31 @@ class TestLocationSiteAdmin(BaseTenantTestCase):
             follow=True
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(LocationSite.objects.count(), 4, '4 Valid, 1 Invalid')
+        self.assertEqual(LocationSite.objects.count(), 0, 'Error was raised')
+
+    def test_import_file_valid(self):
+        self.sites_file = SimpleUploadedFile(
+            'LocationSites.xlsx',
+            self.create_test_xlsx_valid().read(),
+            content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        )
+
+        self.assertEqual(LocationSite.objects.count(), 0)
+
+        self.client.force_login(self.superuser)
+        response = self.client.get(
+            reverse('admin:field_monitoring_settings_locationsite_import')
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        LocationFactory(admin_level=0, is_active=True)
+        response = self.client.post(
+            reverse('admin:field_monitoring_settings_locationsite_import'),
+            data={
+                "import_file": self.sites_file,
+                '_save_records': ['Submit']
+            },
+            follow=True
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(LocationSite.objects.count(), 4)
