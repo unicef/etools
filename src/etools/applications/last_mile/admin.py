a09d8406ef1e178b745980c1db319e291a435af1
smihalache06 <sebastian.mihalache@nordlogic.ro>
2026-01-14 19:13:56 +0200

    Added new feature for lconsignee code

--- src/etools/applications/last_mile/admin.py
+++ src/etools/applications/last_mile/admin.py
@@ -41,10 +41,10 @@ class TransferEvidenceAttachmentInline(AttachmentSingleInline):
 @admin.register(models.PointOfInterest)
 class PointOfInterestAdmin(XLSXImportMixin, admin.ModelAdmin):
     readonly_fields = ('partner_names', 'created_by', 'approved_by')
-    list_display = ('name', 'parent', 'poi_type', 'p_code')
+    list_display = ('name', 'parent', 'poi_type', 'p_code', 'l_consignee_code')
     list_select_related = ('parent', 'approved_by', 'created_by')
     list_filter = ('private', 'is_active', 'poi_type')
-    search_fields = ('name', 'p_code')
+    search_fields = ('name', 'p_code', 'l_consignee_code')
     raw_id_fields = ('partner_organizations',)
     formfield_overrides = {
         models.PointField: {'widget': forms.OSMWidget(attrs={'display_raw': True})},
@@ -213,10 +213,10 @@ class TransferInLine(RestrictedEditAdminMixin, admin.TabularInline):
 class TransferAdmin(AttachmentInlineAdminMixin, admin.ModelAdmin):
     list_display = (
         'display_name', 'partner_organization', 'status', 'transfer_type',
-        'transfer_subtype', 'origin_point', 'destination_point', 'from_partner_organization', 'recipient_partner_organization'
+        'transfer_subtype', 'origin_point', 'destination_point', 'l_consignee_code', 'from_partner_organization', 'recipient_partner_organization'
     )
     list_filter = ('status', 'transfer_type', 'transfer_subtype')
-    search_fields = ('name', 'status', 'origin_point__name', 'destination_point__name', 'partner_organization__organization__name')
+    search_fields = ('name', 'status', 'origin_point__name', 'destination_point__name', 'partner_organization__organization__name', 'l_consignee_code')
     raw_id_fields = ('partner_organization', 'checked_in_by', 'checked_out_by',
                      'origin_point', 'destination_point', 'origin_transfer', 'from_partner_organization', 'recipient_partner_organization', 'transfer_history', 'created_by', 'approved_by')
     inlines = (ProofTransferAttachmentInline, ItemInline)
