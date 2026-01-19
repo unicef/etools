21062b755f79421eeb51361f36a1e11196dd24d0
smihalache06 <sebastian.mihalache@nordlogic.ro>
2026-01-19 14:39:48 +0200

    Permissions rework

---
+++ src/etools/applications/last_mile/services/permissions_service.py
@@ -0,0 +1,98 @@
+from django.contrib.auth.models import Permission
+
+from etools.applications.users.models import Realm
+from etools.applications.last_mile.admin_panel.constants import (
+    LIST_INTERESTED_LASTMILE_PERMS,
+    LIST_INTERESTED_LASTMILE_PERMS_WITHOUT_APPROVE
+)
+
+
+class LMSMPermissionsService:
+
+    LMSM_CO_ADMIN = "LMSM CO Admin"
+    LMSM_HQ_ADMIN = "LMSM HQ Admin"
+    IP_LM_EDITOR = "IP LM Editor"
+    LMSMAPI = "LMSMApi"
+    LMSM_GROUPS = [LMSM_CO_ADMIN, LMSM_HQ_ADMIN, IP_LM_EDITOR, LMSMAPI]
+
+    def assign_permissions_for_co_admin(self, user):
+        for perm_codename in LIST_INTERESTED_LASTMILE_PERMS_WITHOUT_APPROVE:
+            try:
+                perm = Permission.objects.get(codename=perm_codename)
+                if not user.user_permissions.filter(id=perm.id).exists():
+                    user.user_permissions.add(perm)
+            except Permission.DoesNotExist:
+                pass
+
+    def assign_permissions_for_hq_admin(self, user):
+        for perm_codename in LIST_INTERESTED_LASTMILE_PERMS:
+            try:
+                perm = Permission.objects.get(codename=perm_codename)
+                if not user.user_permissions.filter(id=perm.id).exists():
+                    user.user_permissions.add(perm)
+            except Permission.DoesNotExist:
+                pass
+
+    def handle_realm_save_permissions(self, realm_instance, created=False):
+        if realm_instance.group.name not in [self.LMSM_CO_ADMIN, self.LMSM_HQ_ADMIN]:
+            return False
+
+        if realm_instance.is_active:
+            if realm_instance.group.name == self.LMSM_CO_ADMIN:
+                self.assign_permissions_for_co_admin(realm_instance.user)
+                return True
+            elif realm_instance.group.name == self.LMSM_HQ_ADMIN:
+                self.assign_permissions_for_hq_admin(realm_instance.user)
+                return True
+        else:
+            return self.handle_realm_removal_permissions(realm_instance)
+
+        return False
+
+    def handle_realm_deletion_permissions(self, realm_instance):
+        return self.handle_realm_removal_permissions(realm_instance)
+
+    def handle_realm_removal_permissions(self, realm_instance):
+
+        if realm_instance.group.name not in [self.LMSM_CO_ADMIN, self.LMSM_HQ_ADMIN]:
+            return False
+
+        has_co_admin = Realm.objects.filter(
+            user=realm_instance.user,
+            group__name=self.LMSM_CO_ADMIN,
+            is_active=True
+        ).exists()
+
+        has_hq_admin = Realm.objects.filter(
+            user=realm_instance.user,
+            group__name=self.LMSM_HQ_ADMIN,
+            is_active=True
+        ).exists()
+
+        if not has_co_admin and not has_hq_admin:
+            self.remove_all_lmsm_permissions(realm_instance.user)
+        elif has_co_admin and not has_hq_admin:
+            self.remove_approve_permissions(realm_instance.user)
+
+        return True
+
+    def remove_all_lmsm_permissions(self, user):
+        for perm_codename in LIST_INTERESTED_LASTMILE_PERMS:
+            try:
+                perm = Permission.objects.get(codename=perm_codename)
+                user.user_permissions.remove(perm)
+            except Permission.DoesNotExist:
+                pass
+
+    def remove_approve_permissions(self, user):
+        approve_perms = set(LIST_INTERESTED_LASTMILE_PERMS) - set(LIST_INTERESTED_LASTMILE_PERMS_WITHOUT_APPROVE)
+
+        for perm_codename in approve_perms:
+            try:
+                perm = Permission.objects.get(codename=perm_codename)
+                user.user_permissions.remove(perm)
+            except Permission.DoesNotExist:
+                pass
+
+    def is_lmsm_admin_group(self, group_name):
+        return group_name in self.LMSM_GROUPS
--- src/etools/applications/users/signals.py
+++ src/etools/applications/users/signals.py
@@ -6,24 +6,24 @@ from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 from django.utils import timezone
 
-from etools.applications.users.models import Realm, Permission
+from etools.applications.users.models import Realm
 from etools.applications.users.tasks import sync_realms_to_prp
-
-LMSM_CO_ADMIN = "IP LMSM CO Admin"
-LMSM_HQ_ADMIN = "IP LMSM HQ Admin"
+from etools.applications.last_mile.services.permissions_service import LMSMPermissionsService
 
 @receiver(post_save, sender=Realm)
 def sync_realms_to_prp_on_update(instance: Realm, created: bool, **kwargs):
+    # Create an instance of the permissions service
+    permissions_service = LMSMPermissionsService()
+
+    # Check if this is an LMSM admin group and handle permissions (both activation and deactivation)
+    if permissions_service.is_lmsm_admin_group(instance.group.name):
+        permissions_service.handle_realm_save_permissions(instance, created)
+        return
+
     if instance.user.is_unicef_user():
         # only external users are allowed to be synced to prp
         return
-    # Lastmile realms not synced to prp
-    if instance.group.name in ["IP LM Editor", "LMSMApi", LMSM_CO_ADMIN, LMSM_HQ_ADMIN]:
-        if instance.group.name == LMSM_CO_ADMIN:
-            pass
-        if instance.group.name == LMSM_HQ_ADMIN:
-            pass
-        return
+
     transaction.on_commit(
         lambda:
             sync_realms_to_prp.apply_async(
@@ -35,16 +35,17 @@ def sync_realms_to_prp_on_update(instance: Realm, created: bool, **kwargs):
 
 @receiver(post_delete, sender=Realm)
 def sync_realms_to_prp_on_delete(instance: Realm, **kwargs):
+    # Create an instance of the permissions service
+    permissions_service = LMSMPermissionsService()
+
+    # Check if this is an LMSM admin group and handle permissions and not sync to PRP
+    if permissions_service.is_lmsm_admin_group(instance.group.name):
+        permissions_service.handle_realm_deletion_permissions(instance)
+        return
+    
+
     if instance.user.is_unicef_user():
         # only external users are allowed to be synced to prp
-        return
-
-    # Lastmile realms not synced to prp
-    if instance.group.name in ["IP LM Editor", "LMSMApi", LMSM_CO_ADMIN, LMSM_HQ_ADMIN]:
-        if instance.group.name == LMSM_CO_ADMIN:
-            pass
-        if instance.group.name == LMSM_HQ_ADMIN:
-            pass
         return
 
     now = timezone.now()
