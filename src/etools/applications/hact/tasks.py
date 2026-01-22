26da92282c7318c74517f1147596921872228a5e
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2026-01-22 17:40:17 +0200

    294275 Counting of completed Audits completed after year of audit

--- src/etools/applications/hact/tasks.py
+++ src/etools/applications/hact/tasks.py
@@ -8,7 +8,7 @@ from unicef_vision.exceptions import VisionException
 
 from etools.applications.audit.models import UNICEFAuditFocalPoint
 from etools.applications.environment.notifications import send_notification_with_template
-from etools.applications.hact.models import AggregateHact
+from etools.applications.hact.models import AggregateHact, HactHistory
 from etools.applications.partners.models import PartnerOrganization
 from etools.applications.users.models import Country
 from etools.applications.vision.models import VisionSyncLog
@@ -92,6 +92,62 @@ def update_aggregate_hact_values(*args, **kwargs):
                 logger.exception(country)
 
     logger.info('Hact Aggregator Task process finished')
+
+
+@app.task
+def update_audit_hact_count_for_country(business_area_code, starting_year):
+    country = Country.objects.get(business_area_code=business_area_code)
+    log = VisionSyncLog(
+        country=country,
+        handler_name='UpdateAuditHactCountSynchronizer'
+    )
+    connection.set_tenant(country)
+    logger.info('Set country {}'.format(business_area_code))
+
+    try:
+        partners = PartnerOrganization.objects.hact_active()
+        for partner in partners:
+            logger.debug('Updating Partner {} on Audit Count'.format(partner.name))
+            for year in range(starting_year, datetime.today().year + 1):
+                audit_counts = partner.get_audits_completed_for_year(year)
+                logger.debug('{} Audit Counts for year {}:'.format(audit_counts, year))
+                hact_history, created = HactHistory.objects.get_or_create(partner=partner, year=year)
+                if created:
+                    continue
+                for index, li in enumerate(hact_history.partner_values):
+                    if 'Audit Completed' in li:
+                        existing_count = hact_history.partner_values[index][1]
+                        if existing_count != audit_counts:
+                            logger.debug('{} != {} Existing audit count is different from calculated, '
+                                         'updating..'.format(existing_count, audit_counts))
+                            hact_history.partner_values[index][1] = audit_counts
+                            hact_history.save(update_fields=['partner_values'])
+
+    except Exception as e:
+        logger.info('HACT Sync', exc_info=True)
+        log.exception_message = e
+        raise VisionException
+    else:
+        log.total_records = partners.count()
+        log.total_processed = partners.count()
+        log.successful = True
+    finally:
+        log.save()
+
+
+@app.task
+def update_audit_hact_count(*args, **kwargs):
+
+    schema_names = kwargs.get('schema_names', [None])[0]
+    starting_year = kwargs.get('starting_year', [None])
+
+    logger.info('Update Audit Hact Counting process started')
+    countries = Country.objects.exclude(schema_name='public')
+    if schema_names:
+        countries = countries.filter(schema_name__in=schema_names.split(','))
+    for country in countries:
+        update_audit_hact_count_for_country.delay(country.business_area_code, starting_year)
+    logger.info('Update Audit Hact Counting finished.')
 
 
 @app.task
